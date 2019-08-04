8899d4c392fabf02d9306c3808ac76253a4a5cdd
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-04 10:38:30 -0500

    php-redis 5.0.2

--- Formula/php@7.2-redis.rb
+++ Formula/php@7.2-redis.rb
@@ -3,6 +3,6 @@ require File.expand_path("../lib/php_pecl_formula", __dir__)
 class PhpAT72Redis < PhpPeclFormula
   extension_dsl "Redis extension"
 
-  url "https://pecl.php.net/get/redis-5.0.1.tgz"
-  sha256 "6921d6461629e323822bacc91a0906b903cb61f12877a2ee320dd28ddca3c65f"
+  url "https://pecl.php.net/get/redis-5.0.2.tgz"
+  sha256 "4e18d7ebe032a562c358d79f94efa5f187ea90db7a56ef648476e24f4fe0b72c"
 end
