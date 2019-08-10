61251e0e379948e9761c8ce9e6c86f6a6959740b
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 16:59:00 -0500

    php-raphf: add 2.0.0 bottle.

--- Formula/php-raphf.rb
+++ Formula/php-raphf.rb
@@ -5,4 +5,11 @@ class PhpRaphf < PhpPeclFormula
 
   url "https://pecl.php.net/get/raphf-2.0.0.tgz"
   sha256 "95a59ea0dc3fc172805a6acd8327b122dd159b0a85b253826166457a05efa411"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "9bc380831d6c36a47a7fb8fdaf43246f8efb762c3cbeab8416ebeebf6e2bba6d" => :mojave
+    sha256 "7a6dc97bd509d8d4744bcb6ed12d49ebec0795b0054b93a06880b3e3f8950f4b" => :high_sierra
+  end
+
 end
