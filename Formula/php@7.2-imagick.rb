ec0ab6306d55029c080386ae7e943fe27d72cd73
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 14:46:39 -0500

    php@7.2-imagick: add 3.4.4 bottle.

--- Formula/php@7.2-imagick.rb
+++ Formula/php@7.2-imagick.rb
@@ -6,5 +6,11 @@ class PhpAT72Imagick < PhpPeclFormula
   url "https://pecl.php.net/get/imagick-3.4.4.tgz"
   sha256 "8dd5aa16465c218651fc8993e1faecd982e6a597870fd4b937e9ece02d567077"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "58b446abc4509987405e51d3800becc03c3e7328d3eeca68e07ead71aae635d2" => :mojave
+    sha256 "eda2dfeaa176fb1ae737255efed7c3e08b1b9611b03e1d2344858bdb81c0ca36" => :high_sierra
+  end
+
   depends_on "imagemagick"
 end
