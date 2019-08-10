fa02296ffaf223f13988aff8274b8964fedd77a1
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:37:36 -0500

    php-sqlsrv: add 5.6.1 bottle.

--- Formula/php-sqlsrv.rb
+++ Formula/php-sqlsrv.rb
@@ -6,5 +6,12 @@ class PhpSqlsrv < PhpPeclFormula
   url "https://pecl.php.net/get/sqlsrv-5.6.1.tgz"
   sha256 "0ab48ae7a9957586f5ec3ea1c19c528951517529078679a0dc3fd9fe83305445"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "43bf458ac3684f275f5a142bc776afb2c90ea194baac9f56efc3e284c5e3dd9a" => :mojave
+    sha256 "2b82af01f16d9d8d4ad07cf36d94ca850d5066423aa48bf6b0718db77c501ad7" => :high_sierra
+  end
+
   depends_on "unixodbc"
 end
