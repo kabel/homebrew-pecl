7c69e0b276fa429b5c5e7c457a01efbd7fa44035
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:32:24 -0500

    php@7.2-swoole: add 4.4.3 bottle.

--- Formula/php@7.2-swoole.rb
+++ Formula/php@7.2-swoole.rb
@@ -6,6 +6,12 @@ class PhpAT72Swoole< PhpPeclFormula
   url "https://pecl.php.net/get/swoole-4.4.3.tgz"
   sha256 "c04d0be9ae454a784f1799a46fe6b45c83170ef96b76507c75f7a4258cabcf4e"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "14483472ce2c0d8fa0535759e4525c5b742884f2bb09f7dcc7d1016e0e98ae35" => :mojave
+    sha256 "173e8916b227b0a5da3327bb60d49dce911a16e7175e1a8e3ba73e0a8658a583" => :high_sierra
+  end
+
   depends_on "openssl"
   depends_on "brotli"
 
