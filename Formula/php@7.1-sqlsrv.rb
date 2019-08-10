eb7e808b86fb3fc05e33796baf8e69dc3b35e4cc
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:37:37 -0500

    php@7.1-sqlsrv: add 5.6.1 bottle.

--- Formula/php@7.1-sqlsrv.rb
+++ Formula/php@7.1-sqlsrv.rb
@@ -6,5 +6,12 @@ class PhpAT71Sqlsrv < PhpPeclFormula
   url "https://pecl.php.net/get/sqlsrv-5.6.1.tgz"
   sha256 "0ab48ae7a9957586f5ec3ea1c19c528951517529078679a0dc3fd9fe83305445"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "e4ec66cf723d35a1ff4b2b08132ba194f41564ae7f088e65639ddc6b10e92a32" => :mojave
+    sha256 "e1b420dd201c6900de84838e2e37e14f692092a6f9ac33100e134c0a7d646ff9" => :high_sierra
+  end
+
   depends_on "unixodbc"
 end
