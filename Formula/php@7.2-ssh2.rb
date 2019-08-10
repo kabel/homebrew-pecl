594c212ca0217e49db2319e102ac677b44f9c23a
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:37:02 -0500

    php@7.2-ssh2: add 1.2-dev bottle.

--- Formula/php@7.2-ssh2.rb
+++ Formula/php@7.2-ssh2.rb
@@ -7,6 +7,13 @@ class PhpAT72Ssh2 < PhpPeclFormula
   version "1.2-dev"
   sha256 "4c252f0f87eb1a769b0bd784e999218b976c1ef2262302f1fec772f48915dbe6"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "4a0b9fd5f740ded4875cc0b3da24b7ca515e79f370f30062d8f24e7a5b927c36" => :mojave
+    sha256 "98517356e420f698a65705426780b25aa92ab5780688decceac0c6dcbf03739c" => :high_sierra
+  end
+
   depends_on "libssh2"
 
   configure_arg "--with-ssh2=#{Formula["libssh2"]}.opt_prefix"
