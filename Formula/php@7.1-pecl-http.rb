da99ed884a447f9685510946db8bdf1c692a58cd
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:38:32 -0500

    php@7.1-pecl-http: add 3.2.1 bottle.

--- Formula/php@7.1-pecl-http.rb
+++ Formula/php@7.1-pecl-http.rb
@@ -6,6 +6,12 @@ class PhpAT71PeclHttp < PhpPeclFormula
   url "https://pecl.php.net/get/pecl_http-3.2.1.tgz"
   sha256 "9c5ab5ace7f232699bf4d8997dd9135c92bb630357c51d968d1baca698fadc92"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "f63a29991f81cdf6ec9bbc2d4ac9a7a8d6bfd9a0034e1cc0ef0f7b34259ad7df" => :mojave
+    sha256 "6ea01ce5bd7c21451b308ebc45ba160a0907dfce8fb1feac76f20795d55e3aa7" => :high_sierra
+  end
+
   depends_on "brotli"
   depends_on "curl-openssl"
   depends_on "icu4c"
