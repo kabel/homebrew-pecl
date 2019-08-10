1a24eb1d5eefaa7d26491bfa6bb59636149dbaed
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 16:59:50 -0500

    php@7.2-pdo-sqlsrv: add 5.6.1 bottle.

--- Formula/php@7.2-pdo-sqlsrv.rb
+++ Formula/php@7.2-pdo-sqlsrv.rb
@@ -6,5 +6,12 @@ class PhpAT72PdoSqlsrv < PhpPeclFormula
   url "https://pecl.php.net/get/pdo_sqlsrv-5.6.1.tgz"
   sha256 "caf4033677cc7b0992bd68ba1989a095e92150489efc98147445398763a0340a"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "050bd85bc06e2a4fe7588dbbf89cabfe43753047c13cb5043dd8754e28c9416f" => :mojave
+    sha256 "cea30a64a12f795d4f229fc5a3baf95b066e0f4587f717fb571a404ad86ec0eb" => :high_sierra
+  end
+
   depends_on "unixodbc"
 end
