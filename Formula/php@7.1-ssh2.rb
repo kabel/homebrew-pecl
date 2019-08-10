71a2147c562c14cab0b5098b4927cb84bff99e46
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 18:23:56 -0500

    php-ssh2: use https

--- Formula/php@7.1-ssh2.rb
+++ Formula/php@7.1-ssh2.rb
@@ -3,7 +3,7 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT71Ssh2 < PhpPeclFormula
   extension_dsl "Bindings for the libssh2 library"
 
-  url "http://git.php.net/?p=pecl/networking/ssh2.git;a=snapshot;h=79371dc05ff34e58f796e6755e25ba91c169ac1a;sf=tgz"
+  url "https://git.php.net/?p=pecl/networking/ssh2.git;a=snapshot;h=79371dc05ff34e58f796e6755e25ba91c169ac1a;sf=tgz"
   version "1.2-dev"
   sha256 "4c252f0f87eb1a769b0bd784e999218b976c1ef2262302f1fec772f48915dbe6"
 
