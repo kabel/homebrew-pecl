9df37f6e87336af0c1bc379504f45cfadacbbda4
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 19:12:38 -0500

    php@7.1-tideways: add 4.1.7 bottle.

--- Formula/php@7.1-tideways.rb
+++ Formula/php@7.1-tideways.rb
@@ -6,4 +6,11 @@ class PhpAT71Tideways < PhpPeclFormula
 
   url "https://github.com/tideways/php-xhprof-extension/archive/v4.1.7.tar.gz"
   sha256 "3e32ceacc9eec481e27b5df6d06de1e634294e2af9a64fe069bc686dba54fcb8"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "2c6b951ab5a91710f9972e9ff185eb58fee90ae483dbb5b39b4afd9b978a5689" => :mojave
+    sha256 "45ead1e21919e6f9d229efa6146c9d30b74b14dc172e5502a7086ee2ae4f0f4d" => :high_sierra
+  end
+
 end
