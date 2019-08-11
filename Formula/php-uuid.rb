c406deab9ad057b34ed39ece74adcb1b3a73aad5
Kevin Abel <kevin.abel.0@gmail.com>
2019-08-10 19:13:33 -0500

    php-uuid: add 1.0.4 bottle.

--- Formula/php-uuid.rb
+++ Formula/php-uuid.rb
@@ -6,6 +6,13 @@ class PhpUuid < PhpPeclFormula
   url "https://pecl.php.net/get/uuid-1.0.4.tgz"
   sha256 "63079b6a62a9d43691ecbcd4eb52e5e5fe17b5a3d0f8e46e3c17ff265c06a11f"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "01c49798d30d61e44533b90bc4eddeff04c4442ce1bdd791ae7a0ddfb4c2fc75" => :mojave
+    sha256 "bcff22ce80587fe3079f690202e9b612b9283548b87993e3601591b018ad2f04" => :high_sierra
+  end
+
   depends_on "e2fsprogs"
 
   configure_arg "--with-uuid=#{Formula["e2fsprogs"].opt_prefix}"
