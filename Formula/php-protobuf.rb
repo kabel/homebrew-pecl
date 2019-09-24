16a0c0813e90f72a800c7bbb6d8ee641dcef50d7
Kevin Abel <kevin.abel.0@gmail.com>
2019-09-23 20:15:48 -0500

    php-protobuf 3.9.2

--- Formula/php-protobuf.rb
+++ Formula/php-protobuf.rb
@@ -3,13 +3,12 @@ require_relative "../lib/php_pecl_formula"
 class PhpProtobuf < PhpPeclFormula
   extension_dsl "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
 
-  url "https://pecl.php.net/get/protobuf-3.9.1.tgz"
-  sha256 "900cafea4d2e2384979d0c2654d4c33e50bebc23a633dffc57f1afaee780dcb1"
+  url "https://pecl.php.net/get/protobuf-3.9.2.tgz"
+  sha256 "29c39b61afc433c3a53ad90f06d3a2d668808babe7f69af179f4cca92b24db4a"
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "af64045ec7dd8f084765ac20215b296f73cb1be9f799ec666af8b02b42956209" => :mojave
     sha256 "923fd2771444ddd0e41e35b223eef5b6106fdbec9d6e6c8ca1322872276a4d80" => :high_sierra
   end
-
 end
--- Formula/php@7.1-protobuf.rb
+++ Formula/php@7.1-protobuf.rb
@@ -3,13 +3,12 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT71Protobuf < PhpPeclFormula
   extension_dsl "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
 
-  url "https://pecl.php.net/get/protobuf-3.9.1.tgz"
-  sha256 "900cafea4d2e2384979d0c2654d4c33e50bebc23a633dffc57f1afaee780dcb1"
+  url "https://pecl.php.net/get/protobuf-3.9.2.tgz"
+  sha256 "29c39b61afc433c3a53ad90f06d3a2d668808babe7f69af179f4cca92b24db4a"
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "fe692689a23b9e320be2af6faa4c8eed512f90114652b880c2f54572e974f35e" => :mojave
     sha256 "fdf88f0922105b884ce935c737abb24f4e0261f79fdfa87482d36ee92996a49b" => :high_sierra
   end
-
 end
