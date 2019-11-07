e780af651ae24287cd4d2d0c2bfe76b67d2f37d0
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-06 19:32:23 -0600

    php-imagick: revision for upstream dependency update

--- Formula/php-imagick.rb
+++ Formula/php-imagick.rb
@@ -5,6 +5,7 @@ class PhpImagick < PhpPeclFormula
 
   url "https://pecl.php.net/get/imagick-3.4.4.tgz"
   sha256 "8dd5aa16465c218651fc8993e1faecd982e6a597870fd4b937e9ece02d567077"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
