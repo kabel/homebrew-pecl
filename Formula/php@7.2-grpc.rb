85da61fb0624cbd6673b0021575be687fa8d3df7
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-10 14:28:47 -0600

    php@7.2-grpc: add 1.25.0 bottle.

--- Formula/php@7.2-grpc.rb
+++ Formula/php@7.2-grpc.rb
@@ -5,4 +5,11 @@ class PhpAT72Grpc < PhpPeclFormula
 
   url "https://pecl.php.net/get/grpc-1.25.0.tgz"
   sha256 "ca1367a32170c2f500c7a8a09db697444ec3f3513d16aff9b6a02efefb85a7a9"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "e53928c56899e24a85f85c520afb9f080ce5d7e81c48a3773cf347b240048251" => :mojave
+    sha256 "b367d86036ad37151721bada331affb053988ded48f8b1be811340c08971b92d" => :high_sierra
+  end
+
 end
