759e756b169611d20612e89940f33e24cbe3efcd
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-10 11:08:07 -0600

    php-apcu 5.1.18

--- Formula/php-apcu.rb
+++ Formula/php-apcu.rb
@@ -3,13 +3,13 @@ require_relative "../lib/php_pecl_formula"
 class PhpApcu < PhpPeclFormula
   extension_dsl "APC User Cache"
 
-  url "https://pecl.php.net/get/apcu-5.1.17.tgz"
-  sha256 "6b11b477890a9c096ad856e0251920d1d8b9558b7d115256c027e0223755c793"
+  url "https://pecl.php.net/get/apcu-5.1.18.tgz"
+  sha256 "d46debe7acad6073b663ba1710c5cac671a60bf666966cf199bf6d07499bde2b"
+
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "e8a472e3f398fb530960c9c749a43deece78142f79575096818b68485b5bf3e6" => :mojave
     sha256 "bdc55cc84f982b5f1d4a0ff63f3451ce79d4f2da348d556374cf807f4f21a6ba" => :high_sierra
   end
-
 end
--- Formula/php@7.1-apcu.rb
+++ Formula/php@7.1-apcu.rb
@@ -3,13 +3,13 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT71Apcu < PhpPeclFormula
   extension_dsl "APC User Cache"
 
-  url "https://pecl.php.net/get/apcu-5.1.17.tgz"
-  sha256 "6b11b477890a9c096ad856e0251920d1d8b9558b7d115256c027e0223755c793"
+  url "https://pecl.php.net/get/apcu-5.1.18.tgz"
+  sha256 "d46debe7acad6073b663ba1710c5cac671a60bf666966cf199bf6d07499bde2b"
+
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "61690c7d177de92fc35373ec3f8aa83d783369300030d294388c9dec991bf01d" => :mojave
     sha256 "8a52ff703c137ac40f667787baa1c5a8c42b76d92b582dbf4bcf865f464441cb" => :high_sierra
   end
-
 end
