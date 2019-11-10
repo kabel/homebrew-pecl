ccb3d8a8f6df7309ccc8e24ea7b49844bf69c35c
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-10 12:24:26 -0600

    php-grpc 1.25.0 (New Formula)

---
+++ Formula/php@7.1-grpc.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT71Grpc < PhpPeclFormula
+  extension_dsl "A high performance, open source, general RPC framework that puts mobile and HTTP/2 first"
+
+  url "https://pecl.php.net/get/grpc-1.25.0.tgz"
+  sha256 "ca1367a32170c2f500c7a8a09db697444ec3f3513d16aff9b6a02efefb85a7a9"
+end
