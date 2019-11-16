266a127bf030b704bb8e985219ef81df3ed8cbef
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-15 19:11:31 -0600

    php-redis 5.1.1

--- Formula/php@7.2-redis.rb
+++ Formula/php@7.2-redis.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT72Redis < PhpPeclFormula
   extension_dsl "Redis extension"
 
-  url "https://pecl.php.net/get/redis-5.1.0.tgz"
-  sha256 "4b4dca9d2d7f316486f55402f5a29bd976420f728ed731589c8bada973ec89e7"
+  url "https://pecl.php.net/get/redis-5.1.1.tgz"
+  sha256 "621c9d2b4054c797b0e5d5bc5e0f1eeb49bedb37f20e46f838aa4d17d2fe8180"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
