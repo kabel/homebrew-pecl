25e14a82349c17c14052731e5452cc3af473a670
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-30 22:00:14 -0600

    php-igbinary: php at 7.4

--- Formula/php-igbinary.rb
+++ Formula/php-igbinary.rb
@@ -5,11 +5,12 @@ class PhpIgbinary < PhpPeclFormula
 
   url "https://pecl.php.net/get/igbinary-3.0.1.tgz"
   sha256 "5959607f3e236e19a9d01e1a8c74800fb3138f3528ba6601deedbd8b83ab12f1"
+  revision 1
+
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "85d81e1efa3ffdd4c67d1f9f2c60c72b17f58d23940f3c16e16b427eb31cb808" => :mojave
     sha256 "55686884dcba520d99928bfce6fe82eb882650405fa0f9396f23e798a03c2714" => :high_sierra
   end
-
 end
---
+++ Formula/php@7.3-igbinary.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT73Igbinary < PhpPeclFormula
+  extension_dsl "Communicate with any AMQP compliant server"
+
+  url "https://pecl.php.net/get/igbinary-3.0.1.tgz"
+  sha256 "5959607f3e236e19a9d01e1a8c74800fb3138f3528ba6601deedbd8b83ab12f1"
+end
