fa2bc4fc2544de6c9a19e8bee53911b368a44eb1
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-30 22:01:31 -0600

    php-mailparse: php at 7.4

--- Formula/php@7.3-mailparse.rb
+++ Formula/php@7.3-mailparse.rb
@@ -1,9 +1,8 @@
 require_relative "../lib/php_pecl_formula"
 
-class PhpMailparse < PhpPeclFormula
+class PhpAT73Mailparse < PhpPeclFormula
   extension_dsl "Email message manipulation"
 
   url "https://pecl.php.net/get/mailparse-3.0.3.tgz"
   sha256 "bfaeae61342c147bcb66b9a90949722e04b65548e6a9d1eb4896aeb62409d302"
-  revision 1
 end
