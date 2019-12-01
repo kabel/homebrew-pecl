182cc11564564c4ddae42e6ce68aa4608850cd39
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 11:42:35 -0600

    php-mongodb: php at 7.4

--- Formula/php-mongodb.rb
+++ Formula/php-mongodb.rb
@@ -5,6 +5,7 @@ class PhpMongodb < PhpPeclFormula
 
   url "https://pecl.php.net/get/mongodb-1.6.0.tgz"
   sha256 "8686639a5172a8bef01f3991c99eb8e62f4e256fa82da9ac5fc6ec60b474cb2f"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
