08615b26d38916b9f28948d591ac66fc3dbc857a
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 11:49:04 -0600

    php-raphf: php at 7.4

---
+++ Formula/php@7.3-raphf.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT73Raphf < PhpPeclFormula
+  extension_dsl "Resource and persistent handles factory"
+
+  url "https://pecl.php.net/get/raphf-2.0.1.tgz"
+  sha256 "da3566db17422e5ef08b7ff144162952aabc14cb22407cc6b1d2a2d095812bd0"
+end
