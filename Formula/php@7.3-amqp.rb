6e036895845d11c79757e71510ad075fec5de6a4
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-30 21:56:30 -0600

    php-amqp: php at 7.4

--- Formula/php@7.3-amqp.rb
+++ Formula/php@7.3-amqp.rb
@@ -1,11 +1,10 @@
 require_relative "../lib/php_pecl_formula"
 
-class PhpAmqp < PhpPeclFormula
+class PhpAT73Amqp < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
   url "https://pecl.php.net/get/amqp-1.9.4.tgz"
   sha256 "b66b8b8264749cb7a69ae4d09665dad1559b6c4f01430206c8e4187bcd8c782d"
-  revision 1
 
   depends_on "rabbitmq-c"
 end
