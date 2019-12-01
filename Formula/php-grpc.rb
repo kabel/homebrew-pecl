55b2ebcd4a8151ca5e0df31a8f9a0e94e9d0bd26
Kevin Abel <kevin.abel.0@gmail.com>
2019-11-30 21:59:23 -0600

    php-grpc: php at 7.4

--- Formula/php-grpc.rb
+++ Formula/php-grpc.rb
@@ -5,11 +5,12 @@ class PhpGrpc < PhpPeclFormula
 
   url "https://pecl.php.net/get/grpc-1.25.0.tgz"
   sha256 "ca1367a32170c2f500c7a8a09db697444ec3f3513d16aff9b6a02efefb85a7a9"
+  revision 1
+
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "b7201b94ce7b456793ea16f3a29abae05aa9a3ea0edf40ea590783435ab54256" => :mojave
     sha256 "dda95747844b781bf98a5e53620a92807812c80e8027a1d1209ad1ebeffb2bd0" => :high_sierra
   end
-
 end
