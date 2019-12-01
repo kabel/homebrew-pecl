f1e39cb22ffa57de7908bec8d8a13e21b6105668
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 11:51:25 -0600

    php-swoole: php at 7.4

--- Formula/php@7.3-swoole.rb
+++ Formula/php@7.3-swoole.rb
@@ -1,17 +1,10 @@
 require_relative "../lib/php_pecl_formula"
 
-class PhpSwoole< PhpPeclFormula
+class PhpAT73Swoole< PhpPeclFormula
   extension_dsl "Event-driven asynchronous and concurrent networking engine with high performance"
 
   url "https://pecl.php.net/get/swoole-4.4.12.tgz"
   sha256 "ad787fd0602faf8b54060496c3968f2a93c07f40fa083f22c9874699c31cfad5"
-  revision 1
-
-  bottle do
-    root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    sha256 "23da6a99e15838476d5a15e9c4be4753b414fce8e0a828dd7327d8d3aaf4e0e4" => :mojave
-    sha256 "c928522f0cdb7e2a5f0888c715c4fcb70ad2d2f8d3522f16d2838b4ff93b5c69" => :high_sierra
-  end
 
   depends_on "openssl@1.1"
   depends_on "brotli"
