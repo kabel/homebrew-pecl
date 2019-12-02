6725fe30fea63c3811f1efaf3477a34ad696a21e
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 20:45:22 -0600

    php@7.3-igbinary: add 3.0.1 bottle.

--- Formula/php@7.3-igbinary.rb
+++ Formula/php@7.3-igbinary.rb
@@ -5,4 +5,11 @@ class PhpAT73Igbinary < PhpPeclFormula
 
   url "https://pecl.php.net/get/igbinary-3.0.1.tgz"
   sha256 "5959607f3e236e19a9d01e1a8c74800fb3138f3528ba6601deedbd8b83ab12f1"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "1f9a19edce50fc41553794d0e8d0d20a09d6bb6577dec3c4dde554e74622d4f7" => :mojave
+    sha256 "17c069911e2ec3f3bc49c6397c5f8045d345caf822d78e10e6da13de73832142" => :high_sierra
+  end
+
 end
