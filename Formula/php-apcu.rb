4e9b0f3ef5659e57b7e2f9d34505c6d38fe647ee
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 20:47:24 -0600

    php-apcu: update 5.1.18_1 bottle.

--- Formula/php-apcu.rb
+++ Formula/php-apcu.rb
@@ -10,7 +10,7 @@ class PhpApcu < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "964e5ae322fc577f3736318d972956145a6b43a228b4d930be7f5e39759a6661" => :mojave
-    sha256 "d8393b80f248bf97604ec59bfd9e24ee7e44704aa1ba5cde2b2848c8b20177c9" => :high_sierra
+    sha256 "4c09114de4f4f98e5163ca8410c24aa022c87c11db39e0904f00145fe7c2b20c" => :mojave
+    sha256 "c6d8ec669db0b618483de74eb9dfc1e4b6a1fd75d6f1bd73d2e1f23514fd34da" => :high_sierra
   end
 end
