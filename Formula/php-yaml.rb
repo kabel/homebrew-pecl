ad4a45553a0a93bff7c312e67ca7fce8442d1b12
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 21:11:13 -0600

    php-yaml: update 2.0.4_1 bottle.

--- Formula/php-yaml.rb
+++ Formula/php-yaml.rb
@@ -10,8 +10,8 @@ class PhpYaml < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any
-    sha256 "e786f9210896ec3663c89401bbf0c9b6977c9b40f44336fbbd8fb80b89317eb7" => :mojave
-    sha256 "361fc840bf80ebd964b018986c49e8581af4281fe04c35c33c7742910d6a5a36" => :high_sierra
+    sha256 "c458e66fd9a1bef4b9c7903822286b36c3f833d06e62ad59473fa549e4c7b8b0" => :mojave
+    sha256 "a8f538bd9bd34f9aa99b9f95c729c0ffd4e1eae3d6e8f71a0f15ff85b28d2613" => :high_sierra
   end
 
   depends_on "libyaml"
