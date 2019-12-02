b4bc30ca65909a7eb7c1c686a651e9d3e0155804
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 20:47:24 -0600

    php@7.3-apcu: add 5.1.18 bottle.

--- Formula/php@7.3-apcu.rb
+++ Formula/php@7.3-apcu.rb
@@ -5,4 +5,11 @@ class PhpAT73Apcu < PhpPeclFormula
 
   url "https://pecl.php.net/get/apcu-5.1.18.tgz"
   sha256 "d46debe7acad6073b663ba1710c5cac671a60bf666966cf199bf6d07499bde2b"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "cff64d66a4c2532c3074c64d228671a1211e18f8c987bf0c00b466898d667654" => :mojave
+    sha256 "20077a43f0eca35d382c1fcdeee3aa2b80252174d8b548b76e330319983b139b" => :high_sierra
+  end
+
 end
