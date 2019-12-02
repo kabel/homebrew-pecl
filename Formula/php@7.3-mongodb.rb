d634a3d622b56a68270d9b6479d1d81471ce0a88
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 20:49:59 -0600

    php@7.3-mongodb: add 1.6.0 bottle.

--- Formula/php@7.3-mongodb.rb
+++ Formula/php@7.3-mongodb.rb
@@ -6,6 +6,12 @@ class PhpAT73Mongodb < PhpPeclFormula
   url "https://pecl.php.net/get/mongodb-1.6.0.tgz"
   sha256 "8686639a5172a8bef01f3991c99eb8e62f4e256fa82da9ac5fc6ec60b474cb2f"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "4fbad9dbbb2776450e6312ce27b9342b4bd0fb7f12db02181d2e8f61855cbaeb" => :mojave
+    sha256 "22bdbdeceb87ee1edcc1749828fa8ca71b93c859921e7584d6c7d4e00a2b67e7" => :high_sierra
+  end
+
   depends_on "icu4c"
   depends_on "snappy"
 end
