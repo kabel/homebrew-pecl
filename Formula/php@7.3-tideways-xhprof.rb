bd9be55cd41470b35e23f66a4e5ab57a4def92d2
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 23:43:41 -0600

    php@7.3-tideways-xhprof: add 5.0.2 bottle.

--- Formula/php@7.3-tideways-xhprof.rb
+++ Formula/php@7.3-tideways-xhprof.rb
@@ -6,4 +6,11 @@ class PhpAT73TidewaysXhprof < PhpPeclFormula
 
   url "https://github.com/tideways/php-xhprof-extension/archive/v5.0.2.tar.gz"
   sha256 "9ef4accba5a060128a6f4fb4e91da94f55cf42615cea47379d536d9702394559"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "33441943e77acf98bdadab5115bfb50b66686f12f20303723648d137ce3d6e6c" => :mojave
+    sha256 "b80a860ff36ab261e97e215c1747b3babb55acea19e90bc0d494aac830e95351" => :high_sierra
+  end
+
 end
