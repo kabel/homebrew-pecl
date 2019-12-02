b1b084657f2c2ee27434bf15cc46b8d2c2d0963d
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 21:26:55 -0600

    php@7.3-uuid: add 1.1.0 bottle.

--- Formula/php@7.3-uuid.rb
+++ Formula/php@7.3-uuid.rb
@@ -6,6 +6,12 @@ class PhpAT73Uuid < PhpPeclFormula
   url "https://pecl.php.net/get/uuid-1.1.0.tgz"
   sha256 "981c1c2fec77aaffec29242c29a346bc6ec2115053e9b6b5afb4a4e149a2333f"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "e5ecab41bfa301e796a5f4887b80f19e17cfcca10e0f474e8340ea3c4fcc0a9f" => :mojave
+    sha256 "61373d63a193b305c45926b91e4cad96b99757aa60580a0b56e2007f914401b1" => :high_sierra
+  end
+
   depends_on "e2fsprogs"
 
   configure_arg "--with-uuid=#{Formula["e2fsprogs"].opt_prefix}"
