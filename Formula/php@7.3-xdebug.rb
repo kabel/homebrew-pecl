3483f59d35b1df22546e10b767bf40cdee4d9759
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-01 21:07:08 -0600

    php@7.3-xdebug: add 2.8.0 bottle.

--- Formula/php@7.3-xdebug.rb
+++ Formula/php@7.3-xdebug.rb
@@ -6,6 +6,13 @@ class PhpAT73Xdebug < PhpPeclFormula
   url "https://pecl.php.net/get/xdebug-2.8.0.tgz"
   sha256 "cb1d117ecbec7409a408cf24e59d92f7e3816fe8e2331dd0ffc3dce60228c264"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "69923d8031f80faa429ece0dc1cc1ae3fda66a1dc3aa61fd86fecf04817b30ea" => :mojave
+    sha256 "630634eb71d437bed05d91812fb2177bac27bc7ad7d8501b463aed74f6969931" => :high_sierra
+  end
+
   def extension_type
     "zend_extension"
   end
