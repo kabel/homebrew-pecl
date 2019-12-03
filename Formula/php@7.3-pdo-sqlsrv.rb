10839ac61672e55c0f66b3ade322d7985b902591
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-02 21:07:22 -0600

    php@7.3-pdo-sqlsrv: add 5.7.0pre bottle.

--- Formula/php@7.3-pdo-sqlsrv.rb
+++ Formula/php@7.3-pdo-sqlsrv.rb
@@ -6,6 +6,13 @@ class PhpAT73PdoSqlsrv < PhpPeclFormula
   url "https://pecl.php.net/get/pdo_sqlsrv-5.7.0preview.tgz"
   sha256 "9d6ab94ad1ef69906acb44545581d7a3c267a95d859ce37e45c5abbe896227a0"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "807c455978ed679301efce1bd7da1603a59c3997feca983f244e76901d42d864" => :mojave
+    sha256 "bbf7be8449d5e7a1ee82e7778bab4f43efb04e9fc27a387d272dcdf5fd175190" => :high_sierra
+  end
+
   depends_on "unixodbc"
 
   def source_dir
