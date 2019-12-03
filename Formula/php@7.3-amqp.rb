2f3540756dcb7e712372baf21f0aeaa64fd9f540
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-02 21:01:50 -0600

    php@7.3-amqp: add 1.9.4 bottle.

--- Formula/php@7.3-amqp.rb
+++ Formula/php@7.3-amqp.rb
@@ -6,5 +6,11 @@ class PhpAT73Amqp < PhpPeclFormula
   url "https://pecl.php.net/get/amqp-1.9.4.tgz"
   sha256 "b66b8b8264749cb7a69ae4d09665dad1559b6c4f01430206c8e4187bcd8c782d"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "5f754181b4154e81b2f54bac6b3baccf796ac4ba24ad3db1c3686f123cd8dfab" => :mojave
+    sha256 "0c5f792b30cefdaa93b72ae381f1e98892f6fabaeeadaea356c2e4a894078a97" => :high_sierra
+  end
+
   depends_on "rabbitmq-c"
 end
