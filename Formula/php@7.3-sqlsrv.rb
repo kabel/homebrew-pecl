808509f1e9662a810f0d1032aae154e8ec141dea
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-02 21:06:12 -0600

    php@7.3-sqlsrv: add 5.7.0pre bottle.

--- Formula/php@7.3-sqlsrv.rb
+++ Formula/php@7.3-sqlsrv.rb
@@ -6,6 +6,13 @@ class PhpAT73Sqlsrv < PhpPeclFormula
   url "https://pecl.php.net/get/sqlsrv-5.7.0preview.tgz"
   sha256 "31af4b543796c64d9dc220419cc70304853128122a3adca62e07637ad2b56184"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any
+    sha256 "44d302ea39241a110934a66e64d4b6d6c89c49e6de9b40482d09cb9fc667b6f7" => :mojave
+    sha256 "8ad7a614404329e8f4ccc35298e0419c750b083529eedbcd05bad762708446ff" => :high_sierra
+  end
+
   depends_on "unixodbc"
 
   def source_dir
