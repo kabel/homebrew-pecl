e3d8944ee07c0dc253bcf1140c24979d62f8ebb6
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 12:27:46 -0600

    php-mongodb 1.6.1

--- Formula/php-mongodb.rb
+++ Formula/php-mongodb.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpMongodb < PhpPeclFormula
   extension_dsl "MongoDB driver"
 
-  url "https://pecl.php.net/get/mongodb-1.6.0.tgz"
-  sha256 "8686639a5172a8bef01f3991c99eb8e62f4e256fa82da9ac5fc6ec60b474cb2f"
-  revision 1
+  url "https://pecl.php.net/get/mongodb-1.6.1.tgz"
+  sha256 "1560141933a36862ee4d65171d2cc371c9fd468f59f3bef94cf2903186253cc8"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
