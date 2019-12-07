79cfb71515c0fb1275d4f178a6d4b13a9f94e542
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 12:05:44 -0600

    php-mailparse: add 3.0.4pre.ac3be83 bottle.

--- Formula/php-mailparse.rb
+++ Formula/php-mailparse.rb
@@ -6,4 +6,11 @@ class PhpMailparse < PhpPeclFormula
   url "https://git.php.net/?p=pecl/mail/mailparse.git;a=snapshot;h=ac3be83e2c752a04f4ea0029ed71ed5e52e70af7;sf=tgz"
   sha256 "d98551251669f04c6cc5d21a6e0557587802319edcd4bda4eba1c16f35bc3077"
   version "3.0.4pre.ac3be83"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "403426459a61f6a41a8e095318bb3e5f53b51df01a40705d0823041c7fa7b77b" => :mojave
+    sha256 "73fd1383d96ed67a56fb9f734e7f8d433608b3628f99a3af5c2a77ec46f152ab" => :high_sierra
+  end
+
 end
