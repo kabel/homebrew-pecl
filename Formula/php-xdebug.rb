c94f4ce61bf3badb6574362ebaf29851c9eeec61
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 11:20:50 -0600

    php-xdebug 2.8.1

--- Formula/php-xdebug.rb
+++ Formula/php-xdebug.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpXdebug < PhpPeclFormula
   extension_dsl "An extension to assist with debugging and development"
 
-  url "https://pecl.php.net/get/xdebug-2.8.0.tgz"
-  sha256 "cb1d117ecbec7409a408cf24e59d92f7e3816fe8e2331dd0ffc3dce60228c264"
-  revision 1
+  url "https://pecl.php.net/get/xdebug-2.8.1.tgz"
+  sha256 "838be3974e2555bbbd796eb57c34840659815f23079417b5042e8b534fe61520"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
