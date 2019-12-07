eb84a400b6187257b6f9d128a878bb1867b58c57
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 11:41:44 -0600

    php-mailparse: dev url to fix php 7.4 build

--- Formula/php-mailparse.rb
+++ Formula/php-mailparse.rb
@@ -3,7 +3,7 @@ require_relative "../lib/php_pecl_formula"
 class PhpMailparse < PhpPeclFormula
   extension_dsl "Email message manipulation"
 
-  url "https://pecl.php.net/get/mailparse-3.0.3.tgz"
-  sha256 "bfaeae61342c147bcb66b9a90949722e04b65548e6a9d1eb4896aeb62409d302"
-  revision 1
+  url "https://git.php.net/?p=pecl/mail/mailparse.git;a=snapshot;h=ac3be83e2c752a04f4ea0029ed71ed5e52e70af7;sf=tgz"
+  sha256 "d98551251669f04c6cc5d21a6e0557587802319edcd4bda4eba1c16f35bc3077"
+  version "3.0.4pre.ac3be83"
 end
