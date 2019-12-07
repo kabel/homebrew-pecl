35280c9a61e6b3755a35b83b51944b4cff1fc9e1
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-07 12:06:35 -0600

    php-memcached: add 3.1.5 bottle.

--- Formula/php-memcached.rb
+++ Formula/php-memcached.rb
@@ -6,6 +6,12 @@ class PhpMemcached < PhpPeclFormula
   url "https://pecl.php.net/get/memcached-3.1.5.tgz"
   sha256 "b288e45f839593e16c4d8508b241de51a86df4f7322153e6becb9e1c819021fd"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "f770fd385bedb929c3291ee45d8a0139f4dee0609e0749eeefc6a33c652e48af" => :mojave
+    sha256 "3e215a5cafbbe8388090d0627e9fe7ef86452068901549c2e0ced28f12451bf4" => :high_sierra
+  end
+
   depends_on "libmemcached"
 
   configure_arg %W[
