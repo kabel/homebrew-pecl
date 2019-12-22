ed3db0cc0b0cdbf9077125c0ee90c6ca4380f829
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-22 17:39:25 -0600

    php@7.2-memcache: update 4.0.5.2 bottle.

--- Formula/php@7.2-memcache.rb
+++ Formula/php@7.2-memcache.rb
@@ -9,8 +9,8 @@ class PhpAT72Memcache < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "07a527964bdc5f13aeecce512e6d24765fa31bf4e58f9f078f5b53d84f6bf095" => :mojave
-    sha256 "e9b6e933b203560b62cb35c06cd842172f5ceaae70bde0500bea48f7d21ea6af" => :high_sierra
+    sha256 "bb51e4485a31ed4d571fb0c326e83d6b32d964e82c7903e55dcb148a9195ac26" => :mojave
+    sha256 "aee0ffc29383dd8d1b5d817c2a490fa762e48a7b941207ca32575962c448dc18" => :high_sierra
   end
 
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
