d7b717f9f66c249f27f1388c9469044c1f952179
Kevin Abel <kevin.abel.0@gmail.com>
2019-12-22 17:17:37 -0600

    php-memcache 4.0.5.2

--- Formula/php@7.2-memcache.rb
+++ Formula/php@7.2-memcache.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT72Memcache < PhpPeclFormula
   extension_dsl "memcache extension"
 
-  url "https://github.com/websupport-sk/pecl-memcache/archive/4.0.4.tar.gz"
-  sha256 "022607027e31f63c8bf7958a334743ca7c2a1058b3b5fe6e0aea67926954facf"
+  url "https://pecl.php.net/get/memcache-4.0.5.2.tgz"
+  sha256 "7b7667813baea003671f174bbec849e43ff235a8ea4ab7e36c3a0380c2a9ed63"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
