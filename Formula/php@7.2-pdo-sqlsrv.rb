31d7eeb9d421d76fa5fa247cd9bcc3214997b96a
Kevin Abel <kevin.abel.0@gmail.com>
2020-02-01 13:07:21 -0600

    php@7.2-pdo-sqlsrv: update 5.8.0 bottle.

--- Formula/php@7.2-pdo-sqlsrv.rb
+++ Formula/php@7.2-pdo-sqlsrv.rb
@@ -9,8 +9,8 @@ class PhpAT72PdoSqlsrv < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any
-    sha256 "f1882ec78cb539c4ddb71e7731aeee0f6d9293c98a0127845fa93a574bb6cfbc" => :mojave
-    sha256 "00173965b4f41c7ee2d9df2b85ef82005de01c0d67a6f15101ed39becc217997" => :high_sierra
+    sha256 "2c4e4ded878384fb522266ba0ecae5ca305076c2fdc005a4e3e2619c8d1a0afb" => :mojave
+    sha256 "8e6b1285b8470615d5818a8415006fd0322d2f3a2b300d9f22d5e2cc5abd6345" => :high_sierra
   end
 
   depends_on "unixodbc"
