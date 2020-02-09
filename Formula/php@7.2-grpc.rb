095d679e8f8956a1810e0332ac8d0dfced1555bd
Kevin Abel <kevin.abel.0@gmail.com>
2020-02-09 17:55:16 -0600

    php-grpc 1.27.0

--- Formula/php@7.2-grpc.rb
+++ Formula/php@7.2-grpc.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT72Grpc < PhpPeclFormula
   extension_dsl "A high performance, open source, general RPC framework that puts mobile and HTTP/2 first"
 
-  url "https://pecl.php.net/get/grpc-1.26.0.tgz"
-  sha256 "519bc3312937b7f203bc0228099f10056a59aadd5cc09e5d9160aa96c89f3098"
+  url "https://pecl.php.net/get/grpc-1.27.0.tgz"
+  sha256 "3237f0ed837d46b3d270e566d8fb1f8cb4fa9fd8307f88db64835e47acf895c4"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
