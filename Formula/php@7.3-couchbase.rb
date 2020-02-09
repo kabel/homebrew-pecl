1ca881ce7cfd35d58338d7fb46f2bf5c538528c5
Kevin Abel <kevin.abel.0@gmail.com>
2020-02-09 17:52:10 -0600

    php-couchbase 2.6.2

--- Formula/php@7.3-couchbase.rb
+++ Formula/php@7.3-couchbase.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT73Couchbase < PhpPeclFormula
   extension_dsl "Couchbase Server"
 
-  url "https://pecl.php.net/get/couchbase-2.6.1.tgz"
-  sha256 "e977a08f4b6b75eb7a903e76c069ad3a12f19a7ea1b0c4da8b04c1c3cc7a5743"
+  url "https://pecl.php.net/get/couchbase-2.6.2.tgz"
+  sha256 "4f4c1a84edd05891925d7990e8425c00c064f8012ef711a1a7e222df9ad14252"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
