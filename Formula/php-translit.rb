db6acb713c6d27e1e9292b9a7f53bb59f6ee2df4
Elan Ruusamäe <glen@delfi.ee>
2020-02-14 13:18:41 +0200

    Add translit-0.6.3 for PHP 7.4

---
+++ Formula/php-translit.rb
@@ -0,0 +1,7 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpTranslit < PhpPeclFormula
+  extension_dsl "Transliterates non-latin character sets to latin"
+  url "https://pecl.php.net/get/translit-0.6.3.tgz"
+  sha256 "169b0d253049c16463eeffa97343e0758a3ac4fb372336917fe9aea700b22a57"
+end
