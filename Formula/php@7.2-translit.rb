62043df50093e7820f81276273e66a5be34c456c
Kevin Abel <kevin.abel.0@gmail.com>
2020-02-16 15:04:30 -0600

    php@7.2-translit: add 0.6.3 bottle.

--- Formula/php@7.2-translit.rb
+++ Formula/php@7.2-translit.rb
@@ -5,4 +5,11 @@ class PhpAT72Translit < PhpPeclFormula
 
   url "https://pecl.php.net/get/translit-0.6.3.tgz"
   sha256 "169b0d253049c16463eeffa97343e0758a3ac4fb372336917fe9aea700b22a57"
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "66776703d9775af8f0894d8c6e5185b53e3d03428579d68ee0f93f2ab2636966" => :catalina
+    sha256 "dad42f5430c7341299daf55462c9c44febd2645da4d36073a283440d8327874a" => :mojave
+  end
+
 end
