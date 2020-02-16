6cbb014f164ebbe5a3e19f9c45fa1ea9f75cfcc3
Kevin Abel <kevin.abel.0@gmail.com>
2020-02-16 15:12:35 -0600

    php-protobuf: update 3.11.4 bottle.

--- Formula/php-protobuf.rb
+++ Formula/php-protobuf.rb
@@ -9,7 +9,7 @@ class PhpProtobuf < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "a77b9a0a10fc85853d055de104d7f0aa6f1baae69cd9de650a26855c1a0db22c" => :catalina
-    sha256 "73058880789d3901d381129ea0549220b8a0be94e8090bdc2bab06ac1084f7ec" => :mojave
+    sha256 "469eeaa8761ad5801b4f0d293b99eea270833471010d8246a38669fc9c419634" => :catalina
+    sha256 "ce838843594f207df9df1dba9fc71478f79f6bc9a85bf9200b3f328d53fb2353" => :mojave
   end
 end
