62086988ffbffce8b4f00f4c8a6aa738906b7337
Kevin Abel <kevin.abel.0@gmail.com>
2020-03-08 11:51:44 -0500

    php-redis 5.2.0

--- Formula/php-redis.rb
+++ Formula/php-redis.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpRedis < PhpPeclFormula
   extension_dsl "Redis extension"
 
-  url "https://pecl.php.net/get/redis-5.1.1.tgz"
-  sha256 "621c9d2b4054c797b0e5d5bc5e0f1eeb49bedb37f20e46f838aa4d17d2fe8180"
-  revision 1
+  url "https://pecl.php.net/get/redis-5.2.0.tgz"
+  sha256 "4e9096b2bf2d1a67d9a828d07b0a22ba09eb54c1df8a2ba80dc6f04c0504f1bb"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
--- Formula/php@7.3-redis.rb
+++ Formula/php@7.3-redis.rb
@@ -3,13 +3,13 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT73Redis < PhpPeclFormula
   extension_dsl "Redis extension"
 
-  url "https://pecl.php.net/get/redis-5.1.1.tgz"
-  sha256 "621c9d2b4054c797b0e5d5bc5e0f1eeb49bedb37f20e46f838aa4d17d2fe8180"
+  url "https://pecl.php.net/get/redis-5.2.0.tgz"
+  sha256 "4e9096b2bf2d1a67d9a828d07b0a22ba09eb54c1df8a2ba80dc6f04c0504f1bb"
+
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
     sha256 "4bbce4207dc4c1947deeaa00624d4a82059d1c274398af995829a41609047f0b" => :mojave
     sha256 "21d3a4085e7cad6fd100eff9df3c90e036e324d8ab1ed5bee78abaf7793495f6" => :high_sierra
   end
-
 end
