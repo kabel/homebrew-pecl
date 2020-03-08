3e7aa2b1a92f9fcb49e6f0c078551f9e16ce8a65
Kevin Abel <kevin.abel.0@gmail.com>
2020-03-08 12:10:48 -0500

    php@7.2-redis: update 5.2.0 bottle.

--- Formula/php@7.2-redis.rb
+++ Formula/php@7.2-redis.rb
@@ -9,7 +9,7 @@ class PhpAT72Redis < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "b2e22b28ff84fbc2fcf9738b04ed21dba2f29f6740cb45f743d94ffc12689495" => :mojave
-    sha256 "6ac148e7efe8dc341e3c5f8895d5f70ee7060e850864bd1c773dcf4cf765d631" => :high_sierra
+    sha256 "9142bf285d707f37a6a8c19fce8d7a37f352e8494be11a68949c7a6e9c76a7ad" => :catalina
+    sha256 "c8fd5d4acaa42c0b97a01cc9d59aa171028ed506107416b1855c28688ffb0b73" => :mojave
   end
 end
