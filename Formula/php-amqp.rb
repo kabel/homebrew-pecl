e65adc623b7b11643da8f948dba3d0dfab820259
Kevin Abel <kevin.abel.0@gmail.com>
2020-04-03 20:27:52 -0500

    php-amqp: add 1.10.0 bottle.

--- Formula/php-amqp.rb
+++ Formula/php-amqp.rb
@@ -6,5 +6,11 @@ class PhpAmqp < PhpPeclFormula
   url "https://pecl.php.net/get/amqp-1.10.0.tgz"
   sha256 "d5eb39c1cd92c9d50b0dc6529eaaf9b3b1c485d9ee2a97fd892606bd84c93309"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "8b4697ddd2ca21d9f9972acd826533515a75a13f8ba1f72861b8bfc706bd5728" => :catalina
+    sha256 "45922f8f5a8cd1cd7b079715c4e8c5b131f24097cb70d4d513254ff3d3c4f013" => :mojave
+  end
+
   depends_on "rabbitmq-c"
 end
