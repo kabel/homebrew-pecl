3275a2707179cacbc0d364d5ace8e98037fb1d49
Kevin Abel <kevin.abel.0@gmail.com>
2020-04-18 18:24:54 -0500

    php-amqp 1.10.2

--- Formula/php-amqp.rb
+++ Formula/php-amqp.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAmqp < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
-  url "https://pecl.php.net/get/amqp-1.10.0.tgz"
-  sha256 "d5eb39c1cd92c9d50b0dc6529eaaf9b3b1c485d9ee2a97fd892606bd84c93309"
+  url "https://pecl.php.net/get/amqp-1.10.2.tgz"
+  sha256 "0ebc61052eb12406dddf5eabfe8749a12d52c566816b8aab04fb9916d0c26ed2"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
