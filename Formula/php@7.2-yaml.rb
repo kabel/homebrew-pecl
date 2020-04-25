091970139840fd805f8654f321a7d62272914d53
Kevin Abel <kevin.abel.0@gmail.com>
2020-04-25 09:55:18 -0500

    php-yaml 2.1.0

--- Formula/php@7.2-yaml.rb
+++ Formula/php@7.2-yaml.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT72Yaml < PhpPeclFormula
   extension_dsl "YAML 1.1 serialization"
 
-  url "https://pecl.php.net/get/yaml-2.0.4.tgz"
-  sha256 "9786b0386e648f12cc18a038358bd57bee4906e350a2e9ab776d6a5f18fc6680"
+  url "https://pecl.php.net/get/yaml-2.1.0.tgz"
+  sha256 "5acbf64d770b4b0aef8c877326fc0711112db4215a27c0fe8e4478fb7314b666"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
