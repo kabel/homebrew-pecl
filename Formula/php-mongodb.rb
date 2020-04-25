1efc3c3c9a0a7e3f0688b4bc2b4862c3e1e8c627
Kevin Abel <kevin.abel.0@gmail.com>
2020-04-25 10:41:50 -0500

    php-mongodb: revision for icu4c lib change

--- Formula/php-mongodb.rb
+++ Formula/php-mongodb.rb
@@ -5,6 +5,7 @@ class PhpMongodb < PhpPeclFormula
 
   url "https://pecl.php.net/get/mongodb-1.7.4.tgz"
   sha256 "1cec8be860d7d6cff05b075a0e0fc47b1d9f580ef6a0cd46948e455a88a6859a"
+  revision 1
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
