ddcfe6654902d12465e0582c21e00bc642aed8d9
Kevin Abel <kevin.abel.0@gmail.com>
2020-05-30 12:52:27 -0500

    php-protobuf 3.12.2

--- Formula/php@7.3-protobuf.rb
+++ Formula/php@7.3-protobuf.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT73Protobuf < PhpPeclFormula
   extension_dsl "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
 
-  url "https://pecl.php.net/get/protobuf-3.12.1.tgz"
-  sha256 "5877032d206dc33a8135b6e0f1739b6c3f9e533cd2526e803366ed3a3fb7a3f4"
+  url "https://pecl.php.net/get/protobuf-3.12.2.tgz"
+  sha256 "1f4c4927007bebee3622ee0f05a71e7087e0c0f638ec9f3839ba2454fb4d11cb"
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
