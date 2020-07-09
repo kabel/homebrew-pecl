f5fbdcfe4e32b79127dde871fb748fafcbc4f509
Kevin Abel <kevin.abel.0@gmail.com>
2020-07-08 22:01:34 -0500

    php@7.3-mongodb: update 1.7.5 bottle.

--- Formula/php@7.3-mongodb.rb
+++ Formula/php@7.3-mongodb.rb
@@ -8,8 +8,8 @@ class PhpAT73Mongodb < PhpPeclFormula
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    sha256 "5541cd841eb666156991cbae01f4211de0d37686eddb6810fd77a485f6cd7e51" => :catalina
-    sha256 "c2cf7ff22597d988fb7d5a33cdfbd0e2c920a670dd620b78535729fd94f254f2" => :mojave
+    sha256 "15bfc3ac4e760d1da7cedf0431910d3d2c59e9f54a51920d8d5e18eeae5e8287" => :catalina
+    sha256 "39805ba0f6b8cb1e53d99d6d8b901b6f34bdf42ce91a7125c113d41e484fca47" => :mojave
   end
 
   depends_on "icu4c"
