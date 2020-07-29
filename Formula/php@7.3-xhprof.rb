58a2a91684d6587784314c2e007dda5f06ecf6c6
Elan Ruusamäe <glen@delfi.ee>
2020-07-29 18:32:35 +0300

    xhprof: add xhprof 2.2.0

---
+++ Formula/php@7.3-xhprof.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT73Xhprof < PhpPeclFormula
+  extension_dsl "XHProf: A Hierarchical Profiler for PHP"
+
+  url "https://pecl.php.net/get/xhprof-2.2.0.tgz"
+  sha256 "034b3141411f3aaf44f91a0ec86a5f4c805a9372c4ef4f9c5ba38ef0b56f4fa2"
+end
