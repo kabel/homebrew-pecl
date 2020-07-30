ffdc038fc1534e72a264c6bd1150b1b9c33adbb0
Kevin Abel <kevin.abel.0@gmail.com>
2020-07-29 22:17:02 -0500

    php-xhprof: add 2.2.0 bottle.

--- Formula/php-xhprof.rb
+++ Formula/php-xhprof.rb
@@ -7,6 +7,13 @@ class PhpXhprof < PhpPeclFormula
   sha256 "034b3141411f3aaf44f91a0ec86a5f4c805a9372c4ef4f9c5ba38ef0b56f4fa2"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "42984afd57fd4de0737771890b5d440d36c968fdffb29538931111c832571a60" => :catalina
+    sha256 "15bdf917922dde6576aeb632cab5d2bb98111ac063037c5c3c2f16afb4d0505e" => :mojave
+  end
+
   def source_dir
     "#{extension}-#{version}/extension"
   end
