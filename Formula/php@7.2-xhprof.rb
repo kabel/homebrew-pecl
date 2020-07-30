7f14d0c9602ed80cd1141f5f3d8cfca78dd69b95
Kevin Abel <kevin.abel.0@gmail.com>
2020-07-29 21:10:24 -0500

    php-xhprof 2.2.0 (New Formula)
    Squashed commit of the following:
    
    commit fece2c4a2821a2aa3e964905be8225fa060d27aa
    Author: Elan Ruusamäe <glen@pld-linux.org>
    Date:   Wed Jul 29 23:28:56 2020 +0300
    
        xhprof: add required license key
    
        Co-authored-by: Kevin Abel <kevin.abel.0@gmail.com>
    
    commit 77d6ea3e41bfb2286b0091711c5b173b38bc3b1e
    Author: Elan Ruusamäe <glen@delfi.ee>
    Date:   Wed Jul 29 18:48:45 2020 +0300
    
        xhprof: add php@7.4-xhprof alias
    
    commit ca18a3d64c9865ae5e4c1ba04a1e622f3120d758
    Author: Elan Ruusamäe <glen@delfi.ee>
    Date:   Wed Jul 29 18:47:34 2020 +0300
    
        xhprof: override source_dir
    
    commit 58a2a91684d6587784314c2e007dda5f06ecf6c6
    Author: Elan Ruusamäe <glen@delfi.ee>
    Date:   Wed Jul 29 18:32:35 2020 +0300
    
        xhprof: add xhprof 2.2.0

---
+++ Formula/php@7.2-xhprof.rb
@@ -0,0 +1,13 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT72Xhprof < PhpPeclFormula
+  extension_dsl "XHProf: A Hierarchical Profiler for PHP"
+
+  url "https://pecl.php.net/get/xhprof-2.2.0.tgz"
+  sha256 "034b3141411f3aaf44f91a0ec86a5f4c805a9372c4ef4f9c5ba38ef0b56f4fa2"
+  license "Apache-2.0"
+
+  def source_dir
+    "#{extension}-#{version}/extension"
+  end
+end
