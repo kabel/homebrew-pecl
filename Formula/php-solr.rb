42adfae08d99f85e69670e655d39f49f62d4bb0a
Elan Ruusamäe <glen@pld-linux.org>
2020-09-18 12:23:23 +0300

    Solr: Use description from pecl package

--- Formula/php-solr.rb
+++ Formula/php-solr.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/php_pecl_formula"
 
 class PhpSolr < PhpPeclFormula
-  extension_dsl "PHP extension for communicating with Apache Solr Server 4.0+"
+  extension_dsl "The Apache Solr PHP extension is an extremely fast, light-weight, feature-rich library that allows PHP applications to communicate easily and efficiently with Apache Solr server instances using an object-oriented API."
 
   url "https://pecl.php.net/get/solr-2.5.1.tgz"
   sha256 "2b41601e74fe371e6525579637a8ddff270e58a0ff636e7f56df8919d6e7777d"
