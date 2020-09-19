129a5a2f83b97ebd7e0aeed9dad6d3dab320b12d
Kevin Abel <kevin.abel.0@gmail.com>
2020-09-19 14:17:16 -0500

    php-xhprof 2.2.1

--- Formula/php-xhprof.rb
+++ Formula/php-xhprof.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpXhprof < PhpPeclFormula
   extension_dsl "XHProf: A Hierarchical Profiler for PHP"
 
-  url "https://pecl.php.net/get/xhprof-2.2.0.tgz"
-  sha256 "034b3141411f3aaf44f91a0ec86a5f4c805a9372c4ef4f9c5ba38ef0b56f4fa2"
+  url "https://pecl.php.net/get/xhprof-2.2.1.tgz"
+  sha256 "53bebeb3c177cd975549efc953160f98687f30be60993305248c70ce7b15158b"
   license "Apache-2.0"
 
   bottle do
