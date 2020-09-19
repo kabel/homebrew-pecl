85d18fba2eb4c7c908b3f149851bbee496cbc887
Kevin Abel <kevin.abel.0@gmail.com>
2020-09-19 14:15:58 -0500

    php-swoole 4.5.4

--- Formula/php-swoole.rb
+++ Formula/php-swoole.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpSwoole< PhpPeclFormula
   extension_dsl "Event-driven asynchronous and concurrent networking engine with high performance"
 
-  url "https://pecl.php.net/get/swoole-4.5.3.tgz"
-  sha256 "77cc4ef41d76c1a22ff3739d632ec6059b55a21ed1c0db375e0eded08c34a43f"
+  url "https://pecl.php.net/get/swoole-4.5.4.tgz"
+  sha256 "2cf0019d421e5a1f6d745df7655049d4b1c158db88270a296181b1d975449040"
   license "Apache-2.0"
 
   bottle do
