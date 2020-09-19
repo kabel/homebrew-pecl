8c1a92f91519286b5541a117dfd1207901251e87
Elan Ruusamäe <glen@delfi.ee>
2020-09-19 12:10:54 +0300

    Wrap the solr description

--- Formula/php@7.2-solr.rb
+++ Formula/php@7.2-solr.rb
@@ -1,7 +1,12 @@
 require_relative "../lib/php_pecl_formula"
 
 class PhpAT72Solr < PhpPeclFormula
-  extension_dsl "The Apache Solr PHP extension is an extremely fast, light-weight, feature-rich library that allows PHP applications to communicate easily and efficiently with Apache Solr server instances using an object-oriented API."
+  extension_dsl <<~EOS
+    The Apache Solr PHP extension is an extremely fast,
+    light-weight, feature-rich library that allows PHP applications to
+    communicate easily and efficiently with Apache Solr server instances
+    using an object-oriented API
+  EOS
 
   url "https://pecl.php.net/get/solr-2.5.1.tgz"
   sha256 "2b41601e74fe371e6525579637a8ddff270e58a0ff636e7f56df8919d6e7777d"
