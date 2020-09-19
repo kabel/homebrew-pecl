ccd99847b7ecea8e1b8b5759fc4cd78b6ad634bc
Kevin Abel <kevin.abel.0@gmail.com>
2020-09-19 10:24:25 -0500

    php@7.2-solr: add 2.5.1 bottle.

--- Formula/php@7.2-solr.rb
+++ Formula/php@7.2-solr.rb
@@ -12,6 +12,12 @@ class PhpAT72Solr < PhpPeclFormula
   sha256 "2b41601e74fe371e6525579637a8ddff270e58a0ff636e7f56df8919d6e7777d"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "afdb2a6b1e29c146b36e64bb5d8bc8fce6568250d208ea1700b4cbb97434ae63" => :catalina
+    sha256 "f8de2a94029702410bdeac6f9015e186501eab391912502dfddaa3872ea269bc" => :mojave
+  end
+
   depends_on "curl-openssl"
 
   configure_arg %W[
