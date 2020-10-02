429e2668ddf34eb04ee4975348d212df63c2bfe0
Kevin Abel <kevin.abel.0@gmail.com>
2020-10-02 18:16:34 -0500

    php@7.3-xdebug: update 2.9.8 bottle.

--- Formula/php@7.3-xdebug.rb
+++ Formula/php@7.3-xdebug.rb
@@ -10,8 +10,8 @@ class PhpAT73Xdebug < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "34b5111f73abe077dc87d3bf359505c9f0ce16d015436286c0af450f44d9afb3" => :catalina
-    sha256 "e8ad7da958f70ee9d6196325248f75dbdaa429ffc59922b95505022c8268eb09" => :mojave
+    sha256 "6bf5774eb2a0b92781cee9a53b7d1cf350cf673ed99f37200bc47d0f432bc817" => :catalina
+    sha256 "b962bea3220f84efeb2e3f052daf719fef4a027f75572425d8795217791f4034" => :mojave
   end
 
   def extension_type
