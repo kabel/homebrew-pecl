253a475051b69dac4fcb885785cd5a962f91a2df
Kevin Abel <kevin.abel.0@gmail.com>
2020-10-02 18:17:06 -0500

    php-translit: update 0.7.1 bottle.

--- Formula/php-translit.rb
+++ Formula/php-translit.rb
@@ -10,7 +10,7 @@ class PhpTranslit < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "5fb974cf46d18289c803f9264d4b8f40cb4cb94bb7b65ecb33b5c2ee24e41729" => :catalina
-    sha256 "a41b0de8abbeb7b9044793b851039a83c4e3d66756eaa9c7bafd3fda004c9295" => :mojave
+    sha256 "10d309128dd05e337f201af04ae5d0ca9008be00eed03847642d43e448af2c02" => :catalina
+    sha256 "9a87afccb08427239f6f58e630d96e2bea1a7ef8e0790c22a64d05ecb5453c4d" => :mojave
   end
 end
