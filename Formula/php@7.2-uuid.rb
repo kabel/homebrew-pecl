c740a73c475d1fa0cee424370d77d52ee2938a26
Kevin Abel <kevin.abel.0@gmail.com>
2020-10-10 13:11:08 -0500

    php-uuid 1.2.0

--- Formula/php@7.2-uuid.rb
+++ Formula/php@7.2-uuid.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT72Uuid < PhpPeclFormula
   extension_dsl "UUID extension"
 
-  url "https://pecl.php.net/get/uuid-1.1.0.tgz"
-  sha256 "981c1c2fec77aaffec29242c29a346bc6ec2115053e9b6b5afb4a4e149a2333f"
+  url "https://pecl.php.net/get/uuid-1.2.0.tgz"
+  sha256 "5cb834d32fa7d270494aa47fd96e062ef819df59d247788562695fd1f4e470a4"
   license "LGPL-2.1-only"
 
   bottle do
