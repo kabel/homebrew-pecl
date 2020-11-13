4881c33679fd90da46d95f04e59032a088f814f4
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-13 17:54:41 -0600

    php-imagick: bump revisions for another upstream lib break

--- Formula/php@7.3-imagick.rb
+++ Formula/php@7.3-imagick.rb
@@ -6,7 +6,7 @@ class PhpAT73Imagick < PhpPeclFormula
   url "https://pecl.php.net/get/imagick-3.4.4.tgz"
   sha256 "8dd5aa16465c218651fc8993e1faecd982e6a597870fd4b937e9ece02d567077"
   license "PHP-3.01"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
