23241cf9db948fc03dedb064027a1224f1a06cc9
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 12:43:26 -0600

    php-pdo-sqlsrv: Upstream tracking php 8.0

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -5,6 +5,7 @@ class PhpPdoSqlsrv < PhpPeclFormula
 
   url "https://pecl.php.net/get/pdo_sqlsrv-5.8.1.tgz"
   sha256 "2047b6cb4e5d8592c8cc31df88b5f23654ed94f0c2f084b148403f4746276a19"
+  revision 1
   license "MIT"
 
   bottle do
