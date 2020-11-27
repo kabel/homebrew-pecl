90b13f8cc1010f4969fe97a4e2671917cfb2b664
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 12:54:15 -0600

    php-ssh2: Upstream tracking php 8.0

--- Formula/php-ssh2.rb
+++ Formula/php-ssh2.rb
@@ -6,7 +6,7 @@ class PhpSsh2 < PhpPeclFormula
   url "http://git.php.net/?p=pecl/networking/ssh2.git;a=snapshot;h=e9e1fd5cbefe2f43de37974e5988a9d05d005078;sf=tgz"
   version "1.2"
   sha256 "7536cf4975915389c60afc9784e8ecf5100d1d994ea01749cb418e388069c411"
-  revision 1
+  revision 2
   license "PHP-3.01"
 
   bottle do
