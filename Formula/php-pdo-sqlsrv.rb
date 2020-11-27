31f01e43d69379766848148dd240ca1f2ae65909
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 14:43:06 -0600

    php-pdo-sqlsrv: Upstream broke php 8.0
    Remove bottle until compat release

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -8,12 +8,5 @@ class PhpPdoSqlsrv < PhpPeclFormula
   revision 1
   license "MIT"
 
-  bottle do
-    root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    cellar :any
-    sha256 "ecbdfbdf8eba3d2a44de44e0c43c5d1ddd8e5aff4c31d83512cdf4a4a8366b65" => :catalina
-    sha256 "b6441883ae96d73f4e44244263f139513fe189d6fd95da684763a1efab888533" => :mojave
-  end
-
   depends_on "unixodbc"
 end
