92a7e83f2560531dee6f64cf2a459449856c3ec5
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 14:46:42 -0600

    php-tideways-xhprof: Upstream broke php 8.0
    Remove bottle until compat release

--- Formula/php-tideways-xhprof.rb
+++ Formula/php-tideways-xhprof.rb
@@ -8,11 +8,4 @@ class PhpTidewaysXhprof < PhpPeclFormula
   sha256 "9ef4accba5a060128a6f4fb4e91da94f55cf42615cea47379d536d9702394559"
   revision 1
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    cellar :any_skip_relocation
-    sha256 "b844286345920fb30cbc092d6890768b6b7be1f66e5fb331d9ceebbd49b6384a" => :mojave
-    sha256 "329dc5da719919c3ffada73af92e0257c389abac8af254361769099715de951d" => :high_sierra
-  end
 end
