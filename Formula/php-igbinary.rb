9c5343d884b52e5d6f43c9a7f1f7b9e8d52e3461
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 12:32:51 -0600

    php-igbinary: Upstream tracking php 8.0

--- Formula/php-igbinary.rb
+++ Formula/php-igbinary.rb
@@ -5,6 +5,7 @@ class PhpIgbinary < PhpPeclFormula
 
   url "https://pecl.php.net/get/igbinary-3.1.6.tgz"
   sha256 "b9bf87dc4d861c6769ae2a13b085395e2741505c629895ec4559781a5031fdea"
+  revision 1
   license "BSD-3-Clause"
 
   bottle do
