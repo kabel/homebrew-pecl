0b0f64a86902905fd56934101f65d6771bb2a959
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 14:37:09 -0600

    php-couchbase: Upstream broke php 8.0
    Remove bottle until a new release is made

--- Formula/php-couchbase.rb
+++ Formula/php-couchbase.rb
@@ -8,12 +8,6 @@ class PhpCouchbase < PhpPeclFormula
   revision 1
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    sha256 "c80bf0881d25b605e012c322213ae475840547f858196cdeabfe55223998cbd5" => :catalina
-    sha256 "4cf46a16a84e5145faca1786c269c9f156587dc0df7bf04185e4cf6b7b870b9a" => :mojave
-  end
-
   depends_on "libcouchbase"
 
   configure_arg "--with-couchbase=#{Formula["libcouchbase"].opt_prefix}"
