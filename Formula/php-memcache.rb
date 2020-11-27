833a6d1a493d5eaa3758cea443c22f27c36e7e2c
Kevin Abel <kevin.abel.0@gmail.com>
2020-11-27 14:41:35 -0600

    php-memcache: Upstream broke php 8.0
    Remove bottle until compat release

--- Formula/php-memcache.rb
+++ Formula/php-memcache.rb
@@ -8,12 +8,5 @@ class PhpMemcache < PhpPeclFormula
   revision 1
   license "PHP-3.0"
 
-  bottle do
-    root_url "https://dl.bintray.com/kabel/bottles-pecl"
-    cellar :any_skip_relocation
-    sha256 "148f45b24916a59533cb7f7e7de96af60b06fe52474f22d96170d10c5ff4cd59" => :mojave
-    sha256 "af81c72f3a25752add2561d71e2e130b7e8babbe0e05fae0e1af399ae12df72a" => :high_sierra
-  end
-
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
 end
