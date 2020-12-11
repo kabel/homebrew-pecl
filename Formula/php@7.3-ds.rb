f2e77ba07646996bbfd5477dfd31fb85f933b75e
Elan Ruusamäe <glen@delfi.ee>
2020-12-11 16:03:08 +0200

    php-ds 1.3.0 (New Formula)
    https://pecl.php.net/package/ds

---
+++ Formula/php@7.3-ds.rb
@@ -0,0 +1,9 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT73Ds < PhpPeclFormula
+  extension_dsl "Data Structures for PHP"
+
+  url "https://pecl.php.net/get/ds-1.3.0.tgz"
+  sha256 "0114b146e1036d75a83cd438200df73db030b5d12b8c687843809d1d0cec91be"
+  license "MIT"
+end
