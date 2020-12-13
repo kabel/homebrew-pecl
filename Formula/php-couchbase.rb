6ca240341318a9311dc087c7b06b4c1f4573aaf4
Kevin Abel <kevin.abel.0@gmail.com>
2020-12-12 19:42:08 -0600

    php-couchbase: update 3.0.5 bottle.

--- Formula/php-couchbase.rb
+++ Formula/php-couchbase.rb
@@ -9,6 +9,8 @@ class PhpCouchbase < PhpPeclFormula
 
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    sha256 "a7ce35e15b3f0c4927a5c47bd95691a930738451742c88b5ba88e55220405180" => :catalina
+    sha256 "615c0336ea364e6bad023ace4b1ede5518b66d67472c398b38241ab37b32b28b" => :mojave
   end
 
   depends_on "libcouchbase"
