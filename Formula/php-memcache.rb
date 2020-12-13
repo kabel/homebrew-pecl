8e08b6620e3ce4a433c86eb447f94d78fabc8cba
Kevin Abel <kevin.abel.0@gmail.com>
2020-12-12 18:57:23 -0600

    php-memcache 8.0

--- Formula/php-memcache.rb
+++ Formula/php-memcache.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpMemcache < PhpPeclFormula
   extension_dsl "memcache extension"
 
-  url "https://pecl.php.net/get/memcache-4.0.5.2.tgz"
-  sha256 "7b7667813baea003671f174bbec849e43ff235a8ea4ab7e36c3a0380c2a9ed63"
-  revision 1
+  url "https://pecl.php.net/get/memcache-8.0.tgz"
+  sha256 "defe33e6f7831d82b7283b95e14a531070531acbf21278f3f0d7050505cf3395"
   license "PHP-3.0"
 
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
