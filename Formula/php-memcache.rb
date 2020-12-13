d3664e5c74ce1a3f8627fee20e036feb2e262f9b
Kevin Abel <kevin.abel.0@gmail.com>
2020-12-12 19:21:18 -0600

    php-memcache: update 8.0 bottle.

--- Formula/php-memcache.rb
+++ Formula/php-memcache.rb
@@ -7,5 +7,12 @@ class PhpMemcache < PhpPeclFormula
   sha256 "defe33e6f7831d82b7283b95e14a531070531acbf21278f3f0d7050505cf3395"
   license "PHP-3.0"
 
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+    sha256 "44d5052276c22d1a33a2ced90ebf6383c4807541d4325e1d9a39b35a05df505a" => :catalina
+    sha256 "84caca8451432c8233778cedb6054d396ef84a6ff76f827f7ad4a0e5cdb0f00a" => :mojave
+  end
+
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
 end
