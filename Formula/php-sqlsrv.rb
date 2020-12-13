0a1f0d59dace5ef3557ac11c7108d880c56d08a3
Kevin Abel <kevin.abel.0@gmail.com>
2020-12-12 19:39:29 -0600

    php-sqlsrv: update 5.9.0beta2 bottle.

--- Formula/php-sqlsrv.rb
+++ Formula/php-sqlsrv.rb
@@ -10,6 +10,8 @@ class PhpSqlsrv < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any
+    sha256 "1678d8633fe12f2c5325f8aaa276181454358d4d76851fe1df004e88dbad3ec6" => :catalina
+    sha256 "63fb68baa9304a10af7fbcad0feca0c0b9ac746405eb332d65f1e13c608b730e" => :mojave
   end
 
   depends_on "unixodbc"
