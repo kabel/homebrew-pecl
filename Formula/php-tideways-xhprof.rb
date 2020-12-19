9ae8287c9932c9c654f98eba44197a7898595084
Kevin Abel <kevin.abel.0@gmail.com>
2020-12-18 18:07:59 -0600

    php-tideways-xhprof 5.0.4

--- Formula/php-tideways-xhprof.rb
+++ Formula/php-tideways-xhprof.rb
@@ -4,8 +4,12 @@ class PhpTidewaysXhprof < PhpPeclFormula
   extension_dsl "Modern XHProf compatible PHP Profiler"
   homepage "https://tideways.com/"
 
-  url "https://github.com/tideways/php-xhprof-extension/archive/v5.0.2.tar.gz"
-  sha256 "9ef4accba5a060128a6f4fb4e91da94f55cf42615cea47379d536d9702394559"
-  revision 1
+  url "https://github.com/tideways/php-xhprof-extension/archive/v5.0.4.tar.gz"
+  sha256 "a414e5d30cba360fbbc54ce929a6db4ae8a9b93a2e58d447e6a5dd7af6fecf72"
   license "Apache-2.0"
+
+  bottle do
+    root_url "https://dl.bintray.com/kabel/bottles-pecl"
+    cellar :any_skip_relocation
+  end
 end
