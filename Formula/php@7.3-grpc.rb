71291bb65d4f40828520e7e98e6cee405104a077
Kevin Abel <kevin.abel.0@gmail.com>
2021-01-24 14:59:57 -0600

    php@7.3-grpc: update 1.35.0 bottle.

--- Formula/php@7.3-grpc.rb
+++ Formula/php@7.3-grpc.rb
@@ -10,7 +10,7 @@ class PhpAT73Grpc < PhpPeclFormula
   bottle do
     root_url "https://dl.bintray.com/kabel/bottles-pecl"
     cellar :any_skip_relocation
-    sha256 "9c0e9d9111e586c9d329a976dae01d574f51d0a959c506f485755367462ccb55" => :catalina
-    sha256 "d81a9a867a68f575e44d3fe3e43453b84a56aadfd4614731ab55256f0703a0af" => :mojave
+    sha256 "b4c8c09e30520185317139342e722b70e13ee31b5fb165d418da108353ba4ac8" => :catalina
+    sha256 "c37c6963b8b49e99676cf597afb0d4b13c5c105cddb75e1187559777b38f4b61" => :mojave
   end
 end
