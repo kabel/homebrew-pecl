6bd7f39f153600641b1ee1d6b0111307b3e676c5
Kevin Abel <kevin.abel.0@gmail.com>
2021-02-19 23:34:20 -0600

    php-protobuf: add 3.15.1 bottle.

--- Formula/php-protobuf.rb
+++ Formula/php-protobuf.rb
@@ -6,4 +6,11 @@ class PhpProtobuf < PhpPeclFormula
   url "https://pecl.php.net/get/protobuf-3.15.1.tgz"
   sha256 "256aa1f17f86be673f05a6697ef2d0cc022b863ffb09f8ab9295280b6f94c2e4"
   license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, big_sur:  "0d21c48934e8c468bf55da4d1ee29e2c970bb7ff7cab5b0256e8708c4d7aa55c"
+    sha256 cellar: :any_skip_relocation, catalina: "b4b65b5ed3f67577eb3eab1e3153cf7ccd4122d516fcb1621ef6bc18d8d4ced5"
+    sha256 cellar: :any_skip_relocation, mojave:   "5bfb6077e230bcab631b3709f8c7c8d48698321588eb9ae62d769458010d613b"
+  end
 end
