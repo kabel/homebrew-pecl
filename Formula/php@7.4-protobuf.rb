33c35715d33ef36ca274f5e3d87afef318cdb73a
Kevin Abel <kevin.abel.0@gmail.com>
2021-02-26 20:28:37 -0600

    php@7.4-protobuf: update 3.15.3 bottle.

--- Formula/php@7.4-protobuf.rb
+++ Formula/php@7.4-protobuf.rb
@@ -9,8 +9,8 @@ class PhpAT74Protobuf < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, big_sur:  "b01afd9f400da85fab97a6f3a68f50522a113a5768e778a4209bcb89cb753494"
-    sha256 cellar: :any_skip_relocation, catalina: "a11d75119e64f1dca7e0f99700c91af12fc6174a1a90989135cd330ec41d3aba"
-    sha256 cellar: :any_skip_relocation, mojave:   "7f8cc3b448673f1e1392024dfdd1f101b764dd5dfb5fcdd6fcfe75ebf603dc8b"
+    sha256 cellar: :any_skip_relocation, big_sur:  "3eea14403857e013726e73da39685ba40b9e6b50719d334d4a5c9b9fbc8fc3cf"
+    sha256 cellar: :any_skip_relocation, catalina: "3c9d0204cf0ea2eef3e64bc880c6188a817081df08191fb22867850865069064"
+    sha256 cellar: :any_skip_relocation, mojave:   "d504bb18abdcbf0c35d370b05dd54899569ee4c0b970922557d1ec1f96158d59"
   end
 end
