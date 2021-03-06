dc0fca0f019e6f894e00409cbd89c612c9740ffa
Kevin Abel <kevin.abel.0@gmail.com>
2021-03-06 12:18:40 -0600

    php-ssh2 1.3.1

--- Formula/php-ssh2.rb
+++ Formula/php-ssh2.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpSsh2 < PhpPeclFormula
   extension_dsl "Bindings for the libssh2 library"
 
-  url "https://pecl.php.net/get/ssh2-1.2.tgz"
-  sha256 "7f9f205f5b555692f7b010ffb68a01e21860176062f2ec14dc577d994cecd929"
-  revision 2
+  url "https://pecl.php.net/get/ssh2-1.3.1.tgz"
+  sha256 "9093a1f8d24dc65836027b0e239c50de8d5eaebf8396bc3331fdd38c5d69afd9"
   license "PHP-3.01"
 
   depends_on "libssh2"
