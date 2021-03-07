551d3edb544b488a230c35bb9561fc39f15fc9d5
Kevin Abel <kevin.abel.0@gmail.com>
2021-03-06 22:04:47 -0600

    php-redis: Fix versioned dependencies references

--- Formula/php@7.3-redis.rb
+++ Formula/php@7.3-redis.rb
@@ -5,12 +5,12 @@ class PhpAT73Redis < PhpPeclFormula
 
   url "https://pecl.php.net/get/redis-5.3.3.tgz"
   sha256 "37789161e164cd52239a30c9a238da61fec5c8395cdac385b6ed8f0c50fd92f0"
-  revision 1
+  revision 2
   license "PHP-3.01"
 
   depends_on "lz4"
-  depends_on "kabel/pecl/php-igbinary"
-  depends_on "kabel/pecl/php-msgpack"
+  depends_on "kabel/pecl/php@7.3-igbinary"
+  depends_on "kabel/pecl/php@7.3-msgpack"
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
@@ -31,9 +31,9 @@ class PhpAT73Redis < PhpPeclFormula
   def install
     # fix include location for configure to find
     mkdir_p buildpath/source_dir/"include/php/ext"
-    ln_s Formula["kabel/pecl/php-igbinary"].opt_include/"php/ext/igbinary",
+    ln_s Formula["kabel/pecl/php@7.3-igbinary"].opt_include/"php/ext/igbinary",
       buildpath/source_dir/"include/php/ext/igbinary"
-    ln_s Formula["kabel/pecl/php-msgpack"].opt_include/"php/ext/msgpack",
+    ln_s Formula["kabel/pecl/php@7.3-msgpack"].opt_include/"php/ext/msgpack",
       buildpath/source_dir/"include/php/ext/msgpack"
     super
   end
--- Formula/php@7.4-redis.rb
+++ Formula/php@7.4-redis.rb
@@ -5,12 +5,12 @@ class PhpAT74Redis < PhpPeclFormula
 
   url "https://pecl.php.net/get/redis-5.3.3.tgz"
   sha256 "37789161e164cd52239a30c9a238da61fec5c8395cdac385b6ed8f0c50fd92f0"
-  revision 1
+  revision 2
   license "PHP-3.01"
 
   depends_on "lz4"
-  depends_on "kabel/pecl/php-igbinary"
-  depends_on "kabel/pecl/php-msgpack"
+  depends_on "kabel/pecl/php@7.4-igbinary"
+  depends_on "kabel/pecl/php@7.4-msgpack"
 
   configure_arg %W[
     --enable-redis-igbinary
@@ -31,9 +31,9 @@ class PhpAT74Redis < PhpPeclFormula
   def install
     # fix include location for configure to find
     mkdir_p buildpath/source_dir/"include/php/ext"
-    ln_s Formula["kabel/pecl/php-igbinary"].opt_include/"php/ext/igbinary",
+    ln_s Formula["kabel/pecl/php@7.4-igbinary"].opt_include/"php/ext/igbinary",
       buildpath/source_dir/"include/php/ext/igbinary"
-    ln_s Formula["kabel/pecl/php-msgpack"].opt_include/"php/ext/msgpack",
+    ln_s Formula["kabel/pecl/php@7.4-msgpack"].opt_include/"php/ext/msgpack",
       buildpath/source_dir/"include/php/ext/msgpack"
     super
   end
