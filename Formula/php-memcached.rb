6cdbc7c67a54ed5d6a0097a097ef292e87366fff
Kevin Abel <kevin.abel.0@gmail.com>
2021-03-06 22:41:16 -0600

    php-memcached: support all serialization formats

--- Formula/php-memcached.rb
+++ Formula/php-memcached.rb
@@ -5,7 +5,7 @@ class PhpMemcached < PhpPeclFormula
 
   url "https://pecl.php.net/get/memcached-3.1.5.tgz"
   sha256 "b288e45f839593e16c4d8508b241de51a86df4f7322153e6becb9e1c819021fd"
-  revision 1
+  revision 2
   license "PHP-3.01"
 
   bottle do
@@ -15,9 +15,26 @@ class PhpMemcached < PhpPeclFormula
   end
 
   depends_on "libmemcached"
+  depends_on "kabel/pecl/php-igbinary"
+  depends_on "kabel/pecl/php-msgpack"
 
   configure_arg %W[
+    --enable-memcached-session
+    --enable-memcached-igbinary
+    --enable-memcached-json
+    --enable-memcached-msgpack
+    --enable-memcached-sasl
     --with-libmemcached-dir=#{Formula["libmemcached"].opt_prefix}
     --with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
   ]
+
+  def install
+    # fix include location for configure to find
+    mkdir_p buildpath/source_dir/"include/php/ext"
+    ln_s Formula["kabel/pecl/php-igbinary"].opt_include/"php/ext/igbinary",
+      buildpath/source_dir/"include/php/ext/igbinary"
+    ln_s Formula["kabel/pecl/php-msgpack"].opt_include/"php/ext/msgpack",
+      buildpath/source_dir/"include/php/ext/msgpack"
+    super
+  end
 end
--- Formula/php@7.3-memcached.rb
+++ Formula/php@7.3-memcached.rb
@@ -5,6 +5,7 @@ class PhpAT73Memcached < PhpPeclFormula
 
   url "https://pecl.php.net/get/memcached-3.1.5.tgz"
   sha256 "b288e45f839593e16c4d8508b241de51a86df4f7322153e6becb9e1c819021fd"
+  revision 1
   license "PHP-3.01"
 
   bottle do
@@ -14,9 +15,26 @@ class PhpAT73Memcached < PhpPeclFormula
   end
 
   depends_on "libmemcached"
+  depends_on "kabel/pecl/php@7.3-igbinary"
+  depends_on "kabel/pecl/php@7.3-msgpack"
 
   configure_arg %W[
+    --enable-memcached-session
+    --enable-memcached-igbinary
+    --enable-memcached-json
+    --enable-memcached-msgpack
+    --enable-memcached-sasl
     --with-libmemcached-dir=#{Formula["libmemcached"].opt_prefix}
     --with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
   ]
+
+  def install
+    # fix include location for configure to find
+    mkdir_p buildpath/source_dir/"include/php/ext"
+    ln_s Formula["kabel/pecl/php@7.3-igbinary"].opt_include/"php/ext/igbinary",
+      buildpath/source_dir/"include/php/ext/igbinary"
+    ln_s Formula["kabel/pecl/php@7.3-msgpack"].opt_include/"php/ext/msgpack",
+      buildpath/source_dir/"include/php/ext/msgpack"
+    super
+  end
 end
