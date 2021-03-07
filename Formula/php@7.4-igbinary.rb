d75e3f83094985beb807e66dadd0fafa84fe588f
Kevin Abel <kevin.abel.0@gmail.com>
2021-03-06 21:01:45 -0600

    php@7.4-igbinary: update 3.2.1_1 bottle.

--- Formula/php@7.4-igbinary.rb
+++ Formula/php@7.4-igbinary.rb
@@ -10,8 +10,8 @@ class PhpAT74Igbinary < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    cellar :any_skip_relocation
-    sha256 "709cb9cee1246ed898f612b50249ab779d58809b85f9867e1e173be6848f0983" => :catalina
-    sha256 "0feafd84848199b2abf0f77255b49855b4425d86c77a6f95204033e4e7189f44" => :mojave
+    sha256 cellar: :any_skip_relocation, big_sur:  "bef37f7eda847f24bf034a90b9385452dd9fe9fc9cb27e8f87d602e458e22184"
+    sha256 cellar: :any_skip_relocation, catalina: "d546c77f9e039ea72e270240b62a584a6c3de5a520681ed0bf3f304afc2de88e"
+    sha256 cellar: :any_skip_relocation, mojave:   "e7d493ebb46218216dc30e48ac43d2aafb1a3a39cdf917b31b78bc89e575240f"
   end
 end
