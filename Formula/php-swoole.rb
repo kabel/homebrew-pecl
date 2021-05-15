0af963920627615cae38fe9f4e0fe539b1518406
Kevin Abel <kevin.abel.0@gmail.com>
2021-05-15 12:27:32 -0500

    php-swoole: update 4.6.7 bottle.

--- Formula/php-swoole.rb
+++ Formula/php-swoole.rb
@@ -9,9 +9,9 @@ class PhpSwoole< PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "55bb7e92b709c9f39095d162d5c10d487cd0b9131099541f486771ec8c739331"
-    sha256 catalina: "e9237683a0d56b525c06b18d012e77e86ba385e273307904a442e93ad75f2fe7"
-    sha256 mojave:   "5399340bcfc464a35e88121a6c6df3fd84ee564cba580e42dd4592524c83e787"
+    sha256 big_sur:  "eb54c0051208a0c536e1af680dee754790c292ce0493ba1c4d22b35fe2608c19"
+    sha256 catalina: "e8e3913143326c94efd4cb5c8e41ccd6a9a2a3f45f09a5e4d6c08bd49f567569"
+    sha256 mojave:   "8a24978fc2c8cf6e14c90a00e096af33125a5daa0b0a7fed720f9f629832fdb9"
   end
 
   depends_on "openssl@1.1"
