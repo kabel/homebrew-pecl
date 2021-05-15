537288d507c352aa5a47a67520d49b40e393c1e6
Kevin Abel <kevin.abel.0@gmail.com>
2021-05-15 12:26:58 -0500

    php@7.3-couchbase: update 3.1.2 bottle.

--- Formula/php@7.3-couchbase.rb
+++ Formula/php@7.3-couchbase.rb
@@ -9,9 +9,9 @@ class PhpAT73Couchbase < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "93e893bd164bed7f407cc74f6f7379b955ec901226fae87f2b83d4346faaf2a1"
-    sha256 catalina: "7527a2fc54a3cd52d74b4f1febd44271b5165896d6fd026d0859c6f3c6379452"
-    sha256 mojave:   "ab71bb84a3b45b1825792cff9fdd3b879a816984babda3782d5cd524a35948f9"
+    sha256 big_sur:  "ea31da2e6ced3e8b953a421cdb9f2c3167064cc8a4a37353f5471fa148e44d2e"
+    sha256 catalina: "b49022a4743b8ed8e9b75e419df4be906757c2c34b03d74696a5628cde6206c6"
+    sha256 mojave:   "6875883038f90147662b0b255c06469effedc82de799cfa7cc69f2dd9bb97ea9"
   end
 
   depends_on "libcouchbase"
