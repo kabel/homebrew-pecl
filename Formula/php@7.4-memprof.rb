0b97a524515ced5e21f92179e488d906741e4c15
Elan Ruusamäe <glen@delfi.ee>
2021-06-04 09:52:44 +0300

    Add memprof-3.0.0 formulas

---
+++ Formula/php@7.4-memprof.rb
@@ -0,0 +1,9 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT74Memprof < PhpPeclFormula
+  extension_dsl "Memprof is a fast and accurate memory profiler that can be used to find the cause of memory leaks in PHP applications"
+
+  url "https://pecl.php.net/get/memprof-3.0.0.tgz"
+  sha256 "3ea040f59f828fe6999ffa6ff1a6762a457f5c6af6ccfb1614ace884cc19dd82"
+  license "BSD-2-Clause"
+end
