c7f158aba5097e841bf4dd553bea190ed6bf31bf
Kevin Abel <kevin.abel.0@gmail.com>
2021-06-06 14:37:39 -0500

    php@7.4-memprof: add 3.0.0 bottle.

--- Formula/php@7.4-memprof.rb
+++ Formula/php@7.4-memprof.rb
@@ -10,6 +10,13 @@ class PhpAT74Memprof < PhpPeclFormula
   sha256 "3ea040f59f828fe6999ffa6ff1a6762a457f5c6af6ccfb1614ace884cc19dd82"
   license "MIT"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "1dd5b50ab6785923b57b262bf34d2865d5c5bef1a3f456ad67ebda515f39dc67"
+    sha256 catalina: "07c9ed653ffa7d4ea315c86677eadf94e53c8673b56b8cc352a54946957d4d97"
+    sha256 mojave:   "7f223ca03da6df790040fe78e2b7628b23026cd5e80aabb54c8335c6cddd32b7"
+  end
+
   resource "judy" do
     url "https://downloads.sourceforge.net/project/judy/judy/Judy-1.0.5/Judy-1.0.5.tar.gz"
     sha256 "d2704089f85fdb6f2cd7e77be21170ced4b4375c03ef1ad4cf1075bd414a63eb"
