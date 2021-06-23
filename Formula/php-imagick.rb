9b269af8859d52ab786b8d23e08a13ec32905e11
Kevin Abel <kevin.abel.0@gmail.com>
2021-06-23 07:54:20 -0500

    php-imagick: add 3.5.0 bottle.

--- Formula/php-imagick.rb
+++ Formula/php-imagick.rb
@@ -7,5 +7,12 @@ class PhpImagick < PhpPeclFormula
   sha256 "795db7c36fbacd3d33a4f53ff2d38584c846e80a04dcd04c55e9e46c28f5d229"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "139b30baa6d07f706f05ecda10cad0d0423826f60fcbe4b885cccb7687549bba"
+    sha256 catalina: "662ca7df7f20a1ab060c56fe660ec181ac20e4e76318da14d5a9e915c0c7e0e1"
+    sha256 mojave:   "b429430980d40d51e6999d7c8a77b6d6d63759d7c4456b458bf0f6583c7c8314"
+  end
+
   depends_on "imagemagick"
 end
