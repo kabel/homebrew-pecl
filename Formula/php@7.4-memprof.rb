d9bb00af886edef53d7eca2085fcd7a43caa8e47
Kevin Abel <kevin.abel.0@gmail.com>
2021-07-03 10:08:54 -0500

    php-memprof 3.0.1

--- Formula/php@7.4-memprof.rb
+++ Formula/php@7.4-memprof.rb
@@ -6,8 +6,8 @@ class PhpAT74Memprof < PhpPeclFormula
     to find the cause of memory leaks in PHP applications
   EOS
 
-  url "https://pecl.php.net/get/memprof-3.0.0.tgz"
-  sha256 "3ea040f59f828fe6999ffa6ff1a6762a457f5c6af6ccfb1614ace884cc19dd82"
+  url "https://pecl.php.net/get/memprof-3.0.1.tgz"
+  sha256 "00471384e81990e8db9ce9b5e946dad42f313aeff0f1d10a8ce61bb38b02fff5"
   license "MIT"
 
   bottle do
@@ -33,33 +33,4 @@ class PhpAT74Memprof < PhpPeclFormula
     configure_args << "--with-judy-dir=#{prefix}/judy"
     super
   end
-
-  patch :DATA
 end
-
-__END__
-diff --git a/memprof-3.0.0/config.m4 b/memprof-3.0.0/config.m4
-index f3c2fbf..0f4e8cd 100644
---- a/memprof-3.0.0/config.m4
-+++ b/memprof-3.0.0/config.m4
-@@ -70,6 +70,7 @@ if test "$PHP_MEMPROF" != "no"; then
-     AC_DEFINE([HAVE_MALLOC_HOOKS], 1, [Define to 1 if libc supports malloc hooks])
-     AC_MSG_RESULT(yes)
-   ],[
-+    AC_DEFINE([HAVE_MALLOC_HOOKS], 0, [Define to 1 if libc supports malloc hooks])
-     AC_MSG_RESULT(no)
-   ])
- 
-diff --git a/memprof-3.0.0/memprof.c b/memprof-3.0.0/memprof.c
-index d697629..37fae07 100644
---- a/memprof-3.0.0/memprof.c
-+++ b/memprof-3.0.0/memprof.c
-@@ -759,7 +759,7 @@ static char * generate_filename(const char * format) {
- 		slash[0] = DEFAULT_SLASH;
- 	}
- 
--	spprintf(&filename, 0, "%s%smemprof.%s.%ld", output_dir, slash, format, ts);
-+	spprintf(&filename, 0, "%s%smemprof.%s.%lld", output_dir, slash, format, (long long) ts);
- 
- 	return filename;
- }
