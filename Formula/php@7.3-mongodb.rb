dfbc64012162210d6e85938d1221a4bdc4824929
Kevin Abel <kevin.abel.0@gmail.com>
2021-07-24 15:29:41 -0500

    php@7.3-mongodb: update 1.10.0 bottle.

--- Formula/php@7.3-mongodb.rb
+++ Formula/php@7.3-mongodb.rb
@@ -9,9 +9,9 @@ class PhpAT73Mongodb < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "d8886d97fdacfdc017eaefb559df2ddc962ea3e0bd8e7f485d1a779cb464e628"
-    sha256 catalina: "cf36fc3a29f57d3eb3e88c443006234434ee62d483b4bb895413868b8d742b38"
-    sha256 mojave:   "36088fc0f64ee000a3cc50726cc4dbaf18a099de9d85481b206c8d7a8a4e01c9"
+    sha256 big_sur:  "50fa097344d098e5f78d21ae0552286747b0165c532415511ff947255e76b9cc"
+    sha256 catalina: "8184095a03fe5248f1139f9f0924ecf84ec9cfebf0181d7f66dd8768d0ebb7bd"
+    sha256 mojave:   "41c1e139c2e8bd4edba8720f68af600173c1c762df44fa25adbc1998c567fff2"
   end
 
   depends_on "icu4c"
