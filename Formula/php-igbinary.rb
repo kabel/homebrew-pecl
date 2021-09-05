3e20531364cc3b5d2c796f8342d5a4c3f4f121a3
Kevin Abel <kevin.abel.0@gmail.com>
2021-09-05 15:07:50 -0500

    php-igbinary 3.2.6

--- Formula/php-igbinary.rb
+++ Formula/php-igbinary.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpIgbinary < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
-  url "https://pecl.php.net/get/igbinary-3.2.4.tgz"
-  sha256 "c39147f1548b7d9883468e9d12be6cae84466e09baa24199e4e7a66993ef60bb"
+  url "https://pecl.php.net/get/igbinary-3.2.6.tgz"
+  sha256 "ca32e395ecdb4447a69d5ea51b35feb19c3bc5759b46896fef6f7e2ca41a8e43"
   license "BSD-3-Clause"
 
   bottle do
