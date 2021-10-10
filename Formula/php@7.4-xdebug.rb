cc990df74f0cfa1ad6bb99a1da00f765ddd0207a
Kevin Abel <kevin.abel.0@gmail.com>
2021-10-10 16:10:34 -0500

    php@7.4-xdebug: update 3.1.0 bottle.

--- Formula/php@7.4-xdebug.rb
+++ Formula/php@7.4-xdebug.rb
@@ -9,9 +9,9 @@ class PhpAT74Xdebug < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "e6ae51b89b08640e3b9c34f6da06e452db36f7b0adc261303f0d6d89120ab3e0"
-    sha256 catalina: "808aeefdea86aeca851aa9fe300c5eea6b256a4b5e838276423086b161a32b21"
-    sha256 mojave:   "9b73081a0e6041e31667caf3dc4df43fc2bc3db9a12dd3d271e7784f1149416c"
+    sha256 big_sur:  "56b1033bad0e97c1b1c8c2fc9c9258faa5cbf98045f26329b16d95e3b19110ba"
+    sha256 catalina: "1fe617a600398bac40f5e5645b12a26786f319466b28c34899e85ce762fb8c5c"
+    sha256 mojave:   "682b4300ae1c182135f44a94da7ee5d0bfef1729bbc00340be35a88f9e108f35"
   end
 
   def extension_type
