ecb3c7ee115b5e6bfa1ca8d20992038f1571c8bf
Kevin Abel <kevin.abel.0@gmail.com>
2021-11-06 16:39:45 -0500

    php-mongodb 1.11.1

--- Formula/php@7.4-mongodb.rb
+++ Formula/php@7.4-mongodb.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT74Mongodb < PhpPeclFormula
   extension_dsl "MongoDB driver"
 
-  url "https://pecl.php.net/get/mongodb-1.10.0.tgz"
-  sha256 "8033dce1a5a5139a4bda690c15c2c98beb18c996429da6a17796dd0c4fc26a73"
+  url "https://pecl.php.net/get/mongodb-1.11.1.tgz"
+  sha256 "838a5050de50d51f959026bd8cec7349d8af37058c0fe07295a0bc960a82d7ef"
   license "Apache-2.0"
 
   bottle do
