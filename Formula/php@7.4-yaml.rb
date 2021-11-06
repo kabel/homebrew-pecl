114282a3acab9bc6e45b23de927f0080cd38ded6
Kevin Abel <kevin.abel.0@gmail.com>
2021-11-06 16:43:16 -0500

    php-yaml 2.2.2

--- Formula/php@7.4-yaml.rb
+++ Formula/php@7.4-yaml.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT74Yaml < PhpPeclFormula
   extension_dsl "YAML 1.1 serialization"
 
-  url "https://pecl.php.net/get/yaml-2.2.1.tgz"
-  sha256 "e17ad04e752e25fd099bddd2df9d26dfef183c8d00c4179bc9d7a2e1c97d7819"
+  url "https://pecl.php.net/get/yaml-2.2.2.tgz"
+  sha256 "119052f0461d57d86f44c252f9c9b2dd743486c701c1a0aba0aebecdd0d8b82a"
   license "MIT"
 
   bottle do
