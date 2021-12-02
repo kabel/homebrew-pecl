239a37b9ca6e9499d795ce4be1a631cd1385080c
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:52 -0600

    php-xhprof: update 2.3.5_1 bottle.

--- Formula/php-xhprof.rb
+++ Formula/php-xhprof.rb
@@ -10,9 +10,8 @@ class PhpXhprof < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, big_sur:  "537ada7c55c361e1a77d2ddc71f59f63830f6403a79635e4cb8404b9a714b775"
-    sha256 cellar: :any_skip_relocation, catalina: "90fd466dfaeafe2618484b4f566cddb32dd902bac03bc0d6fda1f738c4a86019"
-    sha256 cellar: :any_skip_relocation, mojave:   "9f0bb54b8d93c419da45443772075d12559b78615e8fca7a5da5fffc2c44ca04"
+    sha256 cellar: :any_skip_relocation, big_sur:  "b3207ac1023fb64ac257efa558d2113f11ef2f65b105aea46b22d2fd43e45b48"
+    sha256 cellar: :any_skip_relocation, catalina: "bc8ae0e643cc9473c66b35802e38218055c57bac800c38930d967152f674eca9"
   end
 
   def source_dir
