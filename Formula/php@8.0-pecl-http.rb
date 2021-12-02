70b1efcadd93842a8e6bec146b870b15709d2ada
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:55 -0600

    php@8.0-pecl-http: add 4.2.1 bottle.

--- Formula/php@8.0-pecl-http.rb
+++ Formula/php@8.0-pecl-http.rb
@@ -7,6 +7,12 @@ class PhpAT80PeclHttp < PhpPeclFormula
   sha256 "ab07d55ccdfa897c897c26f1c0851b557b363431766c9ea4c799ea61443bdcdc"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "2de9dbcb46cb3dd0e1b99acb884a0352b12ea8fc4e05e6f624a6b480f9f917bf"
+    sha256 catalina: "822593b11418bb2383d7af0f5b9aaff0d68aace1ad967eb77e479edebd3234ce"
+  end
+
   depends_on "brotli"
   depends_on "curl"
   depends_on "icu4c"
