1849aa732b2c33bdcdfa9fda33b9bfa49d5c5879
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:50 -0600

    php-memcache: update 8.0_2 bottle.

--- Formula/php-memcache.rb
+++ Formula/php-memcache.rb
@@ -10,9 +10,8 @@ class PhpMemcache < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, big_sur:  "68b626e8eea31e1ba55351ea2ee38ef07f2d355373644011b404080477a50cd2"
-    sha256 cellar: :any_skip_relocation, catalina: "400a95f241da88dadd982b723181ec3fd372e8c21016744d72bf3df39c633d19"
-    sha256 cellar: :any_skip_relocation, mojave:   "c6a295970aec777d57b5851d2187770a167aca7a29bc7b330699c3c972d8ec8f"
+    sha256 cellar: :any_skip_relocation, big_sur:  "fe06addbfb7336f207927ab199c8c022429167f0ef9f1ee0fd98644699a20b8e"
+    sha256 cellar: :any_skip_relocation, catalina: "411e4f635e0dce556181a17cc68cb7d47486f609b70f9acc3211063d1cad3cf7"
   end
 
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
