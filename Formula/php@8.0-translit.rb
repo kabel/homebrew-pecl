d5559209b451690ebceacecf30f3e5c215733aca
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:57 -0600

    php@8.0-translit: add 0.7.1 bottle.

--- Formula/php@8.0-translit.rb
+++ Formula/php@8.0-translit.rb
@@ -6,4 +6,10 @@ class PhpAT80Translit < PhpPeclFormula
   url "https://pecl.php.net/get/translit-0.7.1.tgz"
   sha256 "60a914a64416381c8e2b00ccf13188a872bca3e80cc6cc4ab6d7f0af949365b8"
   license "BSD-2-Clause"
+
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, big_sur:  "c0b09314ebb9b4cacbdd486ca974923398beacc9c1e1ef6cda4e40a4dee7954a"
+    sha256 cellar: :any_skip_relocation, catalina: "3a503da2dc764687fcc1f1530d08d8933816cb46fabb347f3b93d3efd0b74bc1"
+  end
 end
