ddeb8880aaeab5d432a020a50fd05ccd7239f017
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:55 -0600

    php@8.0-memcached: add 3.1.5 bottle.

--- Formula/php@8.0-memcached.rb
+++ Formula/php@8.0-memcached.rb
@@ -7,6 +7,12 @@ class PhpAT80Memcached < PhpPeclFormula
   sha256 "b288e45f839593e16c4d8508b241de51a86df4f7322153e6becb9e1c819021fd"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "0a3aac858bfb39bd0a92d40b777d2c0afcdf8850ccac197349a8598865f0b93c"
+    sha256 catalina: "444ee72d69449e4e2d988be320a87fca0fd0c60ecfcf4461ef0f330335217f99"
+  end
+
   depends_on "libmemcached"
   depends_on "kabel/pecl/php@8.0-igbinary"
   depends_on "kabel/pecl/php@8.0-msgpack"
