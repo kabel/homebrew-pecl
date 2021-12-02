4c7b3c352fec667e9a974dd375cd088243651aac
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:56 -0600

    php@8.0-redis: add 5.3.4 bottle.

--- Formula/php@8.0-redis.rb
+++ Formula/php@8.0-redis.rb
@@ -7,6 +7,12 @@ class PhpAT80Redis < PhpPeclFormula
   sha256 "4b39056f3356f135255e059efd76398d1c7607c86bd52513e6341bb07a3fb4cc"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "2a1d45c6f135d7464918566a10479cfa8d2d8a95911f38c4d46fc18601edc53b"
+    sha256 catalina: "dc9b36e77a79b93d562f15c603041e16ef0621503fbb18ba4d53b3439596bd2a"
+  end
+
   depends_on "lz4"
   depends_on "kabel/pecl/php@8.0-igbinary"
   depends_on "kabel/pecl/php@8.0-msgpack"
