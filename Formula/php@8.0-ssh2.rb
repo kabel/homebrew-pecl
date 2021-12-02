26f16c3fc565e10633b3a153deafb8d4aee5545f
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:56 -0600

    php@8.0-ssh2: add 1.3.1 bottle.

--- Formula/php@8.0-ssh2.rb
+++ Formula/php@8.0-ssh2.rb
@@ -7,6 +7,12 @@ class PhpAT80Ssh2 < PhpPeclFormula
   sha256 "9093a1f8d24dc65836027b0e239c50de8d5eaebf8396bc3331fdd38c5d69afd9"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "f8f4482f9057b490f048f49b9e59f3c4bddac18dc5408f5b55ebb6a67a29ae83"
+    sha256 catalina: "4ab3820112470ca165db01df972547661f6fc3e30e466aabc4b62c4332689e7b"
+  end
+
   depends_on "libssh2"
 
   configure_arg "--with-ssh2=#{Formula["libssh2"].opt_prefix}"
