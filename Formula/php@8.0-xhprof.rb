5e769e90c7ddbe6371e69c5f5771f9a30951e755
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:58 -0600

    php@8.0-xhprof: add 2.3.5 bottle.

--- Formula/php@8.0-xhprof.rb
+++ Formula/php@8.0-xhprof.rb
@@ -7,6 +7,12 @@ class PhpAT80Xhprof < PhpPeclFormula
   sha256 "f3b93619e038abe2bbed5f2898cd9c024d6ea1d34eace217ccafc0de7ad50e23"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, big_sur:  "3608f56c62e100f0e8b1b543183959bdd3040f9d7966e83194f507591693a07c"
+    sha256 cellar: :any_skip_relocation, catalina: "ad4bf1c09d088a8d67b4009b35720523739cdcd3bc1e89eee1c1becaac7d6da4"
+  end
+
   def source_dir
     "#{extension}-#{version}/extension"
   end
