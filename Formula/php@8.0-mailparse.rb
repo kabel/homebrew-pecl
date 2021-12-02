294d5709c0f318bc5354682851825bb3eb6eda11
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:54 -0600

    php@8.0-mailparse: add 3.1.2 bottle.

--- Formula/php@8.0-mailparse.rb
+++ Formula/php@8.0-mailparse.rb
@@ -6,4 +6,10 @@ class PhpAT80Mailparse < PhpPeclFormula
   url "https://pecl.php.net/get/mailparse-3.1.2.tgz"
   sha256 "b0647ab07ea480fcc13533368e38fdb4f4bb45d30dce65fc90652a670a4f4010"
   license "PHP-3.01"
+
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, big_sur:  "b767bc78aade1258db82e3ae32227d9c45aa9832f0c2682c4f46b85df6fd86db"
+    sha256 cellar: :any_skip_relocation, catalina: "9dbedb3aaede964e7800cb47d6bd7e363da3a56a99fb26323aa46242c47ade7a"
+  end
 end
