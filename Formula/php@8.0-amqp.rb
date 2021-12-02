69f3115a50113735287e05415acc83c22d29f786
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:53 -0600

    php@8.0-amqp: add 1.11.0 bottle.

--- Formula/php@8.0-amqp.rb
+++ Formula/php@8.0-amqp.rb
@@ -7,5 +7,11 @@ class PhpAT80Amqp < PhpPeclFormula
   sha256 "dc5212b4785f59955118a219bbfbcedb7aa6ab2a91e8038a0ad1898f331c2f08"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "72005bc2713a940c1ea1e635a31f661826fc6290082b717cd3476ce61974e6b6"
+    sha256 catalina: "3e77988ef6a99621279005d8bfc3f7e5ee136b3e4cfba2e73e3fea7672632df2"
+  end
+
   depends_on "rabbitmq-c"
 end
