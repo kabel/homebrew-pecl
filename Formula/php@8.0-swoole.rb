ba97d81e1861cf6c4e4a245ac755d95d336514a1
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:57 -0600

    php@8.0-swoole: add 4.8.1 bottle.

--- Formula/php@8.0-swoole.rb
+++ Formula/php@8.0-swoole.rb
@@ -7,6 +7,12 @@ class PhpAT80Swoole< PhpPeclFormula
   sha256 "fe3699431cfe62c57eeb9af3ddd30d4232fdec454f8b95920c244f13564b8609"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "90fb486fb87e60d89fee6f681911a349a853614f22df8838cc1e647f4ce431c7"
+    sha256 catalina: "ac7f5ae82f5220cf50460f31aa69c5fa0cc28c3dcc355f3a7650f80d7828bf28"
+  end
+
   depends_on "openssl@1.1"
   depends_on "brotli"
 
