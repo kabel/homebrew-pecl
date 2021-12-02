b9c8c2c70ca57072e3b003d3190f161859c8cb90
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:56 -0600

    php@8.0-protobuf: add 3.19.1 bottle.

--- Formula/php@8.0-protobuf.rb
+++ Formula/php@8.0-protobuf.rb
@@ -6,4 +6,10 @@ class PhpAT80Protobuf < PhpPeclFormula
   url "https://pecl.php.net/get/protobuf-3.19.1.tgz"
   sha256 "9003cd3e7bdb0ab9862133371e3a6c9fad9301257c78d0a2cc537cfb5f88e9b6"
   license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, big_sur:  "fe1cae62b6ec37363b444ccecc1acb2374d75da2585cc6a8ae54f196fcbfbea9"
+    sha256 cellar: :any_skip_relocation, catalina: "49a60b5508aecc3dc72a9c381fb014f38e24b2e7e994016c687d8ac1cdadf4fb"
+  end
 end
