175f3d2bfd1a71b6cacdc6f8e61b5ab75d8e6838
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:50:57 -0600

    php@8.0-uuid: add 1.2.0 bottle.

--- Formula/php@8.0-uuid.rb
+++ Formula/php@8.0-uuid.rb
@@ -7,6 +7,12 @@ class PhpAT80Uuid < PhpPeclFormula
   sha256 "5cb834d32fa7d270494aa47fd96e062ef819df59d247788562695fd1f4e470a4"
   license "LGPL-2.1-only"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 big_sur:  "7b0cd48a4c8f85565aa8cf5fc15c568ee4ae8f9ae05e55d90e7438e0d594ec6d"
+    sha256 catalina: "541253adc89526ed1735d402fc96bac663b11fd1dda949dca5f868030efa041c"
+  end
+
   depends_on "e2fsprogs"
 
   configure_arg "--with-uuid=#{Formula["e2fsprogs"].opt_prefix}"
