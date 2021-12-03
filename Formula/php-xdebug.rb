42452bfebe2d8e4fca41f372e228086982f940dd
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 18:24:27 -0600

    php-xdebug: update 3.1.2 bottle.

--- Formula/php-xdebug.rb
+++ Formula/php-xdebug.rb
@@ -9,8 +9,8 @@ class PhpXdebug < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "7ca777a52a00ffc9cbdec9c8ca28f193f9c8f566598bc2572ad79ea3293fd72d"
-    sha256 catalina: "4ba4c7daaf9919285d8806931daa8567760b7c103ffba7aa17b31bc46264a5a9"
+    sha256 big_sur:  "4370d8107081f4776afd4746617371d8935691f35ed2b89933ba7ee379bc09e9"
+    sha256 catalina: "6693f4f8682b7a5e7448a0e51c4da6557e5aa8c1feb32c37a4864ba246cb7beb"
   end
 
   def extension_type
