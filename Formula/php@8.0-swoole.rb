b59a9a9677b3ac0bd7b0745cc521f5b3a8def223
Ben Yan <i@yanbin.org>
2022-01-10 02:14:36 +0800

    update swoole to 4.8.5

--- Formula/php@8.0-swoole.rb
+++ Formula/php@8.0-swoole.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT80Swoole< PhpPeclFormula
   extension_dsl "Event-driven asynchronous and concurrent networking engine with high performance"
 
-  url "https://pecl.php.net/get/swoole-4.8.3.tgz"
-  sha256 "39afd29e698c73f2995f23ffa97f414fb575c4f6b811625afc650d9e0a40e29c"
+  url "https://pecl.php.net/get/swoole-4.8.5.tgz"
+  sha256 "14275af75bceefd6353b53e88efef5d152f3c305543c7c27d0ed649623b228f8"
   license "Apache-2.0"
 
   bottle do
