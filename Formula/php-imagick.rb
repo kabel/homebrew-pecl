5bfa0cc15f68e262dd0b064ec93a2097b2f53376
Kevin Abel <kevin.abel.0@gmail.com>
2022-01-22 14:48:43 -0600

    php-imagick 3.7.0

--- Formula/php-imagick.rb
+++ Formula/php-imagick.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpImagick < PhpPeclFormula
   extension_dsl "Create and modify images using the ImageMagick API"
 
-  url "https://pecl.php.net/get/imagick-3.6.0.tgz"
-  sha256 "4e2965f2d70dd59a40e7957d56e590e731cad2669e9f89e0fca159d748d2947e"
+  url "https://pecl.php.net/get/imagick-3.7.0.tgz"
+  sha256 "5a364354109029d224bcbb2e82e15b248be9b641227f45e63425c06531792d3e"
   license "PHP-3.01"
 
   bottle do
