c1c831090742fedc56615517a4bf3ad45a8f0b6d
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-09 15:49:58 -0600

    php-pdo-sqlsrv 5.10.0

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpPdoSqlsrv < PhpPeclFormula
   extension_dsl "Microsoft Drivers for SQL Server"
 
-  url "https://pecl.php.net/get/pdo_sqlsrv-5.9.0.tgz"
-  sha256 "0fce417b33879fdae3d50cc1aa5b284ab12662147ea2206fa6e1fadde8b48c58"
+  url "https://pecl.php.net/get/pdo_sqlsrv-5.10.0.tgz"
+  sha256 "0446bb8bff1e82fcfd993ebd765d1dc56715a3e754453f43ce8e9fe84765112a"
   license "MIT"
 
   depends_on "unixodbc"
