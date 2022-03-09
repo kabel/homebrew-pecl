227479d3a9a727c369732d8cf514f5a7fdf51c64
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-09 15:48:32 -0600

    php-sqlsrv 5.10.0

--- Formula/php-sqlsrv.rb
+++ Formula/php-sqlsrv.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpSqlsrv < PhpPeclFormula
   extension_dsl "Microsoft Drivers for SQL Server"
 
-  url "https://pecl.php.net/get/sqlsrv-5.9.0.tgz"
-  sha256 "0a108e0408e8b984e5ae8bc52824ed32872d72e3a571cd2a5d2b63b200215ab3"
+  url "https://pecl.php.net/get/sqlsrv-5.10.0.tgz"
+  sha256 "76b3f083a1a8f103581c21ba3a46d6ca257beae6728cd158a64a46ab59a226b4"
   license "MIT"
 
   depends_on "unixodbc"
