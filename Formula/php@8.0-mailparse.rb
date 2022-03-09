7989b1be92c93e5b7c0e886a9fc1f668c0a1905f
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-09 15:55:09 -0600

    php-mailparse 3.1.3

--- Formula/php@8.0-mailparse.rb
+++ Formula/php@8.0-mailparse.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT80Mailparse < PhpPeclFormula
   extension_dsl "Email message manipulation"
 
-  url "https://pecl.php.net/get/mailparse-3.1.2.tgz"
-  sha256 "b0647ab07ea480fcc13533368e38fdb4f4bb45d30dce65fc90652a670a4f4010"
+  url "https://pecl.php.net/get/mailparse-3.1.3.tgz"
+  sha256 "8659ca62dc9a4d7d15f07f97a0e2142cb58251c8e772cd36669ec740d2292471"
   license "PHP-3.01"
 
   bottle do
