3cd534baa88e964d3a07ec2572de5741e6273dbe
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-10 14:08:51 -0600

    php-pdo-sqlsrv: add 5.10.0 bottle.

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -7,5 +7,11 @@ class PhpPdoSqlsrv < PhpPeclFormula
   sha256 "0446bb8bff1e82fcfd993ebd765d1dc56715a3e754453f43ce8e9fe84765112a"
   license "MIT"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, big_sur:  "8a3d24dd70cd170ae60ba4f079626746f2383e9211ed63c1920f320dd74c72cf"
+    sha256 cellar: :any, catalina: "de849f15660c8a04dd6ab3ed4dda31f376e38b8ac5b5e2ed6e1cbdc03bf27e16"
+  end
+
   depends_on "unixodbc"
 end
