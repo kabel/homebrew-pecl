54c85550b63c4324ce172de112e6df0ddb5b83f2
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-09 21:29:52 -0600

    php-sqlsrv: add 5.10.0 bottle.

--- Formula/php-sqlsrv.rb
+++ Formula/php-sqlsrv.rb
@@ -7,5 +7,11 @@ class PhpSqlsrv < PhpPeclFormula
   sha256 "76b3f083a1a8f103581c21ba3a46d6ca257beae6728cd158a64a46ab59a226b4"
   license "MIT"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, big_sur:  "0d48ca5e9d61a930b0ed966d40eafaca4348c70e748fa0296a496d0c7234f58a"
+    sha256 cellar: :any, catalina: "f60edfe4776036b471fd7064abfbbe0a91859cd1c401cb8ee27d3b44f5319c26"
+  end
+
   depends_on "unixodbc"
 end
