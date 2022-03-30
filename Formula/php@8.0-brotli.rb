f2890dc0b71ad50f301366d0400cc0d41e21602b
Florian Weber <florian.weber@ueberbit.de>
2022-03-30 11:54:11 +0200

    Add brotli extension

---
+++ Formula/php@8.0-brotli.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT80Brotli < PhpPeclFormula
+  extension_dsl "Brotli compression"
+  homepage "https://github.com/kjdev/php-ext-brotli"
+
+  url "https://github.com/kjdev/php-ext-brotli/archive/refs/tags/0.13.1.tar.gz"
+  sha256 "1eca1af3208e2f6551064e3f26e771453def588898bfc25858ab1db985363e47"
+  license "MIT"
+
+  depends_on "brotli"
+
+  configure_arg %w[
+    --with-libbrotli
+  ]
+end
