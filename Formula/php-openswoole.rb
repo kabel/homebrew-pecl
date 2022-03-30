06fb027f118a82b3015bc6c0d31888a63e28a0e7
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-30 15:16:21 -0500

    php-openswoole: add 4.11.0 bottle.

--- Formula/php-openswoole.rb
+++ Formula/php-openswoole.rb
@@ -7,6 +7,12 @@ class PhpOpenswoole< PhpPeclFormula
   sha256 "f1840dedec436d51872940fd7b46cfac97db1ee6efdc96fdad9a823df8dae768"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, big_sur:  "70048603693c4d2254729b6c7ee9f15422e64ba83384609c5b1e1b099f744da6"
+    sha256 cellar: :any, catalina: "499d950afc8eb034ebdd964ce39691309df540bcfd6dfe05a9fe7717501c2af5"
+  end
+
   depends_on "openssl@1.1"
   depends_on "brotli"
 
