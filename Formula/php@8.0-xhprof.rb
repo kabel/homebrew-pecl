932b181dd73529644718192447530032763502bf
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:54 -0500

    php@8.0-xhprof: update 2.3.5 bottle.

--- Formula/php@8.0-xhprof.rb
+++ Formula/php@8.0-xhprof.rb
@@ -9,6 +9,7 @@ class PhpAT80Xhprof < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "f88c51a58c7bb6e2a3c27b966fa7f14b92cdff392a80b81dab9fa8d000ae2933"
     sha256 cellar: :any_skip_relocation, big_sur:  "3608f56c62e100f0e8b1b543183959bdd3040f9d7966e83194f507591693a07c"
     sha256 cellar: :any_skip_relocation, catalina: "ad4bf1c09d088a8d67b4009b35720523739cdcd3bc1e89eee1c1becaac7d6da4"
   end
