72f1e9015a3bd00892161c9d8216f0f72724cdda
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:52 -0500

    php@8.0-mongodb: update 1.13.0 bottle.

--- Formula/php@8.0-mongodb.rb
+++ Formula/php@8.0-mongodb.rb
@@ -9,6 +9,7 @@ class PhpAT80Mongodb < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "c4b8f779f5950a92736a233417f911da87aac04dcc405f081fe8a91b35d6ba60"
     sha256 cellar: :any, big_sur:  "aba65fb5e2beddc3c8d1156089145b3657cd50f70c596d767aa2f16d46c572a2"
     sha256 cellar: :any, catalina: "ad138a6a09d57a0ae55e45e151070712249a9eb3fc9d56a6db656c33469f57c0"
   end
