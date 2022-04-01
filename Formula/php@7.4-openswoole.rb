1187dd7e256ec4c3862d14ef0dfabc741610622e
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:27:16 -0500

    php@7.4-openswoole: update 4.11.0 bottle.

--- Formula/php@7.4-openswoole.rb
+++ Formula/php@7.4-openswoole.rb
@@ -9,6 +9,7 @@ class PhpAT74Openswoole< PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "44d8b9f7f0faf419a39b83986f716c86d4664717dc01b4a91a7bdd904d2cac45"
     sha256 cellar: :any, big_sur:  "fb1f51133b6f48be0ccea9a8c777626bd33f40d4655ce2e3c3358999fde0d07d"
     sha256 cellar: :any, catalina: "d83a47a8646829ab2f2c629cc431742f736ea10e21619b5d92d1f3866ed83e8e"
   end
