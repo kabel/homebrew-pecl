d5eca135d685e60493b1298c23db571db34d8f71
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:53 -0500

    php@8.0-sqlsrv: update 5.10.0 bottle.

--- Formula/php@8.0-sqlsrv.rb
+++ Formula/php@8.0-sqlsrv.rb
@@ -9,6 +9,7 @@ class PhpAT80Sqlsrv < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "bfa2d492f971cfff6097a7237356ece4f10da60dd3e07ec31cb439e78af78d96"
     sha256 cellar: :any, big_sur:  "ffd6f02ff4d5afb66d5e9387cdcdabd08bdaedc59a667b9eab930bd87e34ce43"
     sha256 cellar: :any, catalina: "b022c6a017d8fee16d46f5e4d523e4b1c84cc171ef32db73a9058715f120d8d5"
   end
