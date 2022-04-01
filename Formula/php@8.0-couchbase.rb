8a147c6bb7d89299451a967a04be45f8a9c8e4d6
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:50 -0500

    php@8.0-couchbase: update 3.2.2 bottle.

--- Formula/php@8.0-couchbase.rb
+++ Formula/php@8.0-couchbase.rb
@@ -9,6 +9,7 @@ class PhpAT80Couchbase < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "b1f6906dac590bf90d1a563f088a4a4378fe5369d9f7651f5ac6b59bf9fd3b4a"
     sha256 cellar: :any, big_sur:  "0d9881eab7ccdee9fd61d4bb34ecd1b9d645eb49baa3459f341cfacf9fdf2f04"
     sha256 cellar: :any, catalina: "92c15f65273dcbf7bd6749915f74ccc2bad9b980c53f513049e2859aebd7d706"
   end
