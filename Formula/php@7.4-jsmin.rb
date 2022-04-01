0b161bdeef353b40cb360dfb3c681ee9d718b01d
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:27:15 -0500

    php@7.4-jsmin: update 3.0.0 bottle.

--- Formula/php@7.4-jsmin.rb
+++ Formula/php@7.4-jsmin.rb
@@ -8,6 +8,7 @@ class PhpAT74Jsmin < PhpPeclFormula
   license "PHP-3.01"
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "e91ed06a63291ae844032e1a7d3826e00daab804b6eb33827ee54d5fd40e7823"
     sha256 cellar: :any_skip_relocation, catalina: "e7ea1b6e9b04513cb58ba113badb1805d8cb7c4ee01a5a62efc894878d7923fa"
     sha256 cellar: :any_skip_relocation, mojave:   "d96bd4ba5446d12747897c8e894f6415a27edea77e82db937393f3d9949f184e"
   end
