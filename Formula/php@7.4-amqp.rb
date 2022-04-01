9bf025e9c51ec250a7958d60536b1aced554fa0d
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:27:14 -0500

    php@7.4-amqp: update 1.11.0 bottle.

--- Formula/php@7.4-amqp.rb
+++ Formula/php@7.4-amqp.rb
@@ -9,8 +9,9 @@ class PhpAT74Amqp < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "1257d6499baf2ffb5af5da68b056808193182f9a037500c2620259d9d335820d"
-    sha256 catalina: "4a5e637c175e1f4284e306f43607cac722f247307d11a2bb05fa2046a94758d9"
+    sha256 cellar: :any, monterey: "aff9f99ae975fcf72e52fb28b2edcdbd6e1f8fc5be6eabfb66f2eb29c6b0a921"
+    sha256               big_sur:  "1257d6499baf2ffb5af5da68b056808193182f9a037500c2620259d9d335820d"
+    sha256               catalina: "4a5e637c175e1f4284e306f43607cac722f247307d11a2bb05fa2046a94758d9"
   end
 
   depends_on "rabbitmq-c"
