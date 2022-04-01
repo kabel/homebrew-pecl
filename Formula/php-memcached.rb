9966525eb61752baec4e637b825247a1500b24a7
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 09:19:54 -0500

    php-memcached: update 3.2.0 bottle.

--- Formula/php-memcached.rb
+++ Formula/php-memcached.rb
@@ -9,6 +9,7 @@ class PhpMemcached < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "c7c3422fe7c10039bec610bddf4f6520266748a7453fecc4dcf5b0568ba7804e"
     sha256 cellar: :any, big_sur:  "d6bd6721aca933e209e8ed3dbbc285b3509031a31ea6021bde27b3c7c2fbf19d"
     sha256 cellar: :any, catalina: "fa175c08acfb7580c9c64a7d29793f3a929cfdde9bc0b6a6e24eb5959d633fc4"
   end
