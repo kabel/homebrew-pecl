940fa90a401bc3aab2efddd398a76eb6a3dce4c2
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:50 -0500

    php@8.0-brotli: update 0.13.1 bottle.

--- Formula/php@8.0-brotli.rb
+++ Formula/php@8.0-brotli.rb
@@ -10,6 +10,7 @@ class PhpAT80Brotli < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "e4e86e4968a4b08d853933c3cf145854874bcba56e36fd922e65ce03e78ab0d3"
     sha256 cellar: :any, big_sur:  "2ca82be8c77a97fd897cb48ba73e67fc1d2cead1fc2664c3c596cf7df4b82e1b"
     sha256 cellar: :any, catalina: "06eecf32d72e0c033321720f1669162e6e606ba0f9f48d9276b0bd4dc7a108bc"
   end
