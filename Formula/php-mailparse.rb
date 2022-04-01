cb7009c9637c489f973d5140e217a50fbbd9f6ff
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-31 23:05:47 -0500

    php-mailparse: update 3.1.3 bottle.

--- Formula/php-mailparse.rb
+++ Formula/php-mailparse.rb
@@ -9,6 +9,7 @@ class PhpMailparse < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "40489ba4a9339d61e0a0962e7c6be5d6337023ec9be1214a9cfe5fc14bc9d314"
     sha256 cellar: :any_skip_relocation, big_sur:  "e913b038b66c606b18521a310de68a9d605d8699ff4bd894538f57eae4bdb879"
     sha256 cellar: :any_skip_relocation, catalina: "675de8fd497645974a9130b3d11e18e30792b68c67a1f7e1b697b50f23cd6eed"
   end
