376cc9dcee01a3838f9f032974d5b9c37314dc64
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:27:18 -0500

    php@7.4-translit: update 0.7.1 bottle.

--- Formula/php@7.4-translit.rb
+++ Formula/php@7.4-translit.rb
@@ -9,6 +9,7 @@ class PhpAT74Translit < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "507048431a2f22cfa086ec0b19a42689de8542e9add6f86b9896dd6a1c0ad1f1"
     sha256 cellar: :any_skip_relocation, catalina: "bea357284607b265d0dbd311c1d93ebaea9ef9107f4f44eab99b62a49bc27bf0"
     sha256 cellar: :any_skip_relocation, mojave:   "5fe1ae70271aa8ca8e142c189775a08dbcbe80ba3a4bbce50a3e903d522a1ab2"
   end
