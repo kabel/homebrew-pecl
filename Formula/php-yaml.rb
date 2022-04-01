4bcdb3154e2340a64761baa632428331d9f65d7e
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-31 23:05:50 -0500

    php-yaml: update 2.2.2_1 bottle.

--- Formula/php-yaml.rb
+++ Formula/php-yaml.rb
@@ -10,6 +10,7 @@ class PhpYaml < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "e5096cc7207b5d515ab8647d8a3f2a7813b8179759379c863470cff70f1f53d1"
     sha256 cellar: :any, big_sur:  "7ba43e3946a19ed121a6f7aef0590f5ed62aa87aee6ac0b3e46407a49ddceb2f"
     sha256 cellar: :any, catalina: "a5b8d12a22a97e0fdfb2b947ba547ffaaca214019066a10ef5a612110e95e31b"
   end
