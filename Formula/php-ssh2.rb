5331d1a43d2fff0d8144a0603e9c9cf8551e7980
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-31 23:05:49 -0500

    php-ssh2: update 1.3.1_1 bottle.

--- Formula/php-ssh2.rb
+++ Formula/php-ssh2.rb
@@ -10,8 +10,9 @@ class PhpSsh2 < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 big_sur:  "5a0ce2621dc22467d244e3f5f6929bbc69eaededa412fbdf1d6e1067a3d64388"
-    sha256 catalina: "081fa9581c0638a1d0e7ffcb706d611e7b3a1928bf525bc996cb9089eaf066ca"
+    sha256 cellar: :any, monterey: "63cb5f7afa51a7b29d44924482c7283e87740be6b912624b6ccae256d922dbf3"
+    sha256               big_sur:  "5a0ce2621dc22467d244e3f5f6929bbc69eaededa412fbdf1d6e1067a3d64388"
+    sha256               catalina: "081fa9581c0638a1d0e7ffcb706d611e7b3a1928bf525bc996cb9089eaf066ca"
   end
 
   depends_on "libssh2"
