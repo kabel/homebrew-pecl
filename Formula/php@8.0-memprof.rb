2512fa04fb603e98b8f56d1bbecb16e1854ffc46
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:52 -0500

    php@8.0-memprof: update 3.0.2 bottle.

--- Formula/php@8.0-memprof.rb
+++ Formula/php@8.0-memprof.rb
@@ -12,6 +12,7 @@ class PhpAT80Memprof < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "1ceea05187e659ed1d6f7e3a7902319f1367d8c140a1781c4a6f8a1719dce50c"
     sha256 cellar: :any, big_sur:  "7713cc74515ac064c6e5895e7c2c6d5c408e8031625f073387f05478c7fa1ea7"
     sha256 cellar: :any, catalina: "b29182c65cf1a71c9821ccaf15dcfe0f886e89088bd52c25b6b2a99f69a42a1d"
   end
