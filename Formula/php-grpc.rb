f5d7c3c66cbcff505d6c81333dfd80b5bac53716
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-31 23:05:46 -0500

    php-grpc: update 1.45.0 bottle.

--- Formula/php-grpc.rb
+++ Formula/php-grpc.rb
@@ -9,6 +9,7 @@ class PhpGrpc < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "404ad6fb03c0cf5a56f87a7a968a4e94d4751a80cc71adf6ddb3793a436402fc"
     sha256 cellar: :any_skip_relocation, big_sur:  "fba8552db720063a8d631918880dddffe3d1123a2076a7547afa438891f42cf7"
     sha256 cellar: :any_skip_relocation, catalina: "4d9c1edb871b7627172656b640ecf707ae69b7918d6b8609bc27888b4cf72d81"
   end
