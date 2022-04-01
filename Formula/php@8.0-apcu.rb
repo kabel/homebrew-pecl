590ef3fa3c38ee35de12047588cfbe79cfb08f70
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 08:26:50 -0500

    php@8.0-apcu: update 5.1.21 bottle.

--- Formula/php@8.0-apcu.rb
+++ Formula/php@8.0-apcu.rb
@@ -9,6 +9,7 @@ class PhpAT80Apcu < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "18783c29172414af8311ea49a6b85d92b40283c4c07e4859a350a0c28162a91c"
     sha256 cellar: :any_skip_relocation, big_sur:  "8a88e4e6b5f8225355462d788190cd7134d6bb47d9fb1149c94e2bebd01dc21a"
     sha256 cellar: :any_skip_relocation, catalina: "14f55a374b1eff983c5ccb5fb6716f17a802ff0c0c598a0cb29fdb4d7b269d6f"
   end
