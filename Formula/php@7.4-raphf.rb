c971aceb908e2b307a1446a6aa95da58088f2839
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-01 09:13:42 -0500

    php@7.4-raphf: update 2.0.1 bottle.

--- Formula/php@7.4-raphf.rb
+++ Formula/php@7.4-raphf.rb
@@ -10,6 +10,7 @@ class PhpAT74Raphf < PhpPeclFormula
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
     sha256 cellar: :any_skip_relocation, monterey: "d1592bfe4bd598b1aa928b2bb8a3c1ce002ac6db6d0fe6cf4cafc24397858404"
+    sha256 cellar: :any_skip_relocation, big_sur:  "d8e6450335af5336cb3c34c60898ff7da7bd36fb7babe352ceb168ed5290c2af"
     sha256 cellar: :any_skip_relocation, catalina: "56232c741a733ff0c70b583efc51491119e08ef4750cf0b53de9cd8147f20caa"
     sha256 cellar: :any_skip_relocation, mojave:   "688ae0bf1d998ddf2c3b986183c2aefbb926215602f18f75da4416bdca872136"
   end
