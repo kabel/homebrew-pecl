f90307bd7769f4fa57806a3344982b8dac4d18a9
Kevin Abel <kevin.abel.0@gmail.com>
2022-03-31 23:05:49 -0500

    php-tideways-xhprof: update 5.0.4_1 bottle.

--- Formula/php-tideways-xhprof.rb
+++ Formula/php-tideways-xhprof.rb
@@ -11,6 +11,7 @@ class PhpTidewaysXhprof < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, monterey: "8312ce5a76019ca8e8186407024ce9b64bb50f4b67941a4e6df26fa683ff758e"
     sha256 cellar: :any_skip_relocation, big_sur:  "6481f381c1ebf75fcddc761b2b366af9dc6cb18cd8753969013c54fec7c6ef09"
     sha256 cellar: :any_skip_relocation, catalina: "943c909a7a4f3a59ba152ca955b72f20650d6d70a7a1c94ce14ef6e525e60272"
   end
