cf48d7594fe2fc6302f2c038be448a33a2cd5cef
Kevin Abel <kevin.abel.0@gmail.com>
2022-04-07 18:49:32 -0500

    php@7.4-uuid: update 1.2.0 bottle.

--- Formula/php@7.4-uuid.rb
+++ Formula/php@7.4-uuid.rb
@@ -9,6 +9,7 @@ class PhpAT74Uuid < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any, monterey: "1f5d092954b905aa951ce8699b54117197eee59b60d6581ba85114ba4d76bf4b"
     sha256 cellar: :any, big_sur:  "7b365f9c35086bee09aff626831b6853cc4ab3642f34aeea397e6dc0eb004af8"
     sha256               catalina: "2724211e87baaa27003935ccd51d5e46be0d11cc227e08ff34adf218c0507ac2"
     sha256               mojave:   "cfbf973b95ec75091f47c9d95cc11d056eaf4c8a5a6ba0988de3fe6d6df728df"
