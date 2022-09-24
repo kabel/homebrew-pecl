a1a904bdc66451bc44e6ee994c5e9eb6f03f63cb
Kevin Abel <kevin.abel.0@gmail.com>
2022-09-24 13:05:48 -0500

    php@7.4-apcu: update 5.1.22 bottle.

--- Formula/php@7.4-apcu.rb
+++ Formula/php@7.4-apcu.rb
@@ -9,9 +9,8 @@ class PhpAT74Apcu < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, monterey: "4b715073279f0f281eb0004f385ae8886ae9cba1e9322c6415a22f65493f5e76"
-    sha256 cellar: :any_skip_relocation, big_sur:  "da2bf1225de921945a481f59f930d1af2b552d99888dd2e3e337d5547444ee6c"
-    sha256 cellar: :any_skip_relocation, catalina: "492bb3864872645a81bc17be8a942838fa15e46694c91e48be720a602a6a59a7"
-    sha256 cellar: :any_skip_relocation, mojave:   "30695abb3bc5b7dc1880294d3a78c1e70595509497b982d836bed26530c7ba1f"
+    sha256 cellar: :any_skip_relocation, monterey: "f5510af98e8ea412b32f60d2cc76d3c4d1509afbbf64099d94c0aead65f5b297"
+    sha256 cellar: :any_skip_relocation, big_sur:  "dfcb34c148e8eb05e83c9438d83cdc0bdaf46ef5855100c1066c3c9cf65167d7"
+    sha256 cellar: :any_skip_relocation, catalina: "08d390bc02c98a1bd1435b082ad3d00783a662c6a18129bae8643281f5606028"
   end
 end
