7d8c62fcd1e6b20648e9f29497ab1ced367dceb1
Kevin Abel <kevin.abel.0@gmail.com>
2022-09-24 14:40:13 -0500

    php@8.0-grpc: update 1.49.0 bottle.

--- Formula/php@8.0-grpc.rb
+++ Formula/php@8.0-grpc.rb
@@ -9,8 +9,8 @@ class PhpAT80Grpc < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, monterey: "707e473d85cb7b2a0fc1e003467885b3abda84e4c9152adeddd06243fb8a82f5"
-    sha256 cellar: :any_skip_relocation, big_sur:  "59fd93e5180cc6d16a291b35722241bc499be2ecf8238282b18c4403bf9a411b"
-    sha256 cellar: :any_skip_relocation, catalina: "7e1ea00ba142af7b429051eb1765e5f695850fbbc08e8960c351c08f205be488"
+    sha256 cellar: :any_skip_relocation, monterey: "a60dca12356da43a40cb930bb0d81c594a4abb8c8e27cb257d4cfceee2b4a881"
+    sha256 cellar: :any_skip_relocation, big_sur:  "70e5ec9acfa496e725e79b7183de78b06b828970237352bc28c939f652e32da8"
+    sha256 cellar: :any_skip_relocation, catalina: "3aab9a7e4d142c0338cbece1e6b3a858bdd811751550b84b65296873e72f3b34"
   end
 end
