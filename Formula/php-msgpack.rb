0de4864650469e96b26d7e4d5441fd15cdc023e7
Kevin Abel <kevin.abel.0@gmail.com>
2022-11-20 15:12:08 -0600

    php-msgpack: update 2.1.2_1 bottle.

--- Formula/php-msgpack.rb
+++ Formula/php-msgpack.rb
@@ -10,6 +10,7 @@ class PhpMsgpack < PhpPeclFormula
 
   bottle do
     root_url "https://kabel.jfrog.io/artifactory/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, ventura:  "c9394475e4e22f130aae19ac7a61f876ee1fab1c1a6810dc618974788b6ceab0"
     sha256 cellar: :any_skip_relocation, monterey: "92fe3c76208da7fda372e59b0d77b13c10adeb2cbe1774ed82591cb0b72c4ae8"
     sha256 cellar: :any_skip_relocation, big_sur:  "85a674d2ba5475ed920576e2d828ec67cbfc68ff187b9f39096c58d2ee4a769a"
     sha256 cellar: :any_skip_relocation, catalina: "9e95cfc47e860e8f88b87ffc7be1493be08f4b571762ac06eef23c0d7c6a4edd"
