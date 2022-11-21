d7b5890daa6cd37a186924d4f05e1a2476f4b23d
Kevin Abel <kevin.abel.0@gmail.com>
2022-11-20 19:04:48 -0600

    php-pdo-sqlsrv: Revision for broken libs

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -5,6 +5,7 @@ class PhpPdoSqlsrv < PhpPeclFormula
 
   url "https://pecl.php.net/get/pdo_sqlsrv-5.10.1.tgz"
   sha256 "c7854196a236bc83508a346f8c6ef1df999bc21eebbd838bdb0513215e0628ad"
+  revision 1
   license "MIT"
 
   bottle do
