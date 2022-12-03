6e64d8c8db0f9c859dd1d229eeb38c3fd6de8f08
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-03 13:41:39 -0600

    php-mongodb: update 1.15.0 bottle.

--- Formula/php-mongodb.rb
+++ Formula/php-mongodb.rb
@@ -9,9 +9,9 @@ class PhpMongodb < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, ventura:  "a1015fb73adfacc52d24d1ed88b474b1288d1410075e42ea4491a6e18b8f1f14"
-    sha256 cellar: :any, monterey: "ab50bdc2cd4c18b124fe52034320b43a3f0ddba45af64a56c5fe123e46745051"
-    sha256 cellar: :any, big_sur:  "fed9a4642138f677a99cb2b2a818258bb889464b7102d802a61f362616241688"
+    sha256 cellar: :any, ventura:  "6ba615dcdf01ee98052890f6ccd67370ec15bce52c568de1c3603c0cce27c010"
+    sha256 cellar: :any, monterey: "823fb984b0cd47756fb8a46d42c00a4e7ef2fe3d43034fdc770ce0cc31288556"
+    sha256 cellar: :any, big_sur:  "21b94b9768585ce94190f1c5b00a0f4a26d2f68275d5d9eaf67780a772c22b2e"
   end
 
   depends_on "icu4c"
