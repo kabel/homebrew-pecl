1da303b81250bc0a5f1fb22991203cdd14576e27
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:50:58 -0600

    php@8.0-protobuf: update 3.21.11 bottle.

--- Formula/php@8.0-protobuf.rb
+++ Formula/php@8.0-protobuf.rb
@@ -9,9 +9,9 @@ class PhpAT80Protobuf < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, ventura:  "17d35ff7bd1369a2f1bf6782cac065e78d67e9deff7796d800a2c9701023adf4"
-    sha256 cellar: :any_skip_relocation, monterey: "b59beacff20db79149d7b58f0820c4c90535c6dd78608ca2a18dfa76894f9e17"
-    sha256 cellar: :any_skip_relocation, big_sur:  "661b7565cdce8f95c8b211d2ae73ecbe75d3e5b4d24f8cd765641eb2f7bee23b"
+    sha256 cellar: :any_skip_relocation, ventura:  "eb0499034f8708668960c13b2650d802f90be7e8cdb1fc1094a114c8d0037359"
+    sha256 cellar: :any_skip_relocation, monterey: "10058ef1011e169bad235699d48861768145797b458b1512867635e2cdc9d81e"
+    sha256 cellar: :any_skip_relocation, big_sur:  "97436e4a6b794f75a4dd568070f4d5267fa269154c5a13e45c7b6c552b8ce590"
   end
 
   deprecate! date: "2022-11-26", because: :versioned_formula
