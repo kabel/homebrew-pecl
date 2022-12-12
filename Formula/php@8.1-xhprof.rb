8ff181e5fde317b0a290aee58c3f4f7270b56525
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:51:03 -0600

    php@8.1-xhprof: add 2.3.8 bottle.

--- Formula/php@8.1-xhprof.rb
+++ Formula/php@8.1-xhprof.rb
@@ -7,5 +7,12 @@ class PhpAT81Xhprof < PhpPeclFormula
   sha256 "c88126717e895e9cbd15d49b8382238b69aee668adf8ae88cb9886fb083fa9f5"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, ventura:  "6b9827ec97f86d8fe3fd2d51ebcb23faf547433a987413946e8b4476a5ed3dcf"
+    sha256 cellar: :any_skip_relocation, monterey: "833cd63294e824e7aab1a271271017f03ea975c2d4ddd58309bf42e45973119e"
+    sha256 cellar: :any_skip_relocation, big_sur:  "7aaa30814384e99519fe3bad8b2255c7afb550fa524bd3cbb717c51d6ff10bdd"
+  end
+
   source_dir "#{extension}-#{version}/extension"
 end
