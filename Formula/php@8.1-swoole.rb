58fd08959eae4c8e305d019f79981dc811a002fb
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:51:02 -0600

    php@8.1-swoole: add 5.0.1 bottle.

--- Formula/php@8.1-swoole.rb
+++ Formula/php@8.1-swoole.rb
@@ -7,6 +7,13 @@ class PhpAT81Swoole< PhpPeclFormula
   sha256 "6e73086b28ce447cdd895f1f36167b12befd48e10224b43169e35d2cf9de05ff"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, ventura:  "c70f8e37ef0c3529d93d98c84718356395153ad16c4a0f8661b556dbbb784da4"
+    sha256 cellar: :any, monterey: "6b73ddb4e5bf5ff25c2fefedf722c5e0cb0914960797a11bc43ec53209715ac5"
+    sha256 cellar: :any, big_sur:  "77d5d88efa28c35d2f56435c0222a40f24f32930d413a2936d16adf907b3b6e1"
+  end
+
   conflicts_with "php@8.1-openswoole", because: "both provide the same PHP API"
 
   depends_on "openssl@1.1"
