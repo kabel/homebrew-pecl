bc2e617f6ba590a2504358c24c5e11f93c5f3ed3
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:51:02 -0600

    php@8.1-sqlsrv: add 5.10.1 bottle.

--- Formula/php@8.1-sqlsrv.rb
+++ Formula/php@8.1-sqlsrv.rb
@@ -7,5 +7,12 @@ class PhpAT81Sqlsrv < PhpPeclFormula
   sha256 "5cdaedb4d8a286343e6b3b99992d9fcb44a8fb69dd02aa5d7bc20eb2ea5e59d2"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, ventura:  "917af1c9024d8659f0d3843e07faa671ac456e10a9f8f5b49d58834c3cb321c2"
+    sha256 cellar: :any, monterey: "f4d0c37a881ff2b84e26af60d7dbdd4d18dbe179afae077d9bb90707fa1473fc"
+    sha256 cellar: :any, big_sur:  "35dfb94f76a6a14f3efaf1305ec0b6810ccc51ac1712ce88d351db07d255dfc9"
+  end
+
   depends_on "unixodbc"
 end
