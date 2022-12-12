d894c238b789bf429c45b6a2e962f07770c8f0e2
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:50:58 -0600

    php@8.0-xdebug: update 3.2.0 bottle.

--- Formula/php@8.0-xdebug.rb
+++ Formula/php@8.0-xdebug.rb
@@ -9,9 +9,9 @@ class PhpAT80Xdebug < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 ventura:  "8275c23765df7fc353260ea9e56cf03ebbea9dbd0833c1411cecc5723e2a00a1"
-    sha256 monterey: "202629cf1b32e823efeaf225e78e20d4af4484e4a61d433161a4c97d26a76c76"
-    sha256 big_sur:  "4082e9e9bb10416edf25b5e384c10624d8e3bbcc32c28d03d48a84a5ff86f660"
+    sha256 ventura:  "4c184b443fd4ce9429413e9f76493a69957a31b9800c2ab0a5a1aaae25b7bb7c"
+    sha256 monterey: "784d42adf5d699b25f3ead2f5db66419d1fb8db22ac37874c521a7cc983410de"
+    sha256 big_sur:  "efa275e26779fb84fefc13ddff74e7b71b72144aaa960ddc5cbeca9fa2c808ef"
   end
 
   deprecate! date: "2022-11-26", because: :versioned_formula
