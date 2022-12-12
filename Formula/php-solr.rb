c2e1d00829fc5c6115ae8e011ef4736699fc05b8
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 17:50:56 -0600

    php-solr: update 2.6.0_1 bottle.

--- Formula/php-solr.rb
+++ Formula/php-solr.rb
@@ -15,9 +15,9 @@ class PhpSolr < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, ventura:  "893bb3af47c56e212ed7fd8c8850018b8f1baa5ffc9faf88ffd8801e85aa8cd8"
-    sha256 cellar: :any, monterey: "bf1316b4e17f1146567676decb131479ee16cb17cecc853422407a83c2570485"
-    sha256 cellar: :any, big_sur:  "8cfbfed3f71ed460df0f6b77e2fb46e12c9f43d4f1fc898a3924aff511c6a121"
+    sha256 cellar: :any, ventura:  "d10c8e429e10c8ce09b4e2018331463da0a67bc19e20d21dc08925f1bf0a3666"
+    sha256 cellar: :any, monterey: "d37b2dd55c5923d95d04ed2481119123050dead5dd8fe63f92d41903d60c03b9"
+    sha256 cellar: :any, big_sur:  "9132395b8d092f40bac2243e178f3045ba1a80476423a346c9fa74eaee735c22"
   end
 
   depends_on "curl"
