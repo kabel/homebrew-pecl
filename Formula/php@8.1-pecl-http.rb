b2a6b65176c089187e8af1e6dd6ce545b1e6defd
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 19:02:47 -0600

    php@8.1-pecl-http: add 4.2.3 bottle.

--- Formula/php@8.1-pecl-http.rb
+++ Formula/php@8.1-pecl-http.rb
@@ -7,6 +7,13 @@ class PhpAT81PeclHttp < PhpPeclFormula
   sha256 "fa2ab558fc8f0928a10f35c0f566f7c4a1d32e727bd3a96579e4c28482ee9d6a"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, ventura:  "c212998e872110749fccc7d1e7e67cba31fda7c34cea6df78e0cd471b61502a2"
+    sha256 cellar: :any, monterey: "e2d1123fa58bdc6103a49434e92922d7dbae062d8c620b63ddc94f0c01eed4f1"
+    sha256 cellar: :any, big_sur:  "2de372db3224107131b5ae10bfb452c108cd2eae9477e14c31ffa60259fce3eb"
+  end
+
   depends_on "brotli"
   depends_on "curl"
   depends_on "icu4c"
