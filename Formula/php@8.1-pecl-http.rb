06a490728106fe4dbe8683aa1bf1747256037d46
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-12 18:17:06 -0600

    php@8.1-pecl-http 4.2.3 (New Formula)

---
+++ Formula/php@8.1-pecl-http.rb
@@ -0,0 +1,34 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT81PeclHttp < PhpPeclFormula
+  extension_dsl "Extended HTTP Support"
+
+  url "https://pecl.php.net/get/pecl_http-4.2.3.tgz"
+  sha256 "fa2ab558fc8f0928a10f35c0f566f7c4a1d32e727bd3a96579e4c28482ee9d6a"
+  license "BSD-2-Clause"
+
+  depends_on "brotli"
+  depends_on "curl"
+  depends_on "icu4c"
+  depends_on "libevent"
+  depends_on "libidn2"
+  depends_on "kabel/pecl/php@8.1-raphf"
+
+  configure_arg %W[
+    --with-http
+    --with-http-libicu-dir=#{Formula["icu4c"].opt_prefix}
+    --with-http-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
+  ]
+
+  def install
+    extra_includes = %W[
+      -I#{Formula["kabel/pecl/php@8.1-raphf"].opt_include}/php
+    ]
+    ENV["EXTRA_INCLUDES"] = extra_includes * " "
+    super
+  end
+
+  def provides_extension
+    "http"
+  end
+end
