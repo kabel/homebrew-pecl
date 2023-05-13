17efb51bbc59da5d36fe98e18691aad3b3c5724f
Kevin Abel <kevin.abel.0@gmail.com>
2023-05-13 15:22:56 -0500

    php-memcache 8.2

--- Formula/php@8.1-memcache.rb
+++ Formula/php@8.1-memcache.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Memcache < PhpPeclFormula
   extension_dsl "memcache extension"
 
-  url "https://pecl.php.net/get/memcache-8.0.tgz"
-  sha256 "defe33e6f7831d82b7283b95e14a531070531acbf21278f3f0d7050505cf3395"
+  url "https://pecl.php.net/get/memcache-8.2.tgz"
+  sha256 "b3f0640eacdeb9046c6c86a1546d7fb8a4e9f219e5d9a36a287e59b2dd8208e5"
   license "PHP-3.0"
 
   bottle do
