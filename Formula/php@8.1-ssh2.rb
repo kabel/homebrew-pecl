56954c0e3d9d352ad6f344dfdaa0ec7baf39fdbb
Kevin Abel <kevin.abel.0@gmail.com>
2023-05-13 15:33:54 -0500

    php-ssh2 1.4

--- Formula/php@8.1-ssh2.rb
+++ Formula/php@8.1-ssh2.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Ssh2 < PhpPeclFormula
   extension_dsl "Bindings for the libssh2 library"
 
-  url "https://pecl.php.net/get/ssh2-1.3.1.tgz"
-  sha256 "9093a1f8d24dc65836027b0e239c50de8d5eaebf8396bc3331fdd38c5d69afd9"
+  url "https://pecl.php.net/get/ssh2-1.4.tgz"
+  sha256 "988b52e0315bb5ed725050cb02de89b541034b7be6b94623dcb2baa33f811d87"
   license "PHP-3.01"
 
   bottle do
