a1fd5d2423facea6d51ae37d8a8e0a38d58f84b7
Kevin Abel <kevin.abel.0@gmail.com>
2023-05-14 13:52:33 -0500

    php@8.0-couchbase: update 4.1.3 bottle.

--- Formula/php@8.0-couchbase.rb
+++ Formula/php@8.0-couchbase.rb
@@ -9,9 +9,9 @@ class PhpAT80Couchbase < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 ventura:  "029d27b884ecac9c340b9f38328d759c8a735e6008a8d0cc4fd668a9baf98e31"
-    sha256 monterey: "9299dcbc9a28a7d098971c4a0fefd707a4bf30fc8c54f3b2f71b92c8e892fa95"
-    sha256 big_sur:  "81a6c5b148991d13792b376a9a1300baf745752f8128784d44c9a8e1eafb909d"
+    sha256 ventura:  "08daa5fb1e421df177e609c4bdc2880118391a6c14c9d48c84438b38cba977fc"
+    sha256 monterey: "7c6bcc0a811eb1c67fe8ee0672cb85326a31840d5b944ee2c68881b0533532fb"
+    sha256 big_sur:  "6969e772d53410122c43eb3d01ca1f530c16cf5d29510f9255454b3f4530cd05"
   end
 
   deprecate! date: "2022-11-26", because: :versioned_formula
