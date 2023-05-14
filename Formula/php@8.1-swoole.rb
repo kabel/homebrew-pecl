0d0ecf4805139fecd653842cf5566882c0865a1b
Kevin Abel <kevin.abel.0@gmail.com>
2023-05-13 19:27:50 -0500

    php@8.1-swoole: update 5.0.3 bottle.

--- Formula/php@8.1-swoole.rb
+++ Formula/php@8.1-swoole.rb
@@ -9,9 +9,9 @@ class PhpAT81Swoole< PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, ventura:  "0fef84fdacae4a5d1f9ba12e2ab4cd83001f220be09818a201eeb7412f8c5ba0"
-    sha256 cellar: :any, monterey: "154f10aecf6998ecb2dbb194723dfde5ee05c28310d6674d0611b3e3f5832602"
-    sha256 cellar: :any, big_sur:  "f6ff9a081afbc8363bbaba78c4a9aa0caf009696eb32cf20cafa770faa02a074"
+    sha256 cellar: :any, ventura:  "935f25250ad2cf963fd8551a3cf459d132fab9197710333ccc4aca14079e1c37"
+    sha256 cellar: :any, monterey: "f69861e8ab096e9013ec329702c59164802105121393331d41fb1727aa09f8e3"
+    sha256 cellar: :any, big_sur:  "1a9ee31eed713392120d8970530f521ba4b58fc71eed0007166a86e11eae4a9e"
   end
 
   conflicts_with "php@8.1-openswoole", because: "both provide the same PHP API"
