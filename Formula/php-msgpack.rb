81628db07eff18a6d8ac96c88cb63fdc3c996f5f
Kevin Abel <kevin.abel.0@gmail.com>
2023-06-18 14:30:53 -0500

    php-msgpack 2.2.0

--- Formula/php-msgpack.rb
+++ Formula/php-msgpack.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpMsgpack < PhpPeclFormula
   extension_dsl "Interfacing with MessagePack"
 
-  url "https://pecl.php.net/get/msgpack-2.1.2.tgz"
-  sha256 "912ff4d33f323ea7cb04569df5ae23c09ce614412a65c39c2ca11f52802abe82"
-  revision 2
+  url "https://pecl.php.net/get/msgpack-2.2.0.tgz"
+  sha256 "82aa1e404c5ff54ec41d2a201305cd6594ed14a7529e9119fa7ca457e4bbd12a"
   license "PHP-3.01"
 
   bottle do
