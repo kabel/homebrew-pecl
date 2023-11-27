fb8e4c97df0458fcdd35f2c76b6e75efbaa49288
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-27 17:51:10 -0600

    php-sqlsrv 5.11.1

--- Formula/php@8.1-sqlsrv.rb
+++ Formula/php@8.1-sqlsrv.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Sqlsrv < PhpPeclFormula
   extension_dsl "Microsoft Drivers for SQL Server"
 
-  url "https://pecl.php.net/get/sqlsrv-5.11.0.tgz"
-  sha256 "6e437af4db730ab995c597f960e98bac060fc220a8d51ee24877eb7f39090a09"
+  url "https://pecl.php.net/get/sqlsrv-5.11.1.tgz"
+  sha256 "678ab60174be56b09c6916307700e716a4ff266ad53e43990a9d9740d4728463"
   license "MIT"
 
   bottle do
