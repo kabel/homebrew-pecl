1320ac30fc0256ac2423cbec5d5ac43e0b931eca
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-27 16:54:16 -0600

    php-amqp 2.1.1

--- Formula/php-amqp.rb
+++ Formula/php-amqp.rb
@@ -3,9 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAmqp < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
-  url "https://pecl.php.net/get/amqp-1.11.0.tgz"
-  sha256 "dc5212b4785f59955118a219bbfbcedb7aa6ab2a91e8038a0ad1898f331c2f08"
-  revision 2
+  url "https://pecl.php.net/get/amqp-2.1.1.tgz"
+  sha256 "c9b36f10c2e7b7da472440e21c35655c3bf41983b99bf1e438eac7d5bb1c2b45"
   license "PHP-3.01"
 
   bottle do
--- Formula/php@8.2-amqp.rb
+++ Formula/php@8.2-amqp.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT82Amqp < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
-  url "https://pecl.php.net/get/amqp-1.11.0.tgz"
-  sha256 "dc5212b4785f59955118a219bbfbcedb7aa6ab2a91e8038a0ad1898f331c2f08"
+  url "https://pecl.php.net/get/amqp-2.1.1.tgz"
+  sha256 "c9b36f10c2e7b7da472440e21c35655c3bf41983b99bf1e438eac7d5bb1c2b45"
   license "PHP-3.01"
 
   deprecate! date: "2025-12-08", because: :unsupported
