cb4ecd8b4ce53210ef8bd36792c252ddf30f1d20
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-27 17:17:22 -0600

    php-apcu 5.1.23

--- Formula/php@8.1-apcu.rb
+++ Formula/php@8.1-apcu.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Apcu < PhpPeclFormula
   extension_dsl "APC User Cache"
 
-  url "https://pecl.php.net/get/apcu-5.1.22.tgz"
-  sha256 "010a0d8fd112e1ed7a52a356191da3696a6b76319423f7b0dfdeaeeafcb41a1e"
+  url "https://pecl.php.net/get/apcu-5.1.23.tgz"
+  sha256 "67ee7464ccad2335c3fa4aeb0b8edbcf6d8344feea7922620c6a13015d604482"
   license "PHP-3.01"
 
   bottle do
