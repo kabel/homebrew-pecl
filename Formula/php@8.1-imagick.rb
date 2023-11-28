050b9373419496816764867df77cde6b58e1b46d
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:20:37 -0600

    php@8.1-imagick: update 3.7.0 bottle.

--- Formula/php@8.1-imagick.rb
+++ Formula/php@8.1-imagick.rb
@@ -9,6 +9,7 @@ class PhpAT81Imagick < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:   "8f88f93fd4a2a9e61143ebcb10d2dff28bd813039c3bc2bbdd26dc38899fe1d3"
     sha256 cellar: :any, ventura:  "6bd11a29cc9f66ad8223f0f742212012933d30c361d4f44d9a456e783f415fd5"
     sha256 cellar: :any, monterey: "79df35368c2e8bb570295a68fc1e11efad83d89e9baa61dc0d73ea4cbc127317"
     sha256 cellar: :any, big_sur:  "d696ce9df15ad283a6a8734f969f0c63346364ce9f926f3ef09cd0555c266472"
