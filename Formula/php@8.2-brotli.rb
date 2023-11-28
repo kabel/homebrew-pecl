739672eee3a47c59422e9129d4a89cbdcdf499a4
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:22 -0600

    php@8.2-brotli: add 0.14.2 bottle.

--- Formula/php@8.2-brotli.rb
+++ Formula/php@8.2-brotli.rb
@@ -8,6 +8,13 @@ class PhpAT82Brotli < PhpPeclFormula
   sha256 "40b00f6ab75a4ce54b8af009e8ad2ac5077a4a35d6bbb50807324565b8472bee"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:   "367f974072fa1edb0bbfea54c4f20529ddcca85e4253d2b8270b097d216cf310"
+    sha256 cellar: :any, ventura:  "2b9754def567d4dc5fcb7b511afad138610840e619a2570a9c48787c6edeb4bc"
+    sha256 cellar: :any, monterey: "5a559102ef72b6d4e7d39928e0bf09abd056632a17d54ee38bd7bcfb5b79b056"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 
   depends_on "brotli"
