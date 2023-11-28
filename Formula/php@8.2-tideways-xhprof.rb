d31cba3b517c9b8d199c9badd081c3bd25bd3a2a
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:41 -0600

    php@8.2-tideways-xhprof: add 5.0.4 bottle.

--- Formula/php@8.2-tideways-xhprof.rb
+++ Formula/php@8.2-tideways-xhprof.rb
@@ -4,6 +4,13 @@ class PhpAT82TidewaysXhprof < PhpPeclFormula
   extension_dsl "Modern XHProf compatible PHP Profiler"
   homepage "https://tideways.com/"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "68ee1a6875ce5159d1580557f3292fe05c42d72cb7710ef53535fce46d417246"
+    sha256 cellar: :any_skip_relocation, ventura:  "13af07c96dc7454a2434f73bd40fd6ee534b1838dfa4746dd89e707207803280"
+    sha256 cellar: :any_skip_relocation, monterey: "f1bbb4773ba64c5d204e1c5075ef0f4db130f2f0fba4c89c0d1775c69f3546fb"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 
   url "https://github.com/tideways/php-xhprof-extension/archive/v5.0.4.tar.gz"
