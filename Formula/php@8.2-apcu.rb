38dece08f8e0460e9e3c0a50f2b718153c3e5371
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:21 -0600

    php@8.2-apcu: add 5.1.23 bottle.

--- Formula/php@8.2-apcu.rb
+++ Formula/php@8.2-apcu.rb
@@ -7,5 +7,12 @@ class PhpAT82Apcu < PhpPeclFormula
   sha256 "67ee7464ccad2335c3fa4aeb0b8edbcf6d8344feea7922620c6a13015d604482"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "4f238e9a90cfb3594850555c06e066c0eafca762f953cc31e8254101d29031f6"
+    sha256 cellar: :any_skip_relocation, ventura:  "c0146422131ce45e635a97342a5038c2c525b9377ff9e5fd6c733ebb297c9db4"
+    sha256 cellar: :any_skip_relocation, monterey: "06ffbbad9a73ed03f96bf713cfb5c5da4c3c76052a0a7095e851fdf012885c36"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 end
