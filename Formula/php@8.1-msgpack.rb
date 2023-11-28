5b6da6889f77dcc2b9d7791aeb8a009e359ff95d
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:20:41 -0600

    php@8.1-msgpack: update 2.2.0 bottle.

--- Formula/php@8.1-msgpack.rb
+++ Formula/php@8.1-msgpack.rb
@@ -9,6 +9,7 @@ class PhpAT81Msgpack < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "db39c4bae22904e1c45b5fc93ac62af2179e6b6c32ac7e914c478b5087ac98a6"
     sha256 cellar: :any_skip_relocation, ventura:  "a49412f9835e60ea64355e08df30be83ff1794f6949a7cede1b25ec03be88645"
     sha256 cellar: :any_skip_relocation, monterey: "0ad36c1981aee4917d57f59e0f8709495c959fbaa4089ac55ddb84e06b11f592"
     sha256 cellar: :any_skip_relocation, big_sur:  "1812e8a067c8ae782db943376025aa69cf14322b53b46d5fcfc83bc6b00000b3"
