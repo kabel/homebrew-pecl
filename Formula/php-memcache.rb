5e50e33f589182d79b9aa7fd0d4a6b77ee5c081e
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:21:37 -0600

    php-memcache: update 8.2_1 bottle.

--- Formula/php-memcache.rb
+++ Formula/php-memcache.rb
@@ -10,9 +10,9 @@ class PhpMemcache < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, ventura:  "ac7d2346fe10ff6ebc503fc54eb0199cf8b8146a5c33d88b0b83b5f674e30e9a"
-    sha256 cellar: :any_skip_relocation, monterey: "40c5e5e0d13d3519c394a72bfe791616dbfbc2be75cdbcde2097dc6eb5934177"
-    sha256 cellar: :any_skip_relocation, big_sur:  "4b490a379e65f6d88d0eedb0f58ddce8ec8752647784c9c80b764e279827f6f7"
+    sha256 cellar: :any_skip_relocation, sonoma:   "a2e91cd20cf39fc35405d72d5f1a3bd2dc5f21d6b84b6aaf753db56049488cdf"
+    sha256 cellar: :any_skip_relocation, ventura:  "59e729e20943f20dc3a27fa2360ca67ab76793124faa25db8ea4489b32735c60"
+    sha256 cellar: :any_skip_relocation, monterey: "a5d43ba785e42aefc856c481b45b44797bb15d4333f8d4317e06e69dacd6e676"
   end
 
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
