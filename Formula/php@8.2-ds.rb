fbe2acb51688ff5daa304410b641ff812dbfbeeb
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:24 -0600

    php@8.2-ds: add 1.4.0 bottle.

--- Formula/php@8.2-ds.rb
+++ Formula/php@8.2-ds.rb
@@ -7,5 +7,12 @@ class PhpAT82Ds < PhpPeclFormula
   sha256 "a9b930582de8054e2b1a3502bec9d9e064941b5b9b217acc31e4b47f442b93ef"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "9001e805f1cfc76cc4b6dbada65bd14c4dac36018a0e46bd92f674d7ab3d6bb8"
+    sha256 cellar: :any_skip_relocation, ventura:  "37a31e19ada11bfdb7bebefdb20abc3f06c470876319361ffc58294c2388538d"
+    sha256 cellar: :any_skip_relocation, monterey: "e5767de6f8c7b9751292393052054e7fe28f945f3f5b1170c5b39be1e166aa4f"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 end
