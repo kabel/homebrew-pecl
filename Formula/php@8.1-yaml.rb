096c6df303fbd81b1a4c68698522baf9bffa8c2e
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 12:25:18 -0600

    php@8.1-yaml: update 2.2.3_1 bottle.

--- Formula/php@8.1-yaml.rb
+++ Formula/php@8.1-yaml.rb
@@ -10,7 +10,9 @@ class PhpAT81Yaml < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, sonoma: "29d85faa032741a276836263b0a9c4f26155871678ec958178c00beea1e73da1"
+    sha256 cellar: :any, sonoma:   "e94341b7e3b2fbe7e3e77b5258546a19e3c46aa0457185c5a8781be51bd93c9f"
+    sha256 cellar: :any, ventura:  "e669f09838072c2c0a4da2cc2dc1a54356c621fa7ba9e7bea0a9d7f500e70916"
+    sha256 cellar: :any, monterey: "4bb71e3be30e4de6fe3f290c049a77dbe3f394e2ad60ab00bac0516d2f43f87e"
   end
 
   deprecate! date: "2024-11-25", because: :unsupported
