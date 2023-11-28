1a2e183040eb14115d74ba533eab2fd357078bd2
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:43 -0600

    php@8.2-uuid: add 1.2.0 bottle.

--- Formula/php@8.2-uuid.rb
+++ Formula/php@8.2-uuid.rb
@@ -7,6 +7,13 @@ class PhpAT82Uuid < PhpPeclFormula
   sha256 "5cb834d32fa7d270494aa47fd96e062ef819df59d247788562695fd1f4e470a4"
   license "LGPL-2.1-only"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:   "41cff82843932a9cf5e8640aa4eddce6871cab7c325804fc3e4bb05d5c4b6efa"
+    sha256 cellar: :any, ventura:  "41fd70145afddca3018743e6ca440f7cb81c14928f918fcdbccabb354b7490c0"
+    sha256 cellar: :any, monterey: "d4e138fc44daaaaa5666fb85c5f107cbdc40ca0d931f32a2d479313dc798dbac"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 
   depends_on "e2fsprogs"
