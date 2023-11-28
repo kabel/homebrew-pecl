86f135d71af6236648d4312b71d0a948bb131f7d
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:20:43 -0600

    php@8.1-ssh2: update 1.4 bottle.

--- Formula/php@8.1-ssh2.rb
+++ Formula/php@8.1-ssh2.rb
@@ -9,6 +9,7 @@ class PhpAT81Ssh2 < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:   "c9bd9662a570c10ef4303329f293754a6372a48ca6658d53e82bdee7268f32f9"
     sha256 cellar: :any, ventura:  "f5ac29fc329cbd56189e00ca34184fe6f599d54cbef63bd7bb5c7b29ea207f99"
     sha256 cellar: :any, monterey: "720f8e9a8f770da6e85d1382034c12ebc6b191d341bb3b30b48cf89cabed2817"
     sha256 cellar: :any, big_sur:  "7cb4b02161317cb74df8ba79116004aef6d18c1efc14cab21b3bb063a3bc2099"
