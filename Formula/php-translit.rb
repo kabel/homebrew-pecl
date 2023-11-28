f7051c1d6ea300bd36afc8f1afb9d44014cf792d
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:21:50 -0600

    php-translit: update 0.7.1_4 bottle.

--- Formula/php-translit.rb
+++ Formula/php-translit.rb
@@ -10,8 +10,8 @@ class PhpTranslit < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, ventura:  "08c7d4bdf42d3af92bbd07e54658844c18a781d170496b6ce1074423bbbdf55c"
-    sha256 cellar: :any_skip_relocation, monterey: "26e5bf59964a1c48045fc569aaab22715e09dc2ee67e79650e5c1052b2d7040b"
-    sha256 cellar: :any_skip_relocation, big_sur:  "88359d65d718e7bfcf1bb031849f8b1e9254267075bbe1ba07c6f87cbb74b267"
+    sha256 cellar: :any_skip_relocation, sonoma:   "ce023b3781d660da4705ae77cd8e2bce419fceb42d3ebbce61e63368e2fd6629"
+    sha256 cellar: :any_skip_relocation, ventura:  "48957538e3e7f5d705e3dca8c14923b1420834f10eda4ec3a603cc911cbcb083"
+    sha256 cellar: :any_skip_relocation, monterey: "53ed23e895dd9a88435b98c256c7cdd4b70ff0e20e0fff964f9902f7c63d171e"
   end
 end
