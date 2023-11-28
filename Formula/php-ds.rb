afecbc875b8a608bbe767ca0dda964febfd7c291
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 12:25:15 -0600

    php-ds: update 1.4.0_3 bottle.

--- Formula/php-ds.rb
+++ Formula/php-ds.rb
@@ -10,9 +10,9 @@ class PhpDs < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, ventura:  "4c046d2e8773d49d26b846344fabfea329137eab9e6717a06807524a283d6ad2"
-    sha256 cellar: :any_skip_relocation, monterey: "11a71a34cd22dcf97f535c6ff986d566f172914946944484cc23bb4fa26af638"
-    sha256 cellar: :any_skip_relocation, big_sur:  "374546b57f1573a1bb71a7b7caa851e85a447e08b2e7ddc845746e95ee117aab"
+    sha256 cellar: :any_skip_relocation, sonoma:   "7fe207158a1feb0c59766f09bca77836651ee9b0b094307d4a7966da4744b11c"
+    sha256 cellar: :any_skip_relocation, ventura:  "dff75fd73158fd0fd385319f9903b28e26b736c0420208d1713c95ec5ce6e08e"
+    sha256 cellar: :any_skip_relocation, monterey: "0441906c6674f225081565f82be42afeddde2cb669f78b903b55072e2b9f62cc"
   end
 
   # https://github.com/php-ds/ext-ds/pull/195
