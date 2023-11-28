95e707adaed3d316d224093a239c5825a3802cda
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:37 -0600

    php@8.2-raphf: add 2.0.1 bottle.

--- Formula/php@8.2-raphf.rb
+++ Formula/php@8.2-raphf.rb
@@ -7,6 +7,13 @@ class PhpAT82Raphf < PhpPeclFormula
   sha256 "da3566db17422e5ef08b7ff144162952aabc14cb22407cc6b1d2a2d095812bd0"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "8d2be136609e22307a0cfef1bddacc1acf17ce67974a2988c20e4f5a50b3e2ba"
+    sha256 cellar: :any_skip_relocation, ventura:  "47e15382c14c0f1d3fb22eb8446d0d39ad888c12ba834b19e3e5b55e66bf9ed2"
+    sha256 cellar: :any_skip_relocation, monterey: "10752c2178ffbb4754e1e6eabe6ef92d407146499ff87a17d4a38149bb29936c"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 
   conf_order "05"
