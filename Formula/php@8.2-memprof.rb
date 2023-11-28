74b9ca5f248e21c16d2ad1444888274788d8a6cc
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:30 -0600

    php@8.2-memprof: add 3.0.2 bottle.

--- Formula/php@8.2-memprof.rb
+++ Formula/php@8.2-memprof.rb
@@ -10,6 +10,13 @@ class PhpAT82Memprof < PhpPeclFormula
   sha256 "510bf09d58487820ac11b617446199ca86c50fda972dde73feaae667be23d183"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:   "ecead0fa7fbd52aa9f657d7b63268b8da3454c02f710af8e7272d9fa2d6348d4"
+    sha256 cellar: :any, ventura:  "6fe24a72fda796ab96d624b13584191f8373d9a885b38888f6a84cdbc0576d12"
+    sha256 cellar: :any, monterey: "b11840a79ee7fc916625ed7d507134a08b29d5946b7492ff7861591d34a73847"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 
   resource "judy" do
