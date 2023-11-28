97e7d6cab5f6a297477624df45ed409685dca433
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:22:36 -0600

    php@8.2-protobuf: add 3.25.1 bottle.

--- Formula/php@8.2-protobuf.rb
+++ Formula/php@8.2-protobuf.rb
@@ -7,5 +7,12 @@ class PhpAT82Protobuf < PhpPeclFormula
   sha256 "c1bcbd2671cb350355bf5b7f87f07a6813f11447ac6295cc53160756a9f385a6"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:   "6c729006b0c6516bad2f77660f0ca0e9450c7644b0051f6cdd67af8ec6b6b094"
+    sha256 cellar: :any_skip_relocation, ventura:  "07a430923dabf93ba9f3c8f3adb45b2bf57f467eb50407033fbff7df04a2a79b"
+    sha256 cellar: :any_skip_relocation, monterey: "8197664f472816d86cd41279a56f2674c8efc3bfb1bc7ed0dd26d0ef1b18c59c"
+  end
+
   deprecate! date: "2025-12-08", because: :unsupported
 end
