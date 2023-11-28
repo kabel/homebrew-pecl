13a7773782f2d922c48fcdf03d62bb4c82b33300
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-28 10:21:58 -0600

    php@8.0-mailparse: update 3.1.6 bottle.

--- Formula/php@8.0-mailparse.rb
+++ Formula/php@8.0-mailparse.rb
@@ -9,10 +9,9 @@ class PhpAT80Mailparse < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, ventura:  "6c074ba6b3afc399e4d9eb242f92f68fcddbd440a1167571d3740d79379d9376"
-    sha256 cellar: :any_skip_relocation, monterey: "21eb4ce2bc3a81f9159735f2e9708b2191bdea633cd387427bf07a27cbaf84c0"
-    sha256 cellar: :any_skip_relocation, big_sur:  "f08276d2200e95cc07c6dac09f587b5abd71cb3d6ec70a3501e40601da4a5345"
-    sha256 cellar: :any_skip_relocation, catalina: "e7929cfacce03896fcb0992d670d9539f9f68ba9a618860b9ff9b5ef538ab474"
+    sha256 cellar: :any_skip_relocation, sonoma:   "04db5eb157a38428948a5c75641ad3f0348e1574c327baa7bd2e75b336efe332"
+    sha256 cellar: :any_skip_relocation, ventura:  "6dae4146c38c35b52e5974d4e51384dd60f49bcd05284befbf4f98d32640a7de"
+    sha256 cellar: :any_skip_relocation, monterey: "ec94823d2138f6d9a1b2499cd79afdd204c2bf3aaf91e91d6a08729753f13ee1"
   end
 
   deprecate! date: "2022-11-26", because: :versioned_formula
