87184d0b0fd79f95433c0753bbd29b29885a11d9
Kevin Abel <kevin.abel.0@gmail.com>
2023-12-03 11:03:49 -0600

    php-igbinary: update 3.2.15 bottle.

--- Formula/php-igbinary.rb
+++ Formula/php-igbinary.rb
@@ -9,9 +9,9 @@ class PhpIgbinary < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:   "1fac82d6f83f3f98505235da07fef7c86637fe2635e424325598cc6bfa2f2f5b"
-    sha256 cellar: :any_skip_relocation, ventura:  "48d1061bb07c4fd60ce8fce690175de76cc8024da859be53ac09c632c3964e50"
-    sha256 cellar: :any_skip_relocation, monterey: "915dc69fa99195219323dfc5197c2d65b13d95d881df18c5f2983914e6f13beb"
+    sha256 cellar: :any_skip_relocation, sonoma:   "bf2bf064d8326d15db1db840b3f092b575205f9e382659252ae8c9f2fb9c84d7"
+    sha256 cellar: :any_skip_relocation, ventura:  "283bc2fc5829563f81ec6ce132dadd94531d94ab64ac2d98f979f71d84da5aa9"
+    sha256 cellar: :any_skip_relocation, monterey: "0a95b2815506e8cedf6315c9eef5aa64d67a1f85ebd52a3fd85855761965a378"
   end
 
   conf_order "05"
