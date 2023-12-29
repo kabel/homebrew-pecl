f651e7ad53764e8f46d9e8931cab61fc68cb0ce3
Kevin Abel <kevin.abel.0@gmail.com>
2023-12-29 16:52:28 -0600

    php@8.2-mongodb: update 1.17.2 bottle.

--- Formula/php@8.2-mongodb.rb
+++ Formula/php@8.2-mongodb.rb
@@ -9,9 +9,9 @@ class PhpAT82Mongodb < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, sonoma:   "3406930339a9c8794b443a3960fd6c8d10cfbe8dd3113b5eb2a2e4d23f72fd6d"
-    sha256 cellar: :any, ventura:  "67530c0cac1f6457e4bbf489ec5578b6f68e438365e8f0526a838c8634ea279e"
-    sha256 cellar: :any, monterey: "6233823155ce4ff32e9fda99e4bb5639b74d2d6b7821239f3419ee282c8cc325"
+    sha256 cellar: :any, sonoma:   "5174ab0943a6101de1d634e6f55aab36cd66c9d4076bff7a1663dab1e135212e"
+    sha256 cellar: :any, ventura:  "fe0ebd51d24c9e48a84db97589fd7b0135bdf298ace9fac0fb7d2df8bc125fb2"
+    sha256 cellar: :any, monterey: "5ea573a279142f73f0e4f5ccba468c327a5c412821daa5844f24003102301e3d"
   end
 
   deprecate! date: "2025-12-08", because: :unsupported
