b42eabb4fa0db3ed1ef7e6090c1397f7ec9d116f
Kevin Abel <kevin.abel.0@gmail.com>
2024-03-06 15:36:32 -0600

    php-pdo-sqlsrv 5.12.0

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpPdoSqlsrv < PhpPeclFormula
   extension_dsl "Microsoft Drivers for SQL Server"
 
-  url "https://pecl.php.net/get/pdo_sqlsrv-5.11.1.tgz"
-  sha256 "549855a992a1363e4edef7b31be6ab0f9cd6dd9cc446657857750065eae6af89"
+  url "https://pecl.php.net/get/pdo_sqlsrv-5.12.0.tgz"
+  sha256 "22f0cb17b45f0deccd0bba072ee0085ff4094cd6ee2acc26f7f924975ef652c6"
   license "MIT"
 
   bottle do
