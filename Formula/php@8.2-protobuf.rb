ecfcd286f3ccd3b6fc27b8c7857d293ab196425c
Kevin Abel <kevin.abel.0@gmail.com>
2024-03-06 21:35:48 -0600

    php@8.2-protobuf: update 3.25.3 bottle.

--- Formula/php@8.2-protobuf.rb
+++ Formula/php@8.2-protobuf.rb
@@ -9,9 +9,9 @@ class PhpAT82Protobuf < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:   "6c729006b0c6516bad2f77660f0ca0e9450c7644b0051f6cdd67af8ec6b6b094"
-    sha256 cellar: :any_skip_relocation, ventura:  "07a430923dabf93ba9f3c8f3adb45b2bf57f467eb50407033fbff7df04a2a79b"
-    sha256 cellar: :any_skip_relocation, monterey: "8197664f472816d86cd41279a56f2674c8efc3bfb1bc7ed0dd26d0ef1b18c59c"
+    sha256 cellar: :any_skip_relocation, sonoma:   "9ee2de93e4d12877a9bbaef6bcda8dc83d727613e86fc7a3502bd605d8b51043"
+    sha256 cellar: :any_skip_relocation, ventura:  "6e183df3ad8e239099f7d8dd555511dc1b45255208e9da5170485e0ace70b312"
+    sha256 cellar: :any_skip_relocation, monterey: "6a289889d2c91f3e32dbe08bf240d851f6302b62db797b13eba545f3e86b9010"
   end
 
   deprecate! date: "2025-12-08", because: :unsupported
