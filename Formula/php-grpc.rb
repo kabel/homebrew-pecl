d4539aaaa591faa5d7ee3ece557419d4e0184825
Kevin Abel <kevin.abel.0@gmail.com>
2024-03-06 21:28:26 -0600

    php-grpc: update 1.62.0 bottle.

--- Formula/php-grpc.rb
+++ Formula/php-grpc.rb
@@ -9,8 +9,8 @@ class PhpGrpc < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:   "d1b629551e9625db793d5f6461bcbe17069461d70d38ad74263d763117350148"
-    sha256 cellar: :any_skip_relocation, ventura:  "ee6f232b1854ae6de6aaaf31b7960db4337ddd8544cf9d7b6b0e52cd5d9297dc"
-    sha256 cellar: :any_skip_relocation, monterey: "3cde02d143d711cdad5334d4c8ac1a9b6854d17386abf3774bf1c433e4fa08f2"
+    sha256 cellar: :any_skip_relocation, sonoma:   "58b33b9c56c3626bde1abf9c7be86554aef626f133a3ffeeaadeb872eb7b9768"
+    sha256 cellar: :any_skip_relocation, ventura:  "634ac4586fb3e547823deaa65c84a2465621aebd43a76d8d3da592bb11abb88c"
+    sha256 cellar: :any_skip_relocation, monterey: "4e841b9b58a5f9de8e8b68621586d5004166956132b0fcd44f50ed1f9e106f12"
   end
 end
