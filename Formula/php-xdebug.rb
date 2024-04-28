f55d1375c61096119cb416eabf344b2342ebe5da
Kevin Abel <kevin.abel.0@gmail.com>
2024-04-28 18:30:44 -0500

    php-xdebug: update 3.3.2 bottle.

--- Formula/php-xdebug.rb
+++ Formula/php-xdebug.rb
@@ -9,9 +9,9 @@ class PhpXdebug < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 sonoma:   "c3c8d0e09e468ea8cc4175e6e9cb491902a1c7523844e0d6f699edf86d89c2eb"
-    sha256 ventura:  "45d13e4cff83546476cc8a3154e2406a3e7e1834b837c2eac293d45f6e8733c0"
-    sha256 monterey: "49d35f84069a5d53e2564bc4899300bae8d3f0867271d6a32c23a28f52d26ca5"
+    sha256 sonoma:   "2b8dac6893e2af37c2d35a075e636fc66e6f8d090bf7ecd2baed2767e3653785"
+    sha256 ventura:  "a0b28626256d1d79e6feaf740c5fa8a898f8922c20004252c2adcd3b7eff38ab"
+    sha256 monterey: "43da15e250a5b1794ad3a4260eac8e8bc9d9c2628b6bdfdf921c9e3e3bcdab2e"
   end
 
   def extension_type
