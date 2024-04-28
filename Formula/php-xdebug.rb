fb4549ea08f1b12c110f943bb075d2972bf1079d
Kevin Abel <kevin.abel.0@gmail.com>
2024-04-28 13:26:18 -0500

    php-xdebug 3.3.2

--- Formula/php-xdebug.rb
+++ Formula/php-xdebug.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpXdebug < PhpPeclFormula
   extension_dsl "An extension to assist with debugging and development"
 
-  url "https://pecl.php.net/get/xdebug-3.3.1.tgz"
-  sha256 "4eb4ee270bbcc5f14195c38f6ee58580e007cf4886ce32e11430318ab5bc2315"
+  url "https://pecl.php.net/get/xdebug-3.3.2.tgz"
+  sha256 "15545b8311bedb5e7df08fd90d6653d9bf33629791e8119e70350475df932c55"
   license "PHP-3.0"
 
   bottle do
