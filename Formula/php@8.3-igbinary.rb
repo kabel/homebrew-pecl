786bbf12258f12b9faa48f18216dfe8af84b5970
Björn Häuser <b.haeuser@rebuy.com>
2024-12-05 10:46:41 +0100

    Add php 8.3 formulas

---
+++ Formula/php@8.3-igbinary.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Igbinary < PhpPeclFormula
+  extension_dsl "Communicate with any AMQP compliant server"
+
+  url "https://pecl.php.net/get/igbinary-3.2.15.tgz"
+  sha256 "eff099b0343b45fbe9765d4b3d441064ddefbbf9cfb7198487de9bda6b8f4907"
+  license "BSD-3-Clause"
+
+  conf_order "05"
+end
---
+++ Formula/php@8.3-redis.rb
@@ -0,0 +1,32 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Redis < PhpPeclFormula
+  extension_dsl "Redis extension"
+
+  url "https://pecl.php.net/get/redis-6.1.0.tgz"
+  sha256 "f10405f639fe415e9ed4ec99538e72c90694d8dbd62868edcfcd6a453466b48c"
+  license "PHP-3.01"
+
+  depends_on "lz4"
+  depends_on "rebuy-de/pecl/php@8.3-igbinary"
+  depends_on "rebuy-de/pecl/php@8.3-msgpack"
+
+  configure_arg %W[
+    --enable-redis-igbinary
+    --enable-redis-lz4
+    --enable-redis-lzf
+    --enable-redis-msgpack
+    --enable-redis-zstd
+    --with-liblz4=#{Formula["lz4"].opt_prefix}
+  ]
+
+  def install
+    # fix include location for configure to find
+    mkdir_p buildpath/source_dir/"include/php/ext"
+    ln_s Formula["kabel/pecl/php@8.3-igbinary"].opt_include/"php/ext/igbinary",
+      buildpath/source_dir/"include/php/ext/igbinary"
+    ln_s Formula["kabel/pecl/php@8.3-msgpack"].opt_include/"php/ext/msgpack",
+      buildpath/source_dir/"include/php/ext/msgpack"
+    super
+  end
+end
