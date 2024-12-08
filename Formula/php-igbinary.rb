c896abd6a646cce9e9b47a12f812f92416129d26
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 16:15:11 -0600

    php-igbinary: add 3.2.16 bottle.

--- Formula/php-igbinary.rb
+++ Formula/php-igbinary.rb
@@ -7,5 +7,11 @@ class PhpIgbinary < PhpPeclFormula
   sha256 "8bf25d465abc7973d9e2c9a3039a5f8eea635b23bc1477017ff3999ff95836da"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "0495ae5fc2d0cb56da55bac18f855e8e28fa9ece6c7f4bd3a6d4087da86e45b5"
+    sha256 cellar: :any_skip_relocation, ventura: "d9d04cf91bc38098873dabcd0d629363bd50e5be0fd16dac4cea61d43690e047"
+  end
+
   conf_order "05"
 end
