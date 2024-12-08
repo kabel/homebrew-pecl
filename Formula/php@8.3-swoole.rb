c6856e1ea7ecbfefa50b34df75af31eb618dcd1f
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:50:45 -0600

    php@8.3-swoole: add 5.1.6 bottle.

--- Formula/php@8.3-swoole.rb
+++ Formula/php@8.3-swoole.rb
@@ -7,6 +7,12 @@ class PhpAT83Swoole< PhpPeclFormula
   sha256 "335bf67aba33789450701b36fb21d5b46d72f837de92def2f80d4a0090eb57c8"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "aafe3fe6e2753ea6201b3043e8c9aba70df858682ca955e26802e9a267e9d954"
+    sha256 cellar: :any, ventura: "eb0e2ea3666853ff4f07b0a82a176444f4d76e0826d2ea6fcfad517fc1e02e80"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "brotli"
