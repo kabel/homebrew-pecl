81acf33bb9d21a4271e181d6b4e5e5f391bda6bf
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 14:40:01 -0600

    php-xdebug: add 3.4.0 bottle.

--- Formula/php-xdebug.rb
+++ Formula/php-xdebug.rb
@@ -7,6 +7,12 @@ class PhpXdebug < PhpPeclFormula
   sha256 "89667b8d04aaf04c023eb109900e1cce97ca39f97f2f3f24199630cc0e1cc77d"
   license "PHP-3.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 sonoma:  "5d872dcb26cfa3252e282b2235b4a717aaa82c506358cfaca5d4a2533a3153e2"
+    sha256 ventura: "6eca24c9701d0d67ab7d1b13c13e94c2adfe9704776fed0afbbeb127d8270d73"
+  end
+
   def extension_type
     "zend_extension"
   end
