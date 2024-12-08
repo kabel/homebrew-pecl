2c804af1250e876ac07b184fab8d74e1d195254d
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:21:09 -0600

    php-mongodb: add 1.20.1 bottle.

--- Formula/php-mongodb.rb
+++ Formula/php-mongodb.rb
@@ -7,6 +7,12 @@ class PhpMongodb < PhpPeclFormula
   sha256 "614e57594918feb621f525e6516d59ce09b78f5172355ba8afb6c2207c1ce900"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "5e008e43bbd445f6849bc0c49291e73e02ae5dfe3dd88574a0716e1e9084f6fe"
+    sha256 cellar: :any, ventura: "2c218ad7fe472f9a65978c7212cef5186c461ccb5323887596d3452ca174a2ba"
+  end
+
   depends_on "icu4c"
   depends_on "openssl@3"
   depends_on "snappy"
