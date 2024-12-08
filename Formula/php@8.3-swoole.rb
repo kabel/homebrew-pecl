f79f18c5e155ba21c8b9467d355e0aae77500722
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:19:53 -0600

    php@8.3-swoole 5.1.6 (New Formula)

---
+++ Formula/php@8.3-swoole.rb
@@ -0,0 +1,23 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Swoole< PhpPeclFormula
+  extension_dsl "Event-driven asynchronous and concurrent networking engine with high performance"
+
+  url "https://pecl.php.net/get/swoole-5.1.6.tgz"
+  sha256 "335bf67aba33789450701b36fb21d5b46d72f837de92def2f80d4a0090eb57c8"
+  license "Apache-2.0"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  depends_on "brotli"
+  depends_on "libnghttp2"
+  depends_on "openssl@3"
+
+  configure_arg %W[
+    --enable-http2
+    --enable-mysqlnd
+    --enable-sockets
+    --with-openssl-dir=#{Formula["openssl@3"].opt_prefix}
+    --with-nghttp2_dir=#{Formula["libnghttp2"].opt_prefix}
+  ]
+end
