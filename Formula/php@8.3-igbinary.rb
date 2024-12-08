5160ad61007455c275532fea7ac9d19646dc2566
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 16:15:14 -0600

    php@8.3-igbinary: add 3.2.16 bottle.

--- Formula/php@8.3-igbinary.rb
+++ Formula/php@8.3-igbinary.rb
@@ -7,6 +7,12 @@ class PhpAT83Igbinary < PhpPeclFormula
   sha256 "8bf25d465abc7973d9e2c9a3039a5f8eea635b23bc1477017ff3999ff95836da"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "49199f98f93bcd02123bd6d9ec6a6c392e81b330d179284c352e7ed39ac6819c"
+    sha256 cellar: :any_skip_relocation, ventura: "d7c766b2197deee9b6c270b828d610bf402240251612d7237cb981bc043b1238"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   conf_order "05"
