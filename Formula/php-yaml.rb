b86eb3be5510ffd3f2d4afde203c0252f9b415c0
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:57:29 -0600

    php-yaml: add 2.2.4 bottle.

--- Formula/php-yaml.rb
+++ Formula/php-yaml.rb
@@ -7,6 +7,12 @@ class PhpYaml < PhpPeclFormula
   sha256 "8eb353baf87f15b1b62ac6eb71c8b589685958a1fe8b0e3d22ac59560d0e8913"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "940080cb3182a832f9addcf8d7be35afacadfd98540d058e0896b959d845c834"
+    sha256 cellar: :any, ventura: "bb437152654428f6b1da9275d493291e044b42636d97517c080eeed1aa48f42c"
+  end
+
   depends_on "libyaml"
 
   configure_arg "--with-yaml=#{Formula["libyaml"].opt_prefix}"
