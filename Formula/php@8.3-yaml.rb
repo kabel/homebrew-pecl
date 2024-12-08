4e18e5ef3b8e107820398394423205a1800ad73a
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:57:32 -0600

    php@8.3-yaml: add 2.2.4 bottle.

--- Formula/php@8.3-yaml.rb
+++ Formula/php@8.3-yaml.rb
@@ -7,6 +7,12 @@ class PhpAT83Yaml < PhpPeclFormula
   sha256 "8eb353baf87f15b1b62ac6eb71c8b589685958a1fe8b0e3d22ac59560d0e8913"
   license "MIT"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "7a326ebf1adb98e0b7e19f7f06b66b579b62f0f69fdf48a3179c13e6ed39328e"
+    sha256 cellar: :any, ventura: "febafd53d9a16d095b5f6b569987927ab29e480bfe2f7fe9547a2d3821e690b2"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "libyaml"
