b0491c6b08269818bb9951d1f9c01f01eeb5f220
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 15:55:45 -0600

    php-igbinary 3.2.16

--- Formula/php@8.3-igbinary.rb
+++ Formula/php@8.3-igbinary.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT83Igbinary < PhpPeclFormula
   extension_dsl "Communicate with any AMQP compliant server"
 
-  url "https://pecl.php.net/get/igbinary-3.2.15.tgz"
-  sha256 "eff099b0343b45fbe9765d4b3d441064ddefbbf9cfb7198487de9bda6b8f4907"
+  url "https://pecl.php.net/get/igbinary-3.2.16.tgz"
+  sha256 "8bf25d465abc7973d9e2c9a3039a5f8eea635b23bc1477017ff3999ff95836da"
   license "BSD-3-Clause"
 
   deprecate! date: "2027-12-31", because: :unsupported
