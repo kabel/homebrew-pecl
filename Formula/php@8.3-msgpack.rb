7f22ec3ce73cd39fdc194ecf01a8ebe4b739d36c
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 15:49:04 -0600

    php@8.3-msgpack: add 2.2.0 bottle.

--- Formula/php@8.3-msgpack.rb
+++ Formula/php@8.3-msgpack.rb
@@ -7,6 +7,12 @@ class PhpAT83Msgpack < PhpPeclFormula
   sha256 "82aa1e404c5ff54ec41d2a201305cd6594ed14a7529e9119fa7ca457e4bbd12a"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "e078e8d09aff3d42273875bf163cef18ff2e6b9912075a8edf72493c46c096d4"
+    sha256 cellar: :any_skip_relocation, ventura: "ef53fe77d911f928f420cb8645bfd3e31301749da5c71b2ca6bd1ac83e11afc7"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "msgpack"
