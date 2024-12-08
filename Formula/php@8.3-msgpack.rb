aa42e066e89dc893fb6744a2ea29d63daba64643
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 15:24:53 -0600

    php@8.3-msgpack 2.2.0 (New Formula)

---
+++ Formula/php@8.3-msgpack.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Msgpack < PhpPeclFormula
+  extension_dsl "Interfacing with MessagePack"
+
+  url "https://pecl.php.net/get/msgpack-2.2.0.tgz"
+  sha256 "82aa1e404c5ff54ec41d2a201305cd6594ed14a7529e9119fa7ca457e4bbd12a"
+  license "PHP-3.01"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  depends_on "msgpack"
+
+  conf_order "05"
+end
