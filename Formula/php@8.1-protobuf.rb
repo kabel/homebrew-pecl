e74bad0f8b968553ad17ac19d411536d5a68cc01
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:06:42 -0600

    php-protobuf 4.29.1

--- Formula/php@8.1-protobuf.rb
+++ Formula/php@8.1-protobuf.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Protobuf < PhpPeclFormula
   extension_dsl "Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
 
-  url "https://pecl.php.net/get/protobuf-3.25.3.tgz"
-  sha256 "daf75724966e0e83856f90c3179a50f681eb3591f3af1fb57e104b8f3d2964e4"
+  url "https://pecl.php.net/get/protobuf-4.29.1.tgz"
+  sha256 "12cc3b38d055feb2eb4c6b4ca6e2c1e080906d06e9173eea2cc160ecd9d6374b"
   license "BSD-3-Clause"
 
   deprecate! date: "2025-12-31", because: :unsupported
