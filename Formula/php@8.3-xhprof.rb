5bfc9ef1ffcf07dfa7b3bfecc8d8eae93b03920b
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:39:05 -0600

    php@8.3-xhprof 2.3.10 (New Formula)

---
+++ Formula/php@8.3-xhprof.rb
@@ -0,0 +1,13 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Xhprof < PhpPeclFormula
+  extension_dsl "XHProf: A Hierarchical Profiler for PHP"
+
+  url "https://pecl.php.net/get/xhprof-2.3.10.tgz"
+  sha256 "251aee99c2726ebc6126e1ff0bb2db6e2d5fd22056aa335e84db9f1055d59d95"
+  license "Apache-2.0"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  source_dir "#{extension}-#{version}/extension"
+end
