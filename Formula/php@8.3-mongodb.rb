53d9648b99133fa4e3218d92459fdc320caa788e
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:21:11 -0600

    php@8.3-mongodb: add 1.20.1 bottle.

--- Formula/php@8.3-mongodb.rb
+++ Formula/php@8.3-mongodb.rb
@@ -7,6 +7,12 @@ class PhpAT83Mongodb < PhpPeclFormula
   sha256 "614e57594918feb621f525e6516d59ce09b78f5172355ba8afb6c2207c1ce900"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "555ad118bc4ccd1015bd62da46273f4698cbb38fc80a0e6d50a6b0d27ee81f5e"
+    sha256 cellar: :any, ventura: "6b01ba6d8a767af484285862a5f8add3dc3855f0c4ba12d4e543d9208698697c"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "icu4c"
