c460d3347697f5294e10cabb76a82b561401b9c7
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 18:56:50 -0600

    php@8.2-msgpack: update 3.0.0 bottle.

--- Formula/php@8.2-msgpack.rb
+++ Formula/php@8.2-msgpack.rb
@@ -9,9 +9,8 @@ class PhpAT82Msgpack < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:   "f41f67dfc7ff7ad476b934c7c50dcc31ca568ff57933b1707b6be4f1890c0834"
-    sha256 cellar: :any_skip_relocation, ventura:  "9daf8dded6519a8e2e6f58029cf1b06bed01f5d9ce12d927fb89cb3bef3119bc"
-    sha256 cellar: :any_skip_relocation, monterey: "84e8685c99e9bbd100aba814eb98b1c90a67d418626f9c3c292d4fd2fe70b9ee"
+    sha256 cellar: :any_skip_relocation, sonoma:  "243d0ba00a9cd2498bdcbfe613efc33fb63a9a5a0830f78bb158403918f9f186"
+    sha256 cellar: :any_skip_relocation, ventura: "4e127f44496fe2c5a76132629c8796a5e8d94aedec6102ae4cab89cd4195e38b"
   end
 
   deprecate! date: "2026-12-31", because: :unsupported
