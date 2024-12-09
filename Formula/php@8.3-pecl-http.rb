242cfe0d58de4ea18ff63fc6bcbb60d33eb2ebb9
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 20:35:50 -0600

    php@8.3-pecl-http: add 4.2.6 bottle.

--- Formula/php@8.3-pecl-http.rb
+++ Formula/php@8.3-pecl-http.rb
@@ -7,6 +7,12 @@ class PhpAT83PeclHttp < PhpPeclFormula
   sha256 "cd33230050b3f7c5ddb6f4383ce2a81f0bcdb934432029eec72ebf0f942b876d"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "e40938cbae76d03c56999529bdc089cdd8144c843d6f9182e3f5513928af17d1"
+    sha256 cellar: :any, ventura: "6f7d02d8a23d8188f0e15b9ac08900cf2766970ba2b51ef07164545dfa7b5ae9"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "brotli"
