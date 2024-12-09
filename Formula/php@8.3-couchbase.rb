34ac1c9db2f66ad8e53c04cd73052c76dda6b1ec
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 21:39:37 -0600

    php@8.3-couchbase: add 4.2.5 bottle.

--- Formula/php@8.3-couchbase.rb
+++ Formula/php@8.3-couchbase.rb
@@ -7,6 +7,12 @@ class PhpAT83Couchbase < PhpPeclFormula
   sha256 "5b5d830ce2eadb551a251070082b910ccaedd9fab9dc5c554a0bd98b7e50ca5f"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "2162cf4ca5c05e4379c2d85ffd9b4e5ba10e295479ce947f6d93cd212ee1cd7f"
+    sha256 cellar: :any, ventura: "184452e953185b4ec2b13390b9f1e009dc42a2de8e1f978001dc6fbf9ef36f39"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 
   depends_on "cmake" => :build
