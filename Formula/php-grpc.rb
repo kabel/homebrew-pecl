84f3a9b32cec3c670331c9235781f33fc4612343
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 19:11:49 -0600

    php-grpc: add 1.68.0 bottle.

--- Formula/php-grpc.rb
+++ Formula/php-grpc.rb
@@ -6,4 +6,10 @@ class PhpGrpc < PhpPeclFormula
   url "https://pecl.php.net/get/grpc-1.68.0.tgz"
   sha256 "4e022e052d5b7997efd42f3b67f1175dbbb772cfd435570852febc0f569065bb"
   license "Apache-2.0"
+
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "024e4ebdbcc0bb4da3322e01b78a385ecde529ab7ca5b25d212a951a105f8929"
+    sha256 cellar: :any_skip_relocation, ventura: "d44c9adcb50d1949f910e7bc10117a9e600601bb417453c11a169ae0d15ef503"
+  end
 end
