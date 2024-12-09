5287a7c406832e1090ab1e4bb8d69ca8e112aa8e
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 21:46:29 -0600

    php-imagick: Revision and patch for 8.4

--- Formula/php-imagick.rb
+++ Formula/php-imagick.rb
@@ -5,8 +5,14 @@ class PhpImagick < PhpPeclFormula
 
   url "https://pecl.php.net/get/imagick-3.7.0.tgz"
   sha256 "5a364354109029d224bcbb2e82e15b248be9b641227f45e63425c06531792d3e"
-  revision 3
+  revision 4
   license "PHP-3.01"
 
   depends_on "imagemagick"
+
+  # See https://github.com/Imagick/imagick/pull/690
+  patch do
+    url "https://gist.githubusercontent.com/kabel/4594046009421e35eec23336ad52c186/raw/3f8b0daf523420d3b0518f55c5789a1882f7a91f/pecl-imagick-690.patch"
+    sha256 "33570d6536328790eb4594656b593ac428426874952567a164fc6139670bd0df"
+  end
 end
