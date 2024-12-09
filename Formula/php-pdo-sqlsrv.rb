0ed51529eb7b566a32eab6ca474ec78a251d650b
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 21:52:05 -0600

    php-pdo-sqlsrv: Revision for 8.4

--- Formula/php-pdo-sqlsrv.rb
+++ Formula/php-pdo-sqlsrv.rb
@@ -5,6 +5,7 @@ class PhpPdoSqlsrv < PhpPeclFormula
 
   url "https://pecl.php.net/get/pdo_sqlsrv-5.12.0.tgz"
   sha256 "22f0cb17b45f0deccd0bba072ee0085ff4094cd6ee2acc26f7f924975ef652c6"
+  revision 1
   license "MIT"
 
   depends_on "unixodbc"
