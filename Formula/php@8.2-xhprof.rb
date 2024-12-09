f8e14922d321652467e13562712aa6ca22882f3f
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 18:03:50 -0600

    php@8.2-xhprof: update 2.3.10 bottle.

--- Formula/php@8.2-xhprof.rb
+++ Formula/php@8.2-xhprof.rb
@@ -9,9 +9,8 @@ class PhpAT82Xhprof < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:   "9c81483bd51d2d3a481ebbb935b5ec201857663b1f6a1ac7499c92599363612a"
-    sha256 cellar: :any_skip_relocation, ventura:  "f7877a112e16cb16ce47a67a272ccd51ba6a962e7f44200d7d86db70fee71822"
-    sha256 cellar: :any_skip_relocation, monterey: "75fedae55e899be7af69035107abd8e10d0561e5e2b35854577a3e28010d3de7"
+    sha256 cellar: :any_skip_relocation, sonoma:  "23a6d37d7ed10cf20e4c35f7d187fdb8570d4c112843d4a620fd16ea9f111a0b"
+    sha256 cellar: :any_skip_relocation, ventura: "2baa0118ab30392b1a36dab8a11d7b4c040253fc0d4589f5fbbb9db20b83e73c"
   end
 
   deprecate! date: "2026-12-31", because: :unsupported
