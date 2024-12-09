c87ae07e6557704e046fd3898348ffa85ded998f
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 18:13:04 -0600

    php-grpc 1.68.0

--- Formula/php-grpc.rb
+++ Formula/php-grpc.rb
@@ -3,7 +3,7 @@ require_relative "../lib/php_pecl_formula"
 class PhpGrpc < PhpPeclFormula
   extension_dsl "A high performance, open source, general RPC framework that puts mobile and HTTP/2 first"
 
-  url "https://pecl.php.net/get/grpc-1.62.0.tgz"
-  sha256 "ceabf3c564cd3d61ca7a9a06ebdde777322e50701a454f1c5d8a5291afe59302"
+  url "https://pecl.php.net/get/grpc-1.68.0.tgz"
+  sha256 "4e022e052d5b7997efd42f3b67f1175dbbb772cfd435570852febc0f569065bb"
   license "Apache-2.0"
 end
