627b36bbf997397496e7d9ae5a3a07ddb240df1b
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 19:11:52 -0600

    php@8.3-grpc: add 1.68.0 bottle.

--- Formula/php@8.3-grpc.rb
+++ Formula/php@8.3-grpc.rb
@@ -7,5 +7,11 @@ class PhpAT83Grpc < PhpPeclFormula
   sha256 "4e022e052d5b7997efd42f3b67f1175dbbb772cfd435570852febc0f569065bb"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "0cff3fea0df8efdd68922c6191c1ca8acc4f446eefa67c46ad62d75543c8ced0"
+    sha256 cellar: :any_skip_relocation, ventura: "0ccbba2b50010938fed571cdbbddab31d24cbbf9bdc1d66a73ec04a981af94d2"
+  end
+
   deprecate! date: "2027-12-31", because: :unsupported
 end
