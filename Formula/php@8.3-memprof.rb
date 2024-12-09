0728d1d538b46fe1f6743f8c42e5473f04aa7f34
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 18:16:23 -0600

    php@8.3-memprof 3.0.2 (New Formula)

---
+++ Formula/php@8.3-memprof.rb
@@ -0,0 +1,31 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Memprof < PhpPeclFormula
+  extension_dsl <<~EOS
+    Memprof is a fast and accurate memory profiler that can be used
+    to find the cause of memory leaks in PHP applications
+  EOS
+
+  url "https://pecl.php.net/get/memprof-3.0.2.tgz"
+  sha256 "510bf09d58487820ac11b617446199ca86c50fda972dde73feaae667be23d183"
+  license "MIT"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  resource "judy" do
+    url "https://downloads.sourceforge.net/project/judy/judy/Judy-1.0.5/Judy-1.0.5.tar.gz"
+    sha256 "d2704089f85fdb6f2cd7e77be21170ced4b4375c03ef1ad4cf1075bd414a63eb"
+  end
+
+  def install
+    resource("judy").stage do
+      system "./configure", "--prefix=#{prefix}/judy"
+      ENV.deparallelize do
+        system "make", "install"
+      end
+    end
+
+    configure_args << "--with-judy-dir=#{prefix}/judy"
+    super
+  end
+end
