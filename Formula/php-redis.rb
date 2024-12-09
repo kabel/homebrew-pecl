dcc37390bef36bdb1067a30d00befdbf297e38c7
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 22:33:41 -0600

    php-redis: add 6.1.0 bottle.

--- Formula/php-redis.rb
+++ Formula/php-redis.rb
@@ -7,6 +7,12 @@ class PhpRedis < PhpPeclFormula
   sha256 "f10405f639fe415e9ed4ec99538e72c90694d8dbd62868edcfcd6a453466b48c"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "f4d3e0463a71015169a64b190f621b8d4c4cc6c46159afb5ff48e85c122a4ca9"
+    sha256 cellar: :any, ventura: "849bedeb8948c79310a8ed6ba0bee42daafe4b011a4aa678710e34eee9d88ab9"
+  end
+
   depends_on "lz4"
   depends_on "zstd"
   depends_on "kabel/pecl/php-igbinary"
