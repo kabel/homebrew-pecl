2d83af6deecf96b997122821acc5241494128568
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 20:03:20 -0600

    php@8.3-couchbase 4.2.5 (New Formula)

---
+++ Formula/php@8.3-couchbase.rb
@@ -0,0 +1,14 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Couchbase < PhpPeclFormula
+  extension_dsl "Couchbase Server"
+
+  url "https://pecl.php.net/get/couchbase-4.2.5.tgz"
+  sha256 "5b5d830ce2eadb551a251070082b910ccaedd9fab9dc5c554a0bd98b7e50ca5f"
+  license "Apache-2.0"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  depends_on "cmake" => :build
+  depends_on "openssl@3"
+end
