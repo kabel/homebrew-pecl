30fc3aeff833e8ef3dcf4d4726a6ba0412b158c0
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 22:06:21 -0600

    php-ds: add 1.5.0_1 bottle.

--- Formula/php-ds.rb
+++ Formula/php-ds.rb
@@ -7,4 +7,10 @@ class PhpDs < PhpPeclFormula
   sha256 "2b2b45d609ca0958bda52098581ecbab2de749e0b3934d729de61a59226718b0"
   revision 1
   license "MIT"
+
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "54542cf011d3fa1b33e855aadb37a539584f15a047531cac9624b1400d98771a"
+    sha256 cellar: :any_skip_relocation, ventura: "3c7c0893bce3eb53cef4bd3f7e775fe873335a8017313a6c2bde8b7e216dea4c"
+  end
 end
