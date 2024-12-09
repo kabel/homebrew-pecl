dab21a927ae5e25a5b961df52964cc111540e1ae
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 19:12:55 -0600

    php-raphf: add 2.0.1_6 bottle.

--- Formula/php-raphf.rb
+++ Formula/php-raphf.rb
@@ -8,5 +8,11 @@ class PhpRaphf < PhpPeclFormula
   revision 6
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any_skip_relocation, sonoma:  "69e83d184d95a7fd0c697908b261612db98144bec35d6e6a5574cdff26b8a2cd"
+    sha256 cellar: :any_skip_relocation, ventura: "1aadf9c13a614b54b23cee70c87854dd603bfc9c510c1490d3a91ebb978dd68f"
+  end
+
   conf_order "05"
 end
