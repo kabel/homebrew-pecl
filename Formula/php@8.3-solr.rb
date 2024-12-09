7a4de417264e7fefdfb221b20c0ce3af6e2f95c4
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 21:56:01 -0600

    php@8.3-solr 2.7.0 (New Formula)

---
+++ Formula/php@8.3-solr.rb
@@ -0,0 +1,22 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Solr < PhpPeclFormula
+  extension_dsl <<~EOS
+    The Apache Solr PHP extension is an extremely fast,
+    light-weight, feature-rich library that allows PHP applications to
+    communicate easily and efficiently with Apache Solr server instances
+    using an object-oriented API
+  EOS
+
+  url "https://pecl.php.net/get/solr-2.7.0.tgz"
+  sha256 "bfbf7fad1a8738ae5518bd664ca963ea8584cc87ed9d813df29a82170b071ea2"
+  license "PHP-3.01"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+
+  depends_on "curl"
+
+  configure_arg %W[
+    --with-curl=#{Formula["curl"].opt_prefix}
+  ]
+end
