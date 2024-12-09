97da51a848c4e255e3a838c60a7b6b35cee3d2a5
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 21:57:33 -0600

    php-sqlsrv: Revision for 8.4

--- Formula/php-sqlsrv.rb
+++ Formula/php-sqlsrv.rb
@@ -5,6 +5,7 @@ class PhpSqlsrv < PhpPeclFormula
 
   url "https://pecl.php.net/get/sqlsrv-5.12.0.tgz"
   sha256 "a9ebb880b2a558d3d6684f6e6802c53c5bffa49e1ee60d1473a7124fc9cb72ad"
+  revision 1
   license "MIT"
 
   depends_on "unixodbc"
