1e9461a385dd7d746d061e74418e8c83cf9f9023
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 18:18:17 -0600

    php@8.3-translit 0.7.1 (New Formula)

---
+++ Formula/php@8.3-translit.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT83Translit < PhpPeclFormula
+  extension_dsl "Transliterates non-latin character sets to latin"
+
+  url "https://pecl.php.net/get/translit-0.7.1.tgz"
+  sha256 "60a914a64416381c8e2b00ccf13188a872bca3e80cc6cc4ab6d7f0af949365b8"
+  license "BSD-2-Clause"
+
+  deprecate! date: "2027-12-31", because: :unsupported
+end
