7f04acccb86425af3f4f5f65c8e755947deae860
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-21 15:38:33 -0600

    php-swoole: add 6.0.0 bottle.

--- Formula/php-swoole.rb
+++ Formula/php-swoole.rb
@@ -7,6 +7,12 @@ class PhpSwoole< PhpPeclFormula
   sha256 "e9bc196f2678fb42f7b3e38e5489b5b20f3dba50118b9669f124b77d05ea7c5a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "6534ba3d1aa01f41bceb65704c6279ac8d09e7bd24f55c944f62124189acf53f"
+    sha256 cellar: :any, ventura: "0180381a9a69f2f566b22a2f5fa00bc6d1ac7e15ca8176f2e30bcdc49f1f104b"
+  end
+
   conflicts_with "php-openswoole", because: "both provide the same PHP API"
 
   depends_on "brotli"
