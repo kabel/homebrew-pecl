3ca67c55ccd85d2472268b075bfa05da457482f3
Kevin Abel <kevin.abel.0@gmail.com>
2025-03-09 19:11:37 -0500

    php-grpc 1.70.0

--- Formula/php@8.1-grpc.rb
+++ Formula/php@8.1-grpc.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT81Grpc < PhpPeclFormula
   extension_dsl "A high performance, open source, general RPC framework that puts mobile and HTTP/2 first"
 
-  url "https://pecl.php.net/get/grpc-1.68.0.tgz"
-  sha256 "4e022e052d5b7997efd42f3b67f1175dbbb772cfd435570852febc0f569065bb"
+  url "https://pecl.php.net/get/grpc-1.70.0.tgz"
+  sha256 "11336d7bc4465148db506348006dd5559ce478eee4bf1b080bb31b89de6974b7"
   license "Apache-2.0"
 
   bottle do
