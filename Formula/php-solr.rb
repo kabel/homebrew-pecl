e43d18eb17bf9f64afdaff805d1087f31826a92c
Kevin Abel <kevin.abel.0@gmail.com>
2025-03-09 19:28:03 -0500

    php-solr: add 2.8.0 bottle.

--- Formula/php-solr.rb
+++ Formula/php-solr.rb
@@ -12,6 +12,12 @@ class PhpSolr < PhpPeclFormula
   sha256 "092b52a7b94a807e40f8d85032c85dd0c052fab31d4f518e1d756b9f2d42d4b8"
   license "PHP-3.01"
 
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+    sha256 cellar: :any, sonoma:  "b35b6ec33b3245c06efac9f6329a07dbc79090af7b43e7e30c27f9cc0cf0256f"
+    sha256 cellar: :any, ventura: "875a70da47d72571f9fb68e1d4b2789ce53026dcfb038f71118f5657a95c0e0f"
+  end
+
   depends_on "curl"
 
   configure_arg %W[
