7d772d8b95a04d1bccd011c2177c7d38e9f685e2
Kevin Abel <kevin.abel.0@gmail.com>
2025-03-09 19:28:04 -0500

    php@8.1-solr: update 2.8.0 bottle.

--- Formula/php@8.1-solr.rb
+++ Formula/php@8.1-solr.rb
@@ -14,9 +14,8 @@ class PhpAT81Solr < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, sonoma:   "1b841b1429552736e9bce2e679ba6e8797bf68a130081a9be167cb89db9e8038"
-    sha256 cellar: :any, ventura:  "5b6d46ef2229866fb680a5fdcdd19c68597e94d655954e70eb01a898678f5800"
-    sha256 cellar: :any, monterey: "38974938a91496835b18ebb0f7f74e4af5e8e3e5d676062892d2993e166c24de"
+    sha256 cellar: :any, sonoma:  "d4b93e13b68936b3da8f6e2d78c8a6606e5bc6350e43ea642a11746573f97306"
+    sha256 cellar: :any, ventura: "609c3a88933f826624cbc608acd0379ca8d3d61c6252acb4f7f6c839f5d1c7ec"
   end
 
   deprecate! date: "2025-12-31", because: :unsupported
