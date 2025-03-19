22c3a69adc0a52314c122227afffb6bf1e85f06f
Elan Ruusamäe <glen@delfi.ee>
2025-03-19 17:57:04 +0200

    Add phalcon formulas for php 8.1-8.4

---
+++ Formula/php@8.2-phalcon.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/php_pecl_formula"
+
+class PhpAT82Phalcon < PhpPeclFormula
+  extension_dsl "Phalcon is a full stack PHP framework offering low resource consumption and high performance"
+
+  url "https://pecl.php.net/get/phalcon-5.9.0.tgz"
+  sha256 "7b8622bf7df5385e159dde3f41ed91bc6798726d8d725a46db8ba884651664d0"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://bottles.kabel.rocks/bottles-pecl"
+  end
+
+  deprecate! date: "2026-12-31", because: :unsupported
+end
