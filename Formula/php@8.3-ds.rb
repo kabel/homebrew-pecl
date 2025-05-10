29116da3953a0e2709201c19c20c7c2777d0a080
Kevin Abel <kevin.abel.0@gmail.com>
2025-05-10 14:16:21 -0500

    php@8.3-ds: update 1.6.0 bottle.

--- Formula/php@8.3-ds.rb
+++ Formula/php@8.3-ds.rb
@@ -9,8 +9,8 @@ class PhpAT83Ds < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:  "70d94e280bc14b8fda4cbf62c0fd6f1f4d1adb740cb469d347ed4022c86d5d6f"
-    sha256 cellar: :any_skip_relocation, ventura: "df513a6bfc1f555966a318a89cb54777a5714e1372c780dbf22db1cd9f295355"
+    sha256 cellar: :any_skip_relocation, sonoma:  "31cbbda243c95d520c2a26ae59e62112f917219ff8ad7dd2db02e1a869bc4f5a"
+    sha256 cellar: :any_skip_relocation, ventura: "b00dd9a4fa1032004bc95ca09a4c8d03a808d277d82791cc1ad4edc55e5ae8b6"
   end
 
   deprecate! date: "2027-12-31", because: :unsupported
