198ff309ea14475447eae9678088a2bbe3aec093
Kevin Abel <kevin.abel.0@gmail.com>
2025-05-10 13:45:19 -0500

    php-brotli 0.16.0

--- Formula/php@8.2-brotli.rb
+++ Formula/php@8.2-brotli.rb
@@ -4,8 +4,8 @@ class PhpAT82Brotli < PhpPeclFormula
   extension_dsl "Brotli compression"
   homepage "https://github.com/kjdev/php-ext-brotli"
 
-  url "https://github.com/kjdev/php-ext-brotli/archive/refs/tags/0.15.2.tar.gz"
-  sha256 "b5e41174652993d516f14243873d217f46dd2d241252f12ccf899cbdbbfa693e"
+  url "https://github.com/kjdev/php-ext-brotli/archive/refs/tags/0.16.0.tar.gz"
+  sha256 "2ddd7d151974c44b461b1418ee69067ebb4e37183b2217ea7864c74f2d762d51"
   license "MIT"
 
   bottle do
