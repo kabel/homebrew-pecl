d1676426158703560464738c9b036f489d038979
Kevin Abel <kevin.abel.0@gmail.com>
2025-05-10 14:19:00 -0500

    php@8.2-phalcon: update 5.9.3 bottle.

--- Formula/php@8.2-phalcon.rb
+++ Formula/php@8.2-phalcon.rb
@@ -9,8 +9,8 @@ class PhpAT82Phalcon < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:  "097fbcdb6b754d824870897b89f4a18154271f5180dff2e3f874697190ee60c4"
-    sha256 cellar: :any_skip_relocation, ventura: "73392a823cc057d0ef47a86fa6062e0e22ac408c35eee0ec6b9db6722e1e8aa6"
+    sha256 cellar: :any_skip_relocation, sonoma:  "9601e4dd5d6166156aa57cbaaadc2ad6f5e5ca7c1fdfb206588631182d569a4c"
+    sha256 cellar: :any_skip_relocation, ventura: "2d01f45f7f198f99399053b29c9d9e6a732f78a7efc93770d69db4e30ef8d607"
   end
 
   deprecate! date: "2026-12-31", because: :unsupported
