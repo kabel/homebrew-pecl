8afd97d63c2238fbb1988f7e8b952bc0b24cd854
Kevin Abel <kevin.abel.0@gmail.com>
2025-08-30 15:25:51 -0500

    php@8.1-brotli: update 0.18.2 bottle.

--- Formula/php@8.1-brotli.rb
+++ Formula/php@8.1-brotli.rb
@@ -10,8 +10,8 @@ class PhpAT81Brotli < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any, sonoma:  "4a9862b38b0934657a56e44a9d46406a65c111cc6577f44e655a417c8d9e4803"
-    sha256 cellar: :any, ventura: "fafc4ba95383dba4d4df23b30a373df6e659138d671ce2751f0e4494d3bd7eab"
+    sha256 cellar: :any, sonoma:  "6d59ca9507822132242c29f8661d17dc870df1dedcc85a1697bdadf676613cff"
+    sha256 cellar: :any, ventura: "c0970fd56739f369491bcfe2ec4bc6740ff13a4bd4bd444bad483d4d2b3feb6c"
   end
 
   deprecate! date: "2025-12-31", because: :unsupported
