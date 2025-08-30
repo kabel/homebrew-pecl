879cf1072a457b2c87c1a05aac10e1b4ec258bcb
Kevin Abel <kevin.abel.0@gmail.com>
2025-08-30 14:05:59 -0500

    php-translit 0.7.2

--- Formula/php@8.3-translit.rb
+++ Formula/php@8.3-translit.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT83Translit < PhpPeclFormula
   extension_dsl "Transliterates non-latin character sets to latin"
 
-  url "https://pecl.php.net/get/translit-0.7.1.tgz"
-  sha256 "60a914a64416381c8e2b00ccf13188a872bca3e80cc6cc4ab6d7f0af949365b8"
+  url "https://pecl.php.net/get/translit-0.7.2.tgz"
+  sha256 "f9503184d29ff1959df264a5ab39874dd38a2785a650025e8dd84bfa387a8e7e"
   license "BSD-2-Clause"
 
   bottle do
