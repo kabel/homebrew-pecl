b59ded092d85707fc972495a3f262354261f6e53
Kevin Abel <kevin.abel.0@gmail.com>
2025-08-30 15:24:49 -0500

    php-apcu: update 5.1.27 bottle.

--- Formula/php-apcu.rb
+++ Formula/php-apcu.rb
@@ -9,7 +9,7 @@ class PhpApcu < PhpPeclFormula
 
   bottle do
     root_url "https://bottles.kabel.rocks/bottles-pecl"
-    sha256 cellar: :any_skip_relocation, sonoma:  "2be6e3f3e4f9568c07738da0777dc7d0047304782bff1d26d9699097f09e2b40"
-    sha256 cellar: :any_skip_relocation, ventura: "275bbc9a26735370f66fd67a67bb34e5803d33c285a344dcab1dcf2cd92baed0"
+    sha256 cellar: :any_skip_relocation, sonoma:  "6b705a3dff45067a20c107d525c4d19ba0927c21e827abcfae0ffe0174dae752"
+    sha256 cellar: :any_skip_relocation, ventura: "9d2f2c96970b22004c0ed445a840334f1f35ead6bef765e5f0a669ae1a402a5a"
   end
 end
